80bd9f63cf4f94f88a0b916e809b6f05884e8563
qiaozi.zwb <qiaozi.zwb@alibaba-inc.com>
2020-12-24 14:58:40 +0800

    add more log info

--- scripts/prepare_env.sh
+++ scripts/prepare_env.sh
@@ -17,6 +17,7 @@ is_in_wsl=false && [[ ! -z "${IS_WSL}" || ! -z "${WSL_DISTRO_NAME}" ]] && is_in_
 #   None
 ##########################
 function install_dependencies() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') install dependencies."
   if [[ "${platform}" == *"Ubuntu"* ]]; then
     sudo apt-get update -y
     sudo apt-get install -y git
@@ -43,17 +44,21 @@ function install_dependencies() {
 
   curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/"${K8S_VERSION}"/bin/linux/amd64/kubectl && \
   chmod +x kubectl && sudo mv kubectl /usr/local/bin/ && sudo ln /usr/local/bin/kubectl /usr/bin/kubectl || true
+  echo "$(date '+%Y-%m-%d %H:%M:%S') kubectl ${K8S_VERSION} installed."
 
   if [[ "${is_in_wsl}" = false ]]; then
       curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && \
       chmod +x minikube && sudo mv minikube /usr/local/bin/ && sudo ln /usr/local/bin/minikube /usr/bin/minikube || true
+      echo "$(date '+%Y-%m-%d %H:%M:%S') minikube ${K8S_VERSION} installed."
   else
       curl -Lo kind https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64 && \
       chmod +x kind && sudo mv kind /usr/local/bin/ && sudo ln /usr/local/bin/kind /usr/bin/kind || true
+      echo "$(date '+%Y-%m-%d %H:%M:%S') kind v0.9.0 installed."
   fi
 }
 
 function start_docker() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') starting doker daemon."
   # start docker daemon if docker not running.
   if ! sudo docker info >/dev/null 2>&1; then
     if [[ "${is_in_wsl}" = false ]]; then
@@ -62,9 +67,11 @@ function start_docker() {
       sudo dockerd > /dev/null&
     fi
   fi
+  echo "$(date '+%Y-%m-%d %H:%M:%S') docker started."
 }
 
 function launch_k8s_cluster() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') launching k8s cluster"
   if [[ "${is_in_wsl}" = false ]]; then
     export CHANGE_MINIKUBE_NONE_USER=true
     sudo sysctl fs.protected_regular=0 || true
@@ -83,20 +90,24 @@ function launch_k8s_cluster() {
     sudo cp -r /root/.kube ${HOME} || true
     sudo chown -R "$(id -u)":"$(id -g)" "${HOME}"/.kube || true
   fi
+  echo "$(date '+%Y-%m-%d %H:%M:%S') cluster is lauched successfully."
 }
 
 function pull_images() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S') pulling GraphScope images."
   sudo docker pull registry.cn-hongkong.aliyuncs.com/graphscope/graphscope:latest || true
   sudo docker pull registry.cn-hongkong.aliyuncs.com/graphscope/maxgraph_standalone_manager:1.0 || true
   sudo docker pull zookeeper:3.4.14 || true
   sudo docker pull quay.io/coreos/etcd:v3.4.13 || true
+  echo "$(date '+%Y-%m-%d %H:%M:%S') images pulled successfully."
 
   if [[ "${is_in_wsl}" = true ]]; then
-    echo "$(date '+%Y-%m-%d %H:%M:%S') load images into cluster"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') loading images into kind cluster."
     sudo kind load registry.cn-hongkong.aliyuncs.com/graphscope/graphscope:latest || true
     sudo kind load registry.cn-hongkong.aliyuncs.com/graphscope/maxgraph_standalone_manager:1.0 || true
     sudo kind load zookeeper:3.4.14 || true
     sudo kind load quay.io/coreos/etcd:v3.4.13 || true
+    echo "$(date '+%Y-%m-%d %H:%M:%S') images loaded."
   fi
 }
 
@@ -111,16 +122,12 @@ then
     exit 0
 fi
 
-echo "$(date '+%Y-%m-%d %H:%M:%S') install dependencies"
 install_dependencies
 
-echo "$(date '+%Y-%m-%d %H:%M:%S') start doker daemon"
 start_docker
 
-echo "$(date '+%Y-%m-%d %H:%M:%S') launch k8s cluster"
 launch_k8s_cluster
 
-echo "$(date '+%Y-%m-%d %H:%M:%S') pull images(graphscope, etcd, maxgraph_standalone_manager)"
 pull_images
 
 set +x
