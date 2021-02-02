aaaa5781230d122cc67e3ac9a224bf02596959c7
lidongze0629 <9546726@qq.com>
2021-02-02 18:01:24 +0800

    release 0.2.0

--- python/graphscope/version.py
+++ python/graphscope/version.py
@@ -26,7 +26,7 @@ if os.path.isfile(version_file_path):
         __version__ = fp.read().strip()
     __version_tuple__ = (int(v) for v in __version__.split("."))
 else:
-    __version__ = "0.1.3"
-    __version_tuple__ = (0, 1, 3)
+    __version__ = "0.2.0"
+    __version_tuple__ = (0, 2, 0)
 
 del version_file_path
