f5f5509d425b9bd705541ede4d0df6bfce5f442d
lidongze0629 <9546726@qq.com>
2021-04-01 21:33:55 +0800

    update

--- python/tests/test_run_locally.py
+++ python/tests/test_run_locally.py
@@ -33,16 +33,14 @@ from graphscope.dataset.modern_graph import load_modern_graph
 from graphscope.dataset.ogbn_mag import load_ogbn_mag
 from graphscope.framework.graph import Graph
 from graphscope.framework.loader import Loader
+from graphscope.learning.examples import GCN
+from graphscope.learning.graphlearn.python.model.tf.optimizer import get_tf_optimizer
+from graphscope.learning.graphlearn.python.model.tf.trainer import LocalTFTrainer
 
 test_repo_dir = os.path.expandvars("${GS_TEST_DIR}")
 
 
 def train(config, graph):
-    from graphscope.learning.examples import GCN
-    from graphscope.learning.graphlearn.python.model.tf.optimizer import \
-        get_tf_optimizer
-    from graphscope.learning.graphlearn.python.model.tf.trainer import LocalTFTrainer
-
     def model_fn():
         return GCN(
             graph,
