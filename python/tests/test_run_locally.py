c7d961f0e89fac744509dfa0f6a1763b1f308007
lidongze0629 <9546726@qq.com>
2021-04-14 14:28:58 +0800

    Debug

--- python/tests/test_run_locally.py
+++ python/tests/test_run_locally.py
@@ -74,8 +74,6 @@ def train(config, graph):
 
 @pytest.fixture
 def sess():
-    graphscope.set_option(show_log=True)
-    graphscope.set_option(initializing_interactive_engine=False)
     s = graphscope.session(cluster_type="hosts", num_workers=2)
     yield s
     s.close()
@@ -149,6 +147,61 @@ def demo(sess, ogbn_mag_small):
     train(config, lg)
 
 
+def simple_flow(sess, ogbn_mag_small):
+    graph = load_ogbn_mag(sess, ogbn_mag_small)
+
+    # Interactive engine
+    interactive = sess.gremlin(graph)
+    papers = interactive.execute(
+        "g.V().has('author', 'id', 2).out('writes').where(__.in('writes').has('id', 4307)).count()"
+    ).one()
+
+    # GLE on ogbn_mag_small graph
+    paper_features = []
+    for i in range(128):
+        paper_features.append("feat_" + str(i))
+    lg = sess.learning(
+        graph,
+        nodes=[("paper", paper_features)],
+        edges=[("paper", "cites", "paper")],
+        gen_labels=[
+            ("train", "paper", 100, (0, 75)),
+            ("val", "paper", 100, (75, 85)),
+            ("test", "paper", 100, (85, 100)),
+        ],
+    )
+
+    # hyperparameters config.
+    config = {
+        "class_num": 349,  # output dimension
+        "features_num": 128,
+        "batch_size": 500,
+        "val_batch_size": 100,
+        "test_batch_size": 100,
+        "categorical_attrs_desc": "",
+        "hidden_dim": 256,
+        "in_drop_rate": 0.5,
+        "hops_num": 2,
+        "neighs_num": [5, 10],
+        "full_graph_mode": False,
+        "agg_type": "gcn",  # mean, sum
+        "learning_algo": "adam",
+        "learning_rate": 0.01,
+        "weight_decay": 0.0005,
+        "epoch": 5,
+        "node_type": "paper",
+        "edge_type": "cites",
+    }
+
+    train(config, lg)
+
+
+def test_demo(ogbn_mag_small):
+    sess = graphscope.session(cluster_type="hosts", num_workers=2)
+    demo(sess, ogbn_mag_small)
+    sess.close()
+
+
 def test_multiple_session(ogbn_mag_small):
     sess1 = graphscope.session(cluster_type="hosts", num_workers=2)
     assert sess1.info["status"] == "active"
@@ -156,7 +209,8 @@ def test_multiple_session(ogbn_mag_small):
     sess2 = graphscope.session(cluster_type="hosts", num_workers=2)
     assert sess2.info["status"] == "active"
 
-    demo(sess1, ogbn_mag_small)
+    simple_flow(sess1, ogbn_mag_small)
+    simple_flow(sess2, ogbn_mag_small)
 
     sess1.close()
     sess2.close()
