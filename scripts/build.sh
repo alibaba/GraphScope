ae334826a4893e607adeef0e723df3eaef898a1e
siyuan0322 <siyuan0322@gmail.com>
2021-04-29 17:04:41 +0800

    check os and dependencies

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,83 @@ set -e
 set -x
 set -o pipefail
 
+is_in_wsl=false && [[ ! -z "${IS_WSL}" || ! -z "${WSL_DISTRO_NAME}" ]] && is_in_wsl=true
+
+# https://unix.stackexchange.com/questions/6345/how-can-i-get-distribution-name-and-version-number-in-a-simple-shell-script
+function get_os_version() {
+  if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    platform="${NAME}"
+    os_version="${VERSION_ID}"
+  elif type lsb_release >/dev/null 2>&1; then
+    # linuxbase.org
+    platform=$(lsb_release -si)
+    os_version=$(lsb_release -sr)
+  elif [ -f /etc/lsb-release ]; then
+    # For some versions of Debian/Ubuntu without lsb_release command
+    . /etc/lsb-release
+    platform="${DISTRIB_ID}"
+    os_version="${DISTRIB_RELEASE}"
+  elif [ -f /etc/debian_version ]; then
+    # Older Debian/Ubuntu/etc.
+    platform=Debian
+    os_version=$(cat /etc/debian_version)
+  elif [ -f /etc/centos-release ]; then
+    # Older Red Hat, CentOS, etc.
+    platform=CentOS
+    os_version=$(cat /etc/centos-release | sed 's/.* \([0-9]\).*/\1/')
+  else
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, Darwin, etc.
+    platform=$(uname -s)
+    os_version=$(uname -r)
+  fi
+}
+
+function check_os_compatibility() {
+  if [[ "${is_in_wsl}" == true && -z "${WSL_INTEROP}" ]]; then
+    echo "GraphScope not support to run on WSL1, please use WSL2."
+    exit 1
+  fi
+
+  if [[ "${platform}" != *"Ubuntu"* ]]; then
+    echo "This script is only available on Ubuntu"
+    exit 1
+  fi
+
+  if [[ "${platform}" == *"Ubuntu"* && "$(echo ${os_version} | sed 's/\([0-9]\)\([0-9]\).*/\1\2/')" -lt "20" ]]; then
+    echo "This script requires Ubuntu 20 or greater."
+    exit 1
+  fi
+
+  echo "$(date '+%Y-%m-%d %H:%M:%S') preparing environment on '${platform}' '${os_version}'"
+}
+
+function check_dependencies_version() {
+  err_msg="could not be found, you can install it via `install_dependencies.sh`."
+  if ! command -v mvn &> /dev/null; then
+      echo "maven ${err_msg}"
+      exit
+  fi
+  if ! hash python3; then
+    echo "Python3 is not installed"
+    exit 1
+  fi
+  ver=$(python3 -V 2>&1 | sed 's/.* \([0-9]\).\([0-9]\).*/\1\2/')
+  if [ "$ver" -lt "36" ]; then
+    echo "GraphScope requires python 3.6 or greater."
+    exit 1
+  fi
+  if ! command -v cargo &> /dev/null; then
+      echo "cargo ${err_msg}"
+      exit
+  fi
+  if ! command -v go &> /dev/null; then
+      echo "go ${err_msg}"
+      exit
+  fi
+}
+
 graphscope_src="$( cd "$(dirname "$0")/.." >/dev/null 2>&1 ; pwd -P )"
 
 function install_libgrape-lite() {
@@ -113,6 +190,12 @@ function install_client_and_coordinator() {
   pip3 install -U ./dist/*.whl
   popd
 }
+
+get_os_version
+
+check_os_compatibility
+
+check_dependencies_version
 
 install_libgrape-lite
 
