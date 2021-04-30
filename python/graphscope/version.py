3a83eade5565c7e1193a1b8bfc2393ea23b20afc
lidongze0629 <9546726@qq.com>
2021-04-30 21:17:37 +0800

    Release v0.4.0

--- python/graphscope/version.py
+++ python/graphscope/version.py
@@ -26,7 +26,7 @@ if os.path.isfile(version_file_path):
         __version__ = fp.read().strip()
     __version_tuple__ = (int(v) for v in __version__.split("."))
 else:
-    __version__ = "0.3.0"
-    __version_tuple__ = (0, 3, 0)
+    __version__ = "0.4.0"
+    __version_tuple__ = (0, 4, 0)
 
 del version_file_path
