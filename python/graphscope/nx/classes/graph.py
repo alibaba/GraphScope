83e6f6528a66dd7f010a20a2072b9fe0591887f9
qiaozi.zwb <qiaozi.zwb@alibaba-inc.com>
2021-05-23 16:25:43 +0800

    format

--- python/graphscope/nx/classes/graph.py
+++ python/graphscope/nx/classes/graph.py
@@ -262,9 +262,7 @@ class Graph(object):
         create_empty_in_engine = attr.pop(
             "create_empty_in_engine", True
         )  # a hidden parameter
-        self._duplicated_load = attr.pop(
-            "duplicated_load", False
-        )
+        self._duplicated_load = attr.pop("duplicated_load", False)
 
         if self._is_gs_graph(incoming_graph_data):
             self._session_id = incoming_graph_data.session_id
@@ -278,7 +276,9 @@ class Graph(object):
             self._session_id = sess.session_id
 
         if not self._is_gs_graph(incoming_graph_data) and create_empty_in_engine:
-            graph_def = empty_graph_in_engine(self, self.is_directed(), self._duplicated_load)
+            graph_def = empty_graph_in_engine(
+                self, self.is_directed(), self._duplicated_load
+            )
             self._key = graph_def.key
 
         # attempt to load graph with data
