c4e65da023eb949277aae9cbc1a25cc4c0cfa8f5
海虾 <tianli.tl@alibaba-inc.com>
2021-05-28 15:12:43 +0800

    add log

--- coordinator/gscoordinator/launcher.py
+++ coordinator/gscoordinator/launcher.py
@@ -215,7 +215,7 @@ class LocalLauncher(Launcher):
             universal_newlines=True,
             encoding="utf-8",
             stdin=subprocess.DEVNULL,
-            stdout=subprocess.DEVNULL,
+            stdout=sys.stdout,
             stderr=sys.stderr,
             bufsize=1,
         )
