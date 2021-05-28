1107d5a392c2432122c0d1831162590c83df812b
海虾 <tianli.tl@alibaba-inc.com>
2021-05-28 12:41:01 +0800

    tmp

--- interactive_engine/src/v2/src/main/java/com/alibaba/maxgraph/v2/frontend/ClientDdlService.java
+++ interactive_engine/src/v2/src/main/java/com/alibaba/maxgraph/v2/frontend/ClientDdlService.java
@@ -15,9 +15,7 @@
  */
 package com.alibaba.maxgraph.v2.frontend;
 
-import com.alibaba.maxgraph.proto.v2.*;
-import com.alibaba.maxgraph.v2.common.schema.GraphDef;
-import com.alibaba.maxgraph.v2.common.util.UuidUtils;
+import com.alibaba.graphscope.proto.ddl.*;
 import io.grpc.stub.StreamObserver;
 
 public class ClientDdlService extends ClientDdlGrpc.ClientDdlImplBase {
@@ -31,16 +29,26 @@ public class ClientDdlService extends ClientDdlGrpc.ClientDdlImplBase {
     }
 
     @Override
-    public void submitDdl(SubmitDdlRequest request, StreamObserver<SubmitDdlResponse> responseObserver) {
-        DdlRequestBatchPb.Builder builder = DdlRequestBatchPb.newBuilder();
-        for (DdlRequestPb ddlRequestPb : request.getDdlRequestsList()) {
-            builder.addDdlRequests(ddlRequestPb);
-        }
-        long snapshotId = this.schemaWriter.submitBatchDdl(UuidUtils.getBase64UUIDString(), "", builder.build());
-        this.snapshotCache.addListener(snapshotId, () -> {
-            GraphDef graphDef = this.snapshotCache.getSnapshotWithSchema().getGraphDef();
-            responseObserver.onNext(SubmitDdlResponse.newBuilder().setGraphDef(graphDef.toProto()).build());
-            responseObserver.onCompleted();
-        });
+    public void batchSubmit(BatchSubmitRequest request, StreamObserver<BatchSubmitResponse> responseObserver) {
+        request.getl
     }
+
+    @Override
+    public void getGraphDef(GetGraphDefRequest request, StreamObserver<GetGraphDefResponse> responseObserver) {
+    }
+
+    //
+//    @Override
+//    public void submitDdl(SubmitDdlRequest request, StreamObserver<SubmitDdlResponse> responseObserver) {
+//        DdlRequestBatchPb.Builder builder = DdlRequestBatchPb.newBuilder();
+//        for (DdlRequestPb ddlRequestPb : request.getDdlRequestsList()) {
+//            builder.addDdlRequests(ddlRequestPb);
+//        }
+//        long snapshotId = this.schemaWriter.submitBatchDdl(UuidUtils.getBase64UUIDString(), "", builder.build());
+//        this.snapshotCache.addListener(snapshotId, () -> {
+//            GraphDef graphDef = this.snapshotCache.getSnapshotWithSchema().getGraphDef();
+//            responseObserver.onNext(SubmitDdlResponse.newBuilder().setGraphDef(graphDef.toProto()).build());
+//            responseObserver.onCompleted();
+//        });
+//    }
 }
