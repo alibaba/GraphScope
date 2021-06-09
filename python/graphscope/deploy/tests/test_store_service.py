483dfdcac3b5bae2101824bb7f7cc5ed5cc0a247
siyuan0322 <siyuan0322@gmail.com>
2021-06-09 15:54:32 +0800

    test release step

--- python/graphscope/deploy/tests/test_store_service.py
+++ python/graphscope/deploy/tests/test_store_service.py
@@ -18,41 +18,13 @@
 
 import logging
 import os
-import random
-import string
-import subprocess
-import sys
 
 import numpy as np
 import pytest
 
 import graphscope
-from graphscope.config import GSConfig as gs_config
-from graphscope.dataset.ldbc import load_ldbc
-from graphscope.dataset.modern_graph import load_modern_graph
-from graphscope.framework.graph import Graph
-from graphscope.framework.loader import Loader
 
 logger = logging.getLogger("graphscope")
-
-
-def get_k8s_volumes():
-    k8s_volumes = {
-        "data": {
-            "type": "hostPath",
-            "field": {"path": os.environ["GS_TEST_DIR"], "type": "Directory"},
-            "mounts": {"mountPath": "/testingdata"},
-        }
-    }
-    return k8s_volumes
-
-
-def get_gs_image_on_ci_env():
-    if "GS_IMAGE" in os.environ and "GIE_MANAGER_IMAGE" in os.environ:
-        return os.environ["GS_IMAGE"], os.environ["GIE_MANAGER_IMAGE"]
-    else:
-        return gs_config.k8s_gs_image, gs_config.k8s_gie_graph_manager_image
-
 
 @pytest.fixture
 def gs_conn():
@@ -62,11 +34,6 @@ def gs_conn():
     grpc_endpoint = f"{node_ip}:{grpc_port}"
     gremlin_endpoint = f"{node_ip}:{gremlin_port}"
     yield graphscope.conn(grpc_endpoint, gremlin_endpoint)
-
-
-@pytest.fixture
-def data_dir():
-    return "/testingdata/ldbc_sample"
 
 
 def test_demo(gs_conn, data_dir):
@@ -83,6 +50,5 @@ def test_demo(gs_conn, data_dir):
     os.system(load_script)
 
     interactive = gs_conn.gremlin()
-    # TODO: Adapt the queries to ldbc.
     assert interactive.V().count().toList()[0] == 903
     assert interactive.E().count().toList()[0] == 6626
