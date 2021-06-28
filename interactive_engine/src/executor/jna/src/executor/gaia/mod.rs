81496fbf7c2e2c55743f563d38a93cefd8688ac9
海虾 <tianli.tl@alibaba-inc.com>
2021-06-28 17:44:08 +0800

    tmp

--- interactive_engine/src/executor/jna/src/executor/gaia/mod.rs
+++ interactive_engine/src/executor/jna/src/executor/gaia/mod.rs
@@ -4,7 +4,9 @@ use maxgraph_store::api::graph_partition::GraphPartitionManager;
 use gremlin_core::compiler::GremlinJobCompiler;
 
 pub fn initialize_job_compiler<V, VI, E, EI>(_graph_query: Arc<dyn GlobalGraphQuery<V=V, E=E, VI=VI, EI=EI>>,
-                                             _graph_partitioner: Arc<dyn GraphPartitionManager>)
+                                             _graph_partitioner: Arc<dyn GraphPartitionManager>,
+                                             _num_servers: usize,
+                                             _server_index: u64)
     -> GremlinJobCompiler
     where V: Vertex + 'static,
           VI: Iterator<Item=V> + Send + 'static,
