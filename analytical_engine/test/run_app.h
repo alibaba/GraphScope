174b364b16eb870ea4e755f0d24bb1381a792241
宁鑫 <1819397331@qq.com>
2021-07-09 20:38:25 +0800

    format check

--- analytical_engine/test/run_app.h
+++ analytical_engine/test/run_app.h
@@ -49,6 +49,7 @@ limitations under the License.
 #include "wcc/wcc.h"
 #include "wcc/wcc_auto.h"
 
+#include "apps/assortativity/attribute/attribute_assortativity.h"
 #include "apps/assortativity/degree/degree_assortativity_coefficient.h"
 #include "apps/bfs/bfs_generic.h"
 #include "apps/centrality/degree/degree_centrality.h"
@@ -119,7 +120,6 @@ DECLARE_string(dfs_format);
 
 DECLARE_string(source_degree_type);
 DECLARE_string(target_degree_type);
-
 
 namespace gs {
 
@@ -434,6 +434,13 @@ void Run() {
     CreateAndQuery<GraphType, AppType>(
         comm_spec, efile, vfile, out_prefix, FLAGS_datasource, fnum, spec,
         FLAGS_source_degree_type, FLAGS_target_degree_type, FLAGS_directed);
+  } else if (name == "attribute_assortativity") {
+    using GraphType =
+        grape::ImmutableEdgecutFragment<OID_T, VID_T, std::string, EDATA_T,
+                                        grape::LoadStrategy::kBothOutIn>;
+    using AppType = AttributeAssortativity<GraphType>;
+    CreateAndQuery<GraphType, AppType>(comm_spec, efile, vfile, out_prefix,
+                                       FLAGS_datasource, fnum, spec);
   } else {
     LOG(FATAL) << "No available application named [" << name << "].";
   }
