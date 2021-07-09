ad128821194a0b4cbd958f029da4a256f47378bc
MJY_HUST <nn9902@qq.com>
2021-07-09 15:48:10 +0800

    fix some problems and change formant

--- analytical_engine/apps/simple_path/is_simple_path_context.h
+++ analytical_engine/apps/simple_path/is_simple_path_context.h
@@ -17,88 +17,70 @@ limitations under the License.
 #define ANALYTICAL_ENGINE_APPS_SIMPLE_PATH_IS_SIMPLE_PTAH_CONTEXT_H_
 
 #include <limits>
+#include <set>
 #include <utility>
 #include <vector>
-#include <set>
 
 #include "folly/dynamic.h"
 #include "folly/json.h"
 #include "grape/grape.h"
-
 
 #include "core/context/tensor_context.h"
 
 namespace gs {
 
 template <typename FRAG_T>
-class IsSimplePathContext : public TensorContext<FRAG_T, typename FRAG_T::oid_t> {
- public:
+class IsSimplePathContext : public TensorContext<FRAG_T, bool> {
+public:
   using oid_t = typename FRAG_T::oid_t;
   using vid_t = typename FRAG_T::vid_t;
   using vertex_t = typename FRAG_T::vertex_t;
 
-  explicit IsSimplePathContext(const FRAG_T& fragment)
+  explicit IsSimplePathContext(const FRAG_T &fragment)
       : TensorContext<FRAG_T, typename FRAG_T::oid_t>(fragment) {}
 
   /**
    * @brief json formate
    *  josn = {"nodes" : [(oid_t)node1,(oid_t)node2,....]}
-   * 
-   * @param messages 
-   * @param nodes_json 
+   *
+   * @param messages
+   * @param nodes_json
    */
 
-  void Init(grape::DefaultMessageManager& messages, const std::string& nodes_json) {
-    auto& frag = this->fragment();
-    auto vertices = frag.Vertices();
-    auto inner_vertices = frag.InnerVertices();
-
+  void Init(grape::DefaultMessageManager &messages,
+            const std::string &nodes_json) {
+    std::set<oid_t> visit;
     is_simple_path = true;
-
     int counter = 0;
     oid_t pair_1;
-    folly::dynamic deserializeJson = folly::parseJson(json);
-    auto expressions = deserializeJson["nodes"];
-    for (auto val : expressions) {
-        oid_t key = (oid_t)val;
-        if(!visit.count(key)){
-          visit.insert(key);
-        }
-        else{
-          is_simple_path = false;
-          break;
-        }
-        counter ++;
-        if(counter == 1) {}
-        else{
-          pair_list.push_back(make_pair(pair_1,key));
-        }
-        pair_1 = key;
+
+    folly::dynamic::array nodes_array = folly::json(nodes_json);
+    for (auto val : nodes_array) {
+      oid_t key = (oid_t)val;
+      if (!visit.count(key)) {
+        visit.insert(key);
+      } else {
+        is_simple_path = false;
+        break;
+      }
+      counter++;
+      if (counter == 1) {
+      } else {
+        pair_list.push_back(make_pair(pair_1, key));
+      }
+      pair_1 = key;
     }
-
-  }
-  /**
-  void Init(grape::DefaultMessageManager& messages, const std::vector<oid_t>& pathlist) {
-    auto& frag = this->fragment();
-    auto vertices = frag.Vertices();
-    auto inner_vertices = frag.InnerVertices();
-
-    is_simple_path = true;
-    for (std::vector<vid_t>::iterator itr = pathlist.begin(); itr != (pathlist.end()-1); itr++)
-      pair_list.push_back(make_pair(*itr,*(itr+1));
-
-  }
-  */
-  void Output(std::ostream& os) override {
-    auto& frag = this->fragment();
-    os<<is_simple_path<<std::endl;
   }
 
-  std::set<oid_t> visit;
-  std::vector<std::pair<oid_t,oid_t>> pair_list;
+  void Output(std::ostream &os) override {
+    auto &frag = this->fragment();
+    os << is_simple_path << std::endl;
+  }
+
+  std::vector<std::pair<oid_t, oid_t>> pair_list;
   int true_counter = 0;
   bool is_simple_path;
 };
-}  // namespace gs
+} // namespace gs
 
-#endif  // ANALYTICAL_ENGINE_APPS_SIMPLE_PATH_IS_SIMPLE_PTAH_CONTEXT_H_
+#endif // ANALYTICAL_ENGINE_APPS_SIMPLE_PATH_IS_SIMPLE_PTAH_CONTEXT_H_
