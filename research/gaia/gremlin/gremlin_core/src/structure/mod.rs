7e22353fcfbf8066e81fbb964d4edef2a20ba97e
BingqingLyu <lv_bingqing@163.com>
2021-07-12 18:04:32 +0800

    [store_adapter] refine some codes in store adapter

--- research/gaia/gremlin/gremlin_core/src/structure/mod.rs
+++ research/gaia/gremlin/gremlin_core/src/structure/mod.rs
@@ -21,7 +21,7 @@ mod property;
 use crate::generated::gremlin as pb;
 use crate::structure::codec::ParseError;
 use crate::FromPb;
-pub use element::{Edge, Element, GraphElement, Label, Vertex, VertexOrEdge, ID, ID_BITS};
+pub use element::{Edge, Element, GraphElement, Label, LabelId, Vertex, VertexOrEdge, ID, ID_BITS};
 pub use filter::*;
 pub use graph::*;
 pub use property::{DefaultDetails, Details, DynDetails, PropId, PropKey, Token};
