f132828703879b93c13396af004e2e46f350cf2a
BingqingLyu <lv_bingqing@163.com>
2021-07-13 10:39:19 +0800

    [store_adapter] add MultiPartition for graphscope storage

--- research/gaia/gremlin/gs_gremlin/src/graph_proxy/mod.rs
+++ research/gaia/gremlin/gs_gremlin/src/graph_proxy/mod.rs
@@ -15,3 +15,45 @@
 
 mod global_storage;
 mod util;
+use gremlin_core::{Partitioner, ID, ID_MASK};
+use maxgraph_store::api::graph_partition::GraphPartitionManager;
+use maxgraph_store::api::{PropId, VertexId};
+use std::sync::Arc;
+
+/// A partition utility that one server contains multiple graph partitions
+pub struct MultiPartition {
+    graph_partition_manager: Arc<dyn GraphPartitionManager>,
+}
+
+impl MultiPartition {
+    pub fn new(graph_partition_manager: Arc<dyn GraphPartitionManager>) -> Self {
+        MultiPartition {
+            graph_partition_manager,
+        }
+    }
+}
+
+impl Partitioner for MultiPartition {
+    fn get_partition(&self, id: &ID, worker_num_per_server: usize) -> u64 {
+        // The partitioning logics is as follows:
+        // 1. `partition_id = self.graph_partition_manager.get_partition_id(*id as VertexId)` routes a given id
+        // to the partition that holds its data.
+        // 2. `server_index = partition_id % self.num_servers as u64` routes the partition id to the
+        // server R that holds the partition
+        // 3. `worker_index = partition_id % worker_num_per_server` picks up one worker to do the computation.
+        // 4. `server_index * worker_num_per_server + worker_index` computes the worker index in server R
+        // to do the computation.
+        let vid = (*id & (ID_MASK)) as VertexId;
+        let worker_num_per_server = worker_num_per_server as u64;
+        let partition_id = self.graph_partition_manager.get_partition_id(vid) as u64;
+        let server_index = self
+            .graph_partition_manager
+            .get_server_id(partition_id as PropId)
+            .unwrap_or_else(|| {
+                log::debug!("get server id failed in graph_partition_manager");
+                0
+            }) as u64;
+        let worker_index = partition_id % worker_num_per_server;
+        server_index * worker_num_per_server + worker_index as u64
+    }
+}
