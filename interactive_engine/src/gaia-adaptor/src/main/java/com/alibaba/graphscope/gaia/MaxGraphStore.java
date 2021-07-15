b35cddaa629f7c13c5bedc4b8553a500e89c8bb6
shirly121 <zxlmillie@163.com>
2021-07-15 14:34:45 +0800

    refine cached graph schema structure

--- interactive_engine/src/gaia-adaptor/src/main/java/com/alibaba/graphscope/gaia/MaxGraphStore.java
+++ interactive_engine/src/gaia-adaptor/src/main/java/com/alibaba/graphscope/gaia/MaxGraphStore.java
@@ -10,17 +10,17 @@ import org.slf4j.LoggerFactory;
 public class MaxGraphStore extends GraphStoreService {
     private static final Logger logger = LoggerFactory.getLogger(MaxGraphStore.class);
     public static final String MAXGRAPH_MODERN_PROPERTY_RESOURCE = "maxgraph.modern.properties.json";
-    private CachedGraphSchemaPair cachedGraphSchemaPair;
+    private CachedMaxGraphMeta cachedGraphSchemaPair;
 
     public MaxGraphStore(SchemaFetcher schemaFetcher) {
         super(MAXGRAPH_MODERN_PROPERTY_RESOURCE);
-        this.cachedGraphSchemaPair = new CachedGraphSchemaPair(schemaFetcher);
+        this.cachedGraphSchemaPair = new CachedMaxGraphMeta(schemaFetcher);
     }
 
     @Override
     public long getLabelId(String label) {
         try {
-            GraphSchema graphSchema = this.cachedGraphSchemaPair.get().getLeft();
+            GraphSchema graphSchema = this.cachedGraphSchemaPair.getGraphSchema();
             return graphSchema.getElement(label).getLabelId();
         } catch (Exception e) {
             logger.error("label " + label + " is invalid, please check schema");
@@ -30,7 +30,7 @@ public class MaxGraphStore extends GraphStoreService {
 
     @Override
     public String getLabel(long labelId) {
-        GraphSchema graphSchema = this.cachedGraphSchemaPair.get().getLeft();
+        GraphSchema graphSchema = this.cachedGraphSchemaPair.getGraphSchema();
         return graphSchema.getElement((int) labelId).getLabel();
     }
 
@@ -42,7 +42,7 @@ public class MaxGraphStore extends GraphStoreService {
     @Override
     public int getPropertyId(String propertyName) {
         try {
-            GraphSchema graphSchema = this.cachedGraphSchemaPair.get().getLeft();
+            GraphSchema graphSchema = this.cachedGraphSchemaPair.getGraphSchema();
             return graphSchema.getPropertyId(propertyName);
         } catch (Exception e) {
             logger.error("property " + propertyName + " is invalid, please check schema");
@@ -52,13 +52,13 @@ public class MaxGraphStore extends GraphStoreService {
 
     @Override
     public String getPropertyName(int propertyId) {
-        GraphSchema graphSchema = this.cachedGraphSchemaPair.get().getLeft();
+        GraphSchema graphSchema = this.cachedGraphSchemaPair.getGraphSchema();
         return graphSchema.getPropertyName(propertyId);
     }
 
     @Override
     public long getSnapShotId() {
-        return this.cachedGraphSchemaPair.get().getRight();
+        return this.cachedGraphSchemaPair.getSnapshotId();
     }
 
     @Override
