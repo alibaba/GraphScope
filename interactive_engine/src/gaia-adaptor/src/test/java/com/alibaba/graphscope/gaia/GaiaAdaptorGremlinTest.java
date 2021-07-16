f427b4acf0fcfcbe3d02d0d7310764fb75c7385f
shirly121 <zxlmillie@163.com>
2021-07-16 22:04:40 +0800

    reset gremlin test

--- interactive_engine/src/gaia-adaptor/src/test/java/com/alibaba/graphscope/gaia/GaiaAdaptorGremlinTest.java
+++ interactive_engine/src/gaia-adaptor/src/test/java/com/alibaba/graphscope/gaia/GaiaAdaptorGremlinTest.java
@@ -3,7 +3,7 @@ package com.alibaba.graphscope.gaia;
 import org.apache.tinkerpop.gremlin.GraphProviderClass;
 import org.junit.runner.RunWith;
 
-@RunWith(GaiaAdaptorGremlinTestSuite.class)
-@GraphProviderClass(provider = GaiaAdaptorTestGraphProvider.class, graph = GaiaAdaptorTestGraph.class)
+//@RunWith(GaiaAdaptorGremlinTestSuite.class)
+//@GraphProviderClass(provider = GaiaAdaptorTestGraphProvider.class, graph = GaiaAdaptorTestGraph.class)
 public class GaiaAdaptorGremlinTest {
 }
