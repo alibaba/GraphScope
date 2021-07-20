6ff99b52b0a45935705b7aeb34e3881e121af5b9
DongZe Li <9546726@qq.com>
2021-07-20 22:48:30 +0800

    Lazy mode support for GIE/GLE engine (#513)
    * Lazy mode support for GIE/GLE engine

--- python/tests/test_run_locally.py
+++ python/tests/test_run_locally.py
@@ -208,6 +208,70 @@ def test_demo(ogbn_mag_small):
     sess.close()
 
 
+def test_demo_lazy_mode(ogbn_mag_small):
+    sess = graphscope.session(cluster_type="hosts", num_workers=2, mode="lazy")
+    graph_node = load_ogbn_mag(sess, ogbn_mag_small)
+    # Interactive query
+    interactive_node = sess.gremlin(graph_node)
+    paper_result_node = interactive_node.execute(
+        "g.V().has('author', 'id', 2).out('writes').where(__.in('writes').has('id', 4307)).count()",
+        request_options={"engine": "gae"},
+    ).one()
+    sub_graph_node = interactive_node.subgraph(
+        "g.timeout(1000000).V().has('year', inside(2014, 2020)).outE('cites')",
+        request_options={"engine": "gae"},
+    )
+    simple_graph_node = sub_graph_node.project(
+        vertices={"paper": []}, edges={"cites": []}
+    )
+    ret1 = graphscope.k_core(simple_graph_node, k=5)
+    ret2 = graphscope.triangles(simple_graph_node)
+    sub_graph_node = sub_graph_node.add_column(ret1, {"kcore": "r"})
+    sub_graph_node = sub_graph_node.add_column(ret2, {"tc": "r"})
+    # GLE on ogbn_mag_small graph
+    paper_features = []
+    for i in range(128):
+        paper_features.append("feat_" + str(i))
+    paper_features.append("kcore")
+    paper_features.append("tc")
+    learning_graph_node = sess.learning(
+        sub_graph_node,
+        nodes=[("paper", paper_features)],
+        edges=[("paper", "cites", "paper")],
+        gen_labels=[
+            ("train", "paper", 100, (0, 75)),
+            ("val", "paper", 100, (75, 85)),
+            ("test", "paper", 100, (85, 100)),
+        ],
+    )
+    # sess run
+    # r[0]: gremlin result
+    # r[1]: learning graph instance
+    r = sess.run([paper_result_node, learning_graph_node])
+    # hyperparameters config.
+    config = {
+        "class_num": 349,  # output dimension
+        "features_num": 130,  # 128 dimension + kcore + triangle count
+        "batch_size": 500,
+        "val_batch_size": 100,
+        "test_batch_size": 100,
+        "categorical_attrs_desc": "",
+        "hidden_dim": 256,
+        "in_drop_rate": 0.5,
+        "hops_num": 2,
+        "neighs_num": [5, 10],
+        "full_graph_mode": False,
+        "agg_type": "gcn",  # mean, sum
+        "learning_algo": "adam",
+        "learning_rate": 0.01,
+        "weight_decay": 0.0005,
+        "epoch": 5,
+        "node_type": "paper",
+        "edge_type": "cites",
+    }
+    train(config, r[1])
+
+
 @pytest.mark.skipif(sys.platform == "darwin", reason="Mac no need to run this test.")
 def test_multiple_session(ogbn_mag_small):
     sess1 = graphscope.session(cluster_type="hosts", num_workers=1)
