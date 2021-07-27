e654a478eaa1f23cd0e82a1f00bdf5925fb880a2
BingqingLyu <lv_bingqing@163.com>
2021-07-27 15:06:22 +0800

    minor fix

--- interactive_engine/src/instance-manager/script/create_local_instance.sh
+++ interactive_engine/src/instance-manager/script/create_local_instance.sh
@@ -31,10 +31,9 @@ bash ${SCRIPT_DIR}/start_local_frontend.sh $object_id $schema_path $zookeeper_po
 sleep 1
 bash ${SCRIPT_DIR}/start_local_executor.sh $object_id $server_id $VINEYARD_IPC_SOCKET $zookeeper_port
 sleep 1
-graph_name=${object_id}"_gaia"
 #start gaia
 if [ $ENABLE_GAIA ];then
-  graph_name=gaia_graph
+  graph_name=${object_id}"_gaia"
   bash ${SCRIPT_DIR}/start_local_gaia_coordinator.sh $object_id $zookeeper_port $graph_name
   sleep 1
   bash ${SCRIPT_DIR}/start_local_gaia_frontend.sh $object_id $schema_path $zookeeper_port $graph_name
