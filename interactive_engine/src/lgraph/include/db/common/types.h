d16d62636df193ed7c6ca6fdece68da396acb9fa
houbai.zzc <houbai.zzc@alibaba-inc.com>
2021-08-03 14:35:05 +0800

    Store Fix

--- interactive_engine/src/lgraph/include/db/common/types.h
+++ interactive_engine/src/lgraph/include/db/common/types.h
@@ -35,21 +35,50 @@ struct EdgeId {
   EdgeInnerId edge_inner_id;
   VertexId src_vertex_id;
   VertexId dst_vertex_id;
+
+  static EdgeId From(EdgeInnerId inner_id, VertexId src_id, VertexId dst_id) {
+    EdgeId e_id{};
+    e_id.edge_inner_id = inner_id;
+    e_id.src_vertex_id = src_id;
+    e_id.dst_vertex_id = dst_id;
+    return e_id;
+  }
 };
 
 struct EdgeRelation {
   LabelId edge_label_id;
   LabelId src_vertex_label_id;
   LabelId dst_vertex_label_id;
-};
+
+  static EdgeRelation From(LabelId e_label_id, LabelId src_label_id, LabelId dst_label_id) {
+    EdgeRelation er{};
+    er.edge_label_id = e_label_id;
+    er.src_vertex_label_id = src_label_id;
+    er.dst_vertex_label_id = dst_label_id;
+    return er;
+  }
+
+  static EdgeRelation None() {
+    return From(std::numeric_limits<LabelId>::max(),
+                std::numeric_limits<LabelId>::max(),
+                std::numeric_limits<LabelId>::max());
+  }
+ };
 
 struct StringSlice {
   void* data;
   size_t len;
+
+  static StringSlice From(void* data_ptr, size_t length) {
+    StringSlice ss{};
+    ss.data = data_ptr;
+    ss.len = length;
+    return ss;
+  }
 };
 
 const LabelId none_label_id = std::numeric_limits<LabelId>::max();
-const EdgeRelation none_edge_relation{};
+const EdgeRelation none_edge_relation = EdgeRelation::None();
 
 typedef void* PartitionGraphHandle;
 typedef void* PartitionSnapshotHandle;
