40664401ee8b8f73866bb1d56537a2458ed0cb6e
siyuan0322 <siyuan0322@gmail.com>
2021-08-10 11:03:37 +0800

    update

--- analytical_engine/apps/pregel/louvain/louvain_vertex.h
+++ analytical_engine/apps/pregel/louvain/louvain_vertex.h
@@ -121,8 +121,8 @@ class LouvainVertex : public PregelVertex<FRAG_T, VD_T, MD_T> {
         if (edges.find(gid) != edges.end()) {
           ret += edges.at(gid);
         } else {
-          LOG(ERROR) << "Warning: Cannot find a edge from " << this->id() << " to "
-                     << gid;
+          LOG(ERROR) << "Warning: Cannot find a edge from " << this->id()
+                     << " to " << gid;
         }
       }
     }
