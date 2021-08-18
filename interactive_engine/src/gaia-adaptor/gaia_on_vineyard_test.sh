12273e0e2dd2b2d22d097a233083e0835669af22
shirly121 <zxlmillie@163.com>
2021-08-18 12:30:18 +0800

    make ci test based on python server

--- interactive_engine/src/gaia-adaptor/gaia_on_vineyard_test.sh
+++ interactive_engine/src/gaia-adaptor/gaia_on_vineyard_test.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 base_dir=$(cd `dirname $0`; pwd)
-ps -ef | grep "test_load_modern_graph" | grep -v grep | awk '{print $2}' | xargs kill
+_port=8277
 rm -rf /tmp/test_modern.log
-# load data && start server
-cd $base_dir/../../../python/tests/local
-python3 -m pytest -s -v ./load_modern_graph.py -k test_load_modern_graph &
-# wait until gaia gremlin is ready
-while [ 1 ];
-do
-    GAIA_ENDPOINT=`grep "build gaia frontend" /tmp/test_modern.log`
-    if [ $? -eq 0 ]; then
-    	GAIA_ENDPOINT=`echo $GAIA_ENDPOINT | awk -F"frontend " '{print $2}' | awk -F" " '{print $1}'`
-        break;
-    fi
-    sleep 5
-done
+cd ${base_dir}/../../deploy/testing && python3 maxgraph_test_server.py ${_port} &
+sleep 5s
+# load data and start instance
+curl -XPOST http://localhost:${_port} -d 'import graphscope'
+curl -XPOST http://localhost:${_port} -d 'graphscope.set_option(show_log=True)'
+curl -XPOST http://localhost:${_port} -d 'from graphscope.framework.loader import Loader'
+curl -XPOST http://localhost:${_port} -d 'from graphscope.dataset.modern_graph import load_modern_graph'
+curl -XPOST http://localhost:${_port} -d 'session=graphscope.session(cluster_type="hosts", num_workers=2, enable_gaia=True)'
+curl -XPOST http://localhost:${_port} -d 'graph = load_modern_graph(session, "/home/GraphScope/interactive_engine/tests/src/main/resources/modern_graph")'
+curl -XPOST http://localhost:${_port} -d 'interactive = session.gremlin(graph)' 1>/tmp/test_modern.log 2>&1
+GAIA_ENDPOINT=`grep "build gaia frontend" /tmp/test_modern.log`
+GAIA_ENDPOINT=`echo $GAIA_ENDPOINT | awk -F"frontend " '{print $2}' | awk -F" " '{print $1}'`
 echo $GAIA_ENDPOINT > ${base_dir}/src/test/resources/graph.endpoint
 cd ${base_dir} && mvn test
 exit_code=$?
-ps -ef | grep "test_load_modern_graph" | grep -v grep | awk '{print $2}' | xargs kill
+curl -XPOST http://localhost:${_port} -d 'session.close()'
+kill -INT `lsof -i:${_port} -t`
 if [ $exit_code -ne 0 ]; then
     echo "gaia_on_vineyard_store gremlin test fail"
     exit 1
