9da22cd4bf2cf2da5c39f87303c1b2a82a8fbb43
shirly121 <zxlmillie@163.com>
2021-08-18 18:39:52 +0800

    update

--- interactive_engine/src/gaia-adaptor/gaia_on_vineyard_test.sh
+++ interactive_engine/src/gaia-adaptor/gaia_on_vineyard_test.sh
@@ -10,7 +10,7 @@ curl -XPOST http://localhost:${_port} -d 'graphscope.set_option(show_log=True)'
 curl -XPOST http://localhost:${_port} -d 'from graphscope.framework.loader import Loader'
 curl -XPOST http://localhost:${_port} -d 'from graphscope.dataset.modern_graph import load_modern_graph'
 curl -XPOST http://localhost:${_port} -d 'session=graphscope.session(cluster_type="hosts", num_workers=2, enable_gaia=True)'
-curl -XPOST http://localhost:${_port} -d 'graph = load_modern_graph(session, "${GITHUB_WORKSPACE}/interactive_engine/tests/src/main/resources/modern_graph")'
+curl -XPOST http://localhost:${_port} -d 'graph = load_modern_graph(session, \'${GITHUB_WORKSPACE}/interactive_engine/tests/src/main/resources/modern_graph\')'
 curl -XPOST http://localhost:${_port} -d 'interactive = session.gremlin(graph)' --output /tmp/test_modern.log
 curl -XPOST http://localhost:${_port} -d 'interactive._graph_url[1]' --output /tmp/test_modern.log
 GAIA_PORT=`cat /tmp/test_modern.log | awk -F'\/|:' '{print $5}'`
