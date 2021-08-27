c3f7de2aa3c5545d46a43735bc60ea391476eb53
shirly121 <zxlmillie@163.com>
2021-08-27 17:11:14 +0800

    copy compiler under gaia to gaia-x

---
+++ research/gaia-x/gremlin/compiler/gremlin-server-plugin/src/main/java/com/alibaba/graphscope/gaia/plan/meta/object/Vertex.java
@@ -0,0 +1,9 @@
+package com.alibaba.graphscope.gaia.plan.meta.object;
+
+import java.util.UUID;
+
+public class Vertex extends GraphElement {
+    public Vertex() {
+        super(UUID.randomUUID());
+    }
+}
