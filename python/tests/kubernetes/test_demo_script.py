a76fc90048adc626f4e7dd672c36f8dde3ea27bf
qiaozi.zwb <qiaozi.zwb@alibaba-inc.com>
2021-09-02 00:10:50 +0800

    add hdfs to ci

--- python/tests/kubernetes/test_demo_script.py
+++ python/tests/kubernetes/test_demo_script.py
@@ -136,6 +136,60 @@ def test_demo(gs_session, data_dir):
     # GNN engine
 
 
+@pytest.mark.skipif("HDFS_TEST_DIR" not in os.environ, reason="the test case need HDFS")
+def test_demo_on_hdfs(gs_session_distributed):
+    graph = gs_session_distributed.g()
+    graph.add_vertices(
+        Loader(
+            os.environ["HDFS_TEST_DIR"] + "/person_0_0.csv",
+            host="localhost",
+            port="9000",
+            delimiter="|",
+        ),
+        "person",
+        [
+            "firstName",
+            "lastName",
+            "gender",
+            "birthday",
+            "creationDate",
+            "locationIP",
+            "browserUsed",
+        ],
+        "id",
+    )
+    graph = graph.add_edges(
+        Loader(
+            os.environ["HDFS_TEST_DIR"] + "/person_knows_person_0_0.csv",
+            host="localhost",
+            port="9000",
+            delimiter="|",
+        ),
+        "knows",
+        ["creationDate"],
+        src_label="person",
+        dst_label="person",
+    )
+
+    # Interactive engine
+    interactive = gs_session.gremlin(graph)
+    sub_graph = interactive.subgraph(  # noqa: F841
+        'g.V().hasLabel("person").outE("knows")'
+    )
+
+    # Analytical engine
+    # project the projected graph to simple graph.
+    simple_g = sub_graph.project(vertices={"person": []}, edges={"knows": []})
+
+    pr_result = graphscope.pagerank(simple_g, delta=0.8)
+    tc_result = graphscope.triangles(simple_g)
+
+    # add the PageRank and triangle-counting results as new columns to the property graph
+    # FIXME: Add column to sub_graph
+    sub_graph.add_column(pr_result, {"Ranking": "r"})
+    sub_graph.add_column(tc_result, {"TC": "r"})
+
+
 def test_demo_distribute(gs_session_distributed, data_dir, modern_graph_data_dir):
     graph = load_ldbc(gs_session_distributed, data_dir)
 
