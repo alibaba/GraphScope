ce6a1a3f960619ff207026b8eb125378d9253a47
qiaozi.zwb <qiaozi.zwb@alibaba-inc.com>
2021-09-03 12:23:02 +0800

    fix local ci

--- interactive_engine/src/gaia-adaptor/gremlin_test.sh
+++ interactive_engine/src/gaia-adaptor/gremlin_test.sh
@@ -2,7 +2,7 @@
 base_dir=$(cd `dirname $0`; pwd)
 ps -ef | grep "com.alibaba.graphscope.gaia.MaxNode" | grep -v grep | awk '{print $2}' | xargs kill -9
 cd ${base_dir}/../.. && mvn clean install -DskipTests -Pv2
-cd ./distribution/target/ && sudo tar xvzf maxgraph.tar.gz && cd maxgraph
+cd ./distribution/target/ && tar xvzf maxgraph.tar.gz && cd maxgraph
 
 # start server
 current_dir=$(pwd)
