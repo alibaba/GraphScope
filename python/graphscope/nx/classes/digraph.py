4f40b828dbb5b20a61fb560500ac33aaa031bb95
qiaozi.zwb <qiaozi.zwb@alibaba-inc.com>
2021-09-16 14:43:34 +0800

    update

--- python/graphscope/nx/classes/digraph.py
+++ python/graphscope/nx/classes/digraph.py
@@ -38,8 +38,8 @@ from graphscope.nx.classes.graph import Graph
 from graphscope.nx.convert import from_gs_graph
 from graphscope.nx.convert import to_nx_graph
 from graphscope.nx.utils.compat import patch_docstring
-from graphscope.nx.utils.other import check_node_is_legal
-from graphscope.nx.utils.other import empty_graph_in_engine
+from graphscope.nx.utils.misc import check_node_is_legal
+from graphscope.nx.utils.misc import empty_graph_in_engine
 from graphscope.proto import types_pb2
 
 
--- python/graphscope/nx/classes/graph.py
+++ python/graphscope/nx/classes/graph.py
@@ -43,9 +43,9 @@ from graphscope.nx.classes.dicts import NodeDict
 from graphscope.nx.convert import from_gs_graph
 from graphscope.nx.convert import to_nx_graph
 from graphscope.nx.utils.compat import patch_docstring
-from graphscope.nx.utils.other import check_node_is_legal
-from graphscope.nx.utils.other import empty_graph_in_engine
-from graphscope.nx.utils.other import parse_ret_as_dict
+from graphscope.nx.utils.misc import check_node_is_legal
+from graphscope.nx.utils.misc import empty_graph_in_engine
+from graphscope.nx.utils.misc import parse_ret_as_dict
 from graphscope.proto import graph_def_pb2
 from graphscope.proto import types_pb2
 
