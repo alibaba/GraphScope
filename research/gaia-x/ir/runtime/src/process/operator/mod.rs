ee820d6792194fa8288b7d976d160035930d05cb
BingqingLyu <lv_bingqing@163.com>
2021-10-29 18:17:46 +0800

    [WIP] fix and add tests for TagKey

--- research/gaia-x/ir/runtime/src/process/operator/mod.rs
+++ research/gaia-x/ir/runtime/src/process/operator/mod.rs
@@ -102,6 +102,7 @@ impl<'a> TagKey {
             }
         } else {
             match entry {
+                Entry::Element(RecordElement::OnGraph(element)) => Ok(element.as_borrow_object()),
                 Entry::Element(RecordElement::OutGraph(object)) => match object {
                     ObjectElement::None => Err(KeyedError::from(
                         "Get key failed since it refers to an empty object element",
@@ -110,7 +111,7 @@ impl<'a> TagKey {
                     ObjectElement::Count(cnt) => Ok((*cnt).into()),
                     ObjectElement::Agg(agg) => Ok(agg.as_borrow()),
                 },
-                _ => Err(KeyedError::from(
+                Entry::Collection(_) => Err(KeyedError::from(
                     "Get key failed since it refers to a Collection type entry",
                 )),
             }
@@ -157,5 +158,156 @@ impl TryFrom<common_pb::Variable> for TagKey {
             None
         };
         Ok(TagKey { tag, key: prop })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::graph::element::Vertex;
+    use crate::graph::property::{DefaultDetails, DynDetails};
+    use dyn_type::Object;
+    use std::collections::HashMap;
+
+    fn init_vertex1() -> Vertex {
+        let map1: HashMap<NameOrId, Object> = vec![
+            (NameOrId::from("age".to_string()), 31.into()),
+            (NameOrId::from("birthday".to_string()), 19900416.into()),
+            (
+                NameOrId::from("name".to_string()),
+                "John".to_string().into(),
+            ),
+        ]
+        .into_iter()
+        .collect();
+
+        Vertex::new(DynDetails::new(DefaultDetails::with_property(
+            1,
+            NameOrId::from(9),
+            map1,
+        )))
+    }
+
+    fn init_vertex2() -> Vertex {
+        let map2: HashMap<NameOrId, Object> = vec![
+            (NameOrId::from("age".to_string()), 26.into()),
+            (NameOrId::from("birthday".to_string()), 19950816.into()),
+            (
+                NameOrId::from("name".to_string()),
+                "Nancy".to_string().into(),
+            ),
+        ]
+        .into_iter()
+        .collect();
+
+        Vertex::new(DynDetails::new(DefaultDetails::with_property(
+            1,
+            NameOrId::from(9),
+            map2,
+        )))
+    }
+
+    fn init_record() -> Record {
+        let vertex1 = init_vertex1();
+        let vertex2 = init_vertex2();
+        let object3 = ObjectElement::Count(10);
+
+        let mut record = Record::new(vertex1, None);
+        record.append(vertex2, Some(NameOrId::Id(0)));
+        record.append(object3, Some(NameOrId::Id(1)));
+        record
+    }
+
+    #[test]
+    fn test_get_none_tag_entry() {
+        let tag_key = TagKey {
+            tag: None,
+            key: None,
+        };
+        let expected = init_vertex1();
+        let record = init_record();
+        let entry = tag_key.get_entry(&record).unwrap();
+        match entry {
+            Entry::Element(RecordElement::OnGraph(element)) => {
+                assert_eq!(element.id(), expected.id());
+            }
+            _ => {
+                assert!(false);
+            }
+        }
+    }
+
+    #[test]
+    fn test_get_none_tag_obj() {
+        let tag_key = TagKey {
+            tag: None,
+            key: None,
+        };
+        let expected = init_vertex1();
+        let record = init_record();
+        let obj = tag_key.get_obj(&record).unwrap();
+        assert_eq!(obj, expected.as_borrow_object());
+    }
+
+    #[test]
+    fn test_get_tag_entry() {
+        let tag_key = TagKey {
+            tag: Some(NameOrId::Id(0)),
+            key: None,
+        };
+        let expected = init_vertex2();
+        let record = init_record();
+        let entry = tag_key.get_entry(&record).unwrap();
+        match entry {
+            Entry::Element(RecordElement::OnGraph(element)) => {
+                assert_eq!(element.id(), expected.id());
+            }
+            _ => {
+                assert!(false);
+            }
+        }
+    }
+
+    #[test]
+    fn test_get_tag_obj() {
+        let tag_key = TagKey {
+            tag: Some(NameOrId::Id(1)),
+            key: None,
+        };
+        let expected = 10.into();
+        let record = init_record();
+        let obj = tag_key.get_obj(&record).unwrap();
+        assert_eq!(obj, expected)
+    }
+
+    #[test]
+    fn test_get_tag_key_entry() {
+        let tag_key = TagKey {
+            tag: Some(NameOrId::Id(0)),
+            key: Some(PropKey::Key(NameOrId::Str("age".to_string()))),
+        };
+        let expected = 26;
+        let record = init_record();
+        let entry = tag_key.get_entry(&record).unwrap();
+        match entry {
+            Entry::Element(RecordElement::OutGraph(ObjectElement::Prop(obj))) => {
+                assert_eq!(obj, expected.into());
+            }
+            _ => {
+                assert!(false);
+            }
+        }
+    }
+
+    #[test]
+    fn test_get_tag_key_obj() {
+        let tag_key = TagKey {
+            tag: Some(NameOrId::Id(0)),
+            key: Some(PropKey::Key(NameOrId::Str("name".to_string()))),
+        };
+        let expected = "Nancy";
+        let record = init_record();
+        let obj = tag_key.get_obj(&record).unwrap();
+        assert_eq!(obj, expected.into())
     }
 }
