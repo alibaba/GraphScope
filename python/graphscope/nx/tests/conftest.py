83586e293185629384cecf5f4b5a25306af1e524
acezen <weibin.zen@gmail.com>
2021-10-29 12:15:58 +0800

    update

--- python/graphscope/nx/tests/conftest.py
+++ python/graphscope/nx/tests/conftest.py
@@ -28,10 +28,10 @@ def graphscope_session():
     graphscope.set_option(show_log=True)
     graphscope.set_option(initializing_interactive_engine=False)
 
+    subprocess.check_call(["free", "-h"])
     sess = graphscope.session(cluster_type="hosts")
 
     sess.as_default()
-    subprocess.check_call(["free", "-h"])
     yield sess
     subprocess.check_call(["free", "-h"])
     sess.close()
