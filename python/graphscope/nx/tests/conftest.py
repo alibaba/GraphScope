dc622be0e59d6e9a28dbae76115fc9f8942bbef8
acezen <weibin.zen@gmail.com>
2021-11-01 00:43:06 +0800

    revet debug
    Signed-off-by: acezen <weibin.zen@gmail.com>

--- python/graphscope/nx/tests/conftest.py
+++ python/graphscope/nx/tests/conftest.py
@@ -16,8 +16,6 @@
 # limitations under the License.
 #
 
-import subprocess
-
 import pytest
 
 import graphscope
@@ -28,10 +26,7 @@ def graphscope_session():
     graphscope.set_option(show_log=True)
     graphscope.set_option(initializing_interactive_engine=False)
 
-    sess = graphscope.session(cluster_type="hosts")
-    subprocess.check_call(["free", "-h"])
-
+    sess = graphscope.session(cluster_type="hosts", num_workers=1)
     sess.as_default()
     yield sess
-    subprocess.check_call(["free", "-h"])
     sess.close()
