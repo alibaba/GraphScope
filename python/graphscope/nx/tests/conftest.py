b55f8a3cc0d012f65aa7b470ca44278534bb0012
acezen <weibin.zen@gmail.com>
2021-11-01 08:46:06 +0800

    update

--- python/graphscope/nx/tests/conftest.py
+++ python/graphscope/nx/tests/conftest.py
@@ -26,7 +26,7 @@ def graphscope_session():
     graphscope.set_option(show_log=True)
     graphscope.set_option(initializing_interactive_engine=False)
 
-    sess = graphscope.session(cluster_type="hosts", num_workers=1)
+    sess = graphscope.session(cluster_type="hosts")
     sess.as_default()
     yield sess
     sess.close()
