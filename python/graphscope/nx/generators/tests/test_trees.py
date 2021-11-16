b0503c0e5cb9de78c1c9e044158387c178ad8be9
acezen <weibin.zen@gmail.com>
2021-11-16 19:13:28 +0800

    update

--- python/graphscope/nx/generators/tests/test_trees.py
+++ python/graphscope/nx/generators/tests/test_trees.py
@@ -1,7 +1,12 @@
 import pytest
 from networkx.generators.tests.test_trees import test_random_tree
 
-from graphscope.nx.generators.trees import NIL
+try:
+    from graphscope.nx.generators.trees import NIL
+except ImportError:
+    # NetworkX >=2.6 not contains NIL
+    pass
+
 from graphscope.nx.utils import arbitrary_element
 from graphscope.nx.utils.compat import with_graphscope_nx_context
 
