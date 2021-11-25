fdab784ad005c03bdd4e3effbf66838234240cbb
longbin.lailb <longbin.lailb@alibaba-inc.com>
2021-11-25 13:59:37 +0800

    [gaia-x]: to only preserve the engine codes

--- research/gaia/gremlin/compiler/gremlin-server-plugin/src/main/java/com/alibaba/graphscope/gaia/broadcast/channel/RpcChannelFetcher.java
+++
@@ -1,9 +0,0 @@
-package com.alibaba.graphscope.gaia.broadcast.channel;
-
-import com.alibaba.pegasus.RpcChannel;
-
-import java.util.List;
-
-public interface RpcChannelFetcher {
-    List<RpcChannel> fetch();
-}
