db249be9a80d3786ae2fbdf90d6804eedc663079
acezen <weibin.zen@gmail.com>
2021-11-25 19:38:48 +0800

    update

--- python/graphscope/nx/algorithms/tests/forward/test_threshold.py
+++ python/graphscope/nx/algorithms/tests/forward/test_threshold.py
@@ -1,3 +1,5 @@
+import os
+
 import networkx.algorithms.tests.test_threshold
 import pytest
 
@@ -31,3 +33,23 @@ class TestGeneratorThreshold:
                              nxt.threshold_graph,
                              cs,
                              create_using=nx.DiGraph())
+
+    @pytest.mark.skipif("CI" in os.environ, reason="This case would failed in ci.")
+    def test_finding_routines(self):
+        G = nx.Graph({1: [2], 2: [3], 3: [4], 4: [5], 5: [6]})
+        G.add_edge(2, 4)
+        G.add_edge(2, 5)
+        G.add_edge(2, 7)
+        G.add_edge(3, 6)
+        G.add_edge(4, 6)
+
+        # Alternating 4 cycle
+        assert nxt.find_alternating_4_cycle(G) == [1, 2, 3, 6]
+
+        # Threshold graph
+        TG = nxt.find_threshold_graph(G)
+        assert nxt.is_threshold_graph(TG)
+        assert sorted(TG.nodes()) == [1, 2, 3, 4, 5, 7]
+
+        cs = nxt.creation_sequence(dict(TG.degree()), with_labels=True)
+        assert nxt.find_creation_sequence(G) == cs
