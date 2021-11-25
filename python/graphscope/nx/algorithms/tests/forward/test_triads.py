5b9c9f067bd4037844cb04e9d78d444ce29e3268
acezen <weibin.zen@gmail.com>
2021-11-25 15:56:53 +0800

    update

--- python/graphscope/nx/algorithms/tests/forward/test_triads.py
+++ python/graphscope/nx/algorithms/tests/forward/test_triads.py
@@ -2,7 +2,6 @@ import networkx.algorithms.tests.test_triads
 import pytest
 
 from graphscope.nx.utils.compat import import_as_graphscope_nx
-from graphscope.nx.utils.compat import with_graphscope_nx_context
 
 import_as_graphscope_nx(networkx.algorithms.tests.test_triads,
                         decorators=pytest.mark.usefixtures("graphscope_session"))
