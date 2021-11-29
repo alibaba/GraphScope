144fba5c612d40dc3113534ecaa1ca5684ee0c86
bmmcq <chenqiang.mcq@alibaba-inc.com>
2021-11-29 11:21:30 +0800

    [GAIA/engine] bug fix in merge;

--- research/engine/pegasus/pegasus/tests/merge_test.rs
+++ research/engine/pegasus/pegasus/tests/merge_test.rs
@@ -46,14 +46,14 @@ fn copy_merge_test_02() {
         |input, output| {
             let src = input.input_from(0..3)?;
             let (s1, s2) = src.copied()?;
-            let s3 = s1.map(|l| Ok(l.clone()))?;
-            let s4 = s2.map(|r| Ok(r.clone()))?;
+            let s3 = s1.map(|l| Ok(l))?;
+            let s4 = s2.map(|r| Ok(r))?;
             s3.merge(s4)?.sink_into(output)
         }
     })
         .expect("submit job failure");
 
-    let expected = vec![0, 0, 1, 1, 2, 2, 3, 3];
+    let expected = vec![0, 0, 1, 1, 2, 2];
     let mut results = vec![];
     while let Some(Ok(res)) = result.next() {
         results.push(res);
