ed0def6689b99ff496ab320eb765092eb1f3a4fb
shirly121 <zxlmillie@163.com>
2021-12-16 17:41:21 +0800

    refine code

--- research/query_service/ir/compiler/src/main/java/com/alibaba/graphscope/common/IrPlan.java
+++ research/query_service/ir/compiler/src/main/java/com/alibaba/graphscope/common/IrPlan.java
@@ -37,7 +37,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 // represent ir plan as a chain of operators
 public class IrPlan implements Closeable {
