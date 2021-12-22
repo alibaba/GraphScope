34633ea56278eb0df84b00c35b71bda47ce303fe
siyuan0322 <siyuan0322@gmail.com>
2021-12-22 20:04:54 +0800

    Make the definition of src in sssp and bfs accurate

--- python/graphscope/analytical/app/sssp.py
+++ python/graphscope/analytical/app/sssp.py
@@ -29,13 +29,15 @@ __all__ = [
 @project_to_simple
 @not_compatible_for("arrow_property", "dynamic_property")
 def sssp(graph, src=0):
-    """Compute single source shortest path on the `graph`.
+    """Compute single source shortest path length on the `graph`.
 
     Note that SSSP requires an numerical property on the edge.
 
     Args:
         graph (:class:`Graph`): A projected simple graph.
-        src (int, optional): The source vertex. Defaults to 0.
+        src (optional): The source vertex. The type should be consistent
+            with the id type of the `graph`, that is, it's `int` or `str` depending
+            on the `oid_type` is `int64_t` or `string` of the `graph`. Defaults to 0.
 
     Returns:
         :class:`graphscope.framework.context.VertexDataContextDAGNode`:
@@ -62,7 +64,9 @@ def property_sssp(graph, src=0):
 
     Args:
         graph (Graph): a property graph.
-        src (int, optional): the source. Defaults to 0.
+        src (optional): The source vertex. The type should be consistent
+            with the id type of the `graph`, that is, it's `int` or `str` depending
+            on the `oid_type` is `int64_t` or `string` of the `graph`. Defaults to 0.
 
     Returns:
         :class:`graphscope.framework.context.LabeledVertexDataContext`:
