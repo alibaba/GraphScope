68982a466e6608295ebe740d654dff4b2110ccda
shirly121 <zxlmillie@163.com>
2022-01-14 18:14:05 +0800

    release job buffer by value

--- research/query_service/ir/compiler/src/main/java/com/alibaba/graphscope/common/jna/type/FfiJobBuffer.java
+++ research/query_service/ir/compiler/src/main/java/com/alibaba/graphscope/common/jna/type/FfiJobBuffer.java
@@ -24,31 +24,30 @@ import com.sun.jna.Structure;
 import java.io.Closeable;
 
 @Structure.FieldOrder({"buffer", "len"})
-public class FfiJobBuffer extends Structure implements Closeable{
+public class FfiJobBuffer extends Structure {
     public FfiJobBuffer() {
         super(IrTypeMapper.INSTANCE);
     }
 
-    public static class ByValue extends FfiJobBuffer implements Structure.ByValue {
+    public static class ByValue extends FfiJobBuffer implements Structure.ByValue, Closeable {
+        public byte[] getBytes() {
+            if (buffer != null && len > 0) {
+                byte[] bytes = new byte[len];
+                for (int i = 0; i < len; ++i) {
+                    bytes[i] = buffer.getByte(i);
+                }
+                return bytes;
+            }
+            return null;
+        }
+
+        @Override
+        public void close() {
+            setAutoSynch(false);
+            IrCoreLibrary.INSTANCE.destroyJobBuffer(this);
+        }
     }
 
     public Pointer buffer;
     public int len;
-
-    public byte[] getBytes() {
-        if (buffer != null && len > 0) {
-            byte[] bytes = new byte[len];
-            for (int i = 0; i < len; ++i) {
-                bytes[i] = buffer.getByte(i);
-            }
-            return bytes;
-        }
-        return null;
-    }
-
-    @Override
-    public void close() {
-        setAutoSynch(false);
-        IrCoreLibrary.INSTANCE.destroyJobBuffer(this);
-    }
 }
