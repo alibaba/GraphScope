4ee325c1bbe5d27755e9d952116d8495f4090594
shirly121 <zxlmillie@163.com>
2022-02-23 17:58:53 +0800

    adapt ir compiler to groot store

---
+++ interactive_engine/groot-server/src/main/java/com/alibaba/maxgraph/servers/ir/SchemaConfig.java
@@ -0,0 +1,8 @@
+package com.alibaba.maxgraph.servers.ir;
+
+import com.alibaba.maxgraph.common.config.Config;
+
+public class SchemaConfig {
+    public static final Config<String> SCHEMA_PATH =
+            Config.stringConfig("graph.schema", ".");
+}
