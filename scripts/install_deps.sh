45fffe0d1b558a271c0139a2e0898423d0da0bb6
Tao He <sighingnow@gmail.com>
2022-03-01 10:21:09 +0800

    Uninstall binutils using brew, if any.
    Signed-off-by: Tao He <sighingnow@gmail.com>

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -767,8 +767,6 @@ install_dependencies() {
       brew install ${packages_to_install[*]}
     fi
 
-
-
     if [[ "$(uname -m)" == "x86_64" ]]; then
       declare -r homebrew_prefix="/usr/local"
     else
@@ -780,6 +778,9 @@ install_dependencies() {
     export CC=${homebrew_prefix}/opt/llvm/bin/clang
     export CXX=${homebrew_prefix}/opt/llvm/bin/clang++
     export CPPFLAGS=-I${homebrew_prefix}/opt/llvm/include
+
+    # remove binutils installed by brew
+    brew uninstall binutils || true
   fi
 
   log "Installing python packages for vineyard codegen."
