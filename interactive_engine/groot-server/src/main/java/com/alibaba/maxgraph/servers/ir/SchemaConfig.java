63f80abaf4737fb7146d3c0af33a59371473a35c
shirly121 <zxlmillie@163.com>
2022-03-17 17:36:38 +0800

    add ir_core lib to distribution target

--- interactive_engine/groot-server/src/main/java/com/alibaba/maxgraph/servers/ir/SchemaConfig.java
+++
@@ -1,8 +0,0 @@
-package com.alibaba.maxgraph.servers.ir;
-
-import com.alibaba.maxgraph.common.config.Config;
-
-public class SchemaConfig {
-    public static final Config<String> SCHEMA_PATH =
-            Config.stringConfig("graph.schema", "/usr/local/maxgraph/conf/ldbc.schema");
-}
