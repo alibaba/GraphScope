2fdded80cdd03cbfd891e5553d484601befe3ed7
BingqingLyu <lv_bingqing@163.com>
2022-03-21 20:11:25 +0800

    minor: add pegasus back

--- interactive_engine/executor/ffi/src/lib.rs
+++ interactive_engine/executor/ffi/src/lib.rs
@@ -3,6 +3,7 @@ extern crate maxgraph_store;
 extern crate maxgraph_common;
 extern crate maxgraph_server;
 extern crate itertools;
+extern crate pegasus;
 #[macro_use]
 extern crate log;
 extern crate log4rs;
