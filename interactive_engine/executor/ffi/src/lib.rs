d40dcfbcf18af5f1223dd3fbc5238d62f3e6daa4
BingqingLyu <lv_bingqing@163.com>
2022-03-21 19:17:44 +0800

    [GAIA Remove] remove pegasus crate dependency from gie

--- interactive_engine/executor/ffi/src/lib.rs
+++ interactive_engine/executor/ffi/src/lib.rs
@@ -2,15 +2,12 @@ extern crate maxgraph_runtime;
 extern crate maxgraph_store;
 extern crate maxgraph_common;
 extern crate maxgraph_server;
-extern crate gaia_pegasus;
 extern crate itertools;
-extern crate pegasus;
 #[macro_use]
 extern crate log;
 extern crate log4rs;
 extern crate grpcio;
 extern crate lazy_static;
-extern crate pegasus_network;
 
 mod executor;
 mod store;
