1c452a9e0ba3ff062786a1fcca253c345a0f2ff6
acezen <weibin.zen@gmail.com>
2022-05-23 08:57:14 +0800

    Fix

--- python/graphscope/nx/algorithms/tests/builtin/test_voterank.py
+++ python/graphscope/nx/algorithms/tests/builtin/test_voterank.py
@@ -1,12 +1,25 @@
+#
+# This file test_bfs.py is referred and derived from project NetworkX
+#
+# which has the following license:
+#
+# Copyright (C) 2004-2020, NetworkX Developers
+# Aric Hagberg <hagberg@lanl.gov>
+# Dan Schult <dschult@colgate.edu>
+# Pieter Swart <swart@lanl.gov>
+# All rights reserved.
+#
+# This file is part of NetworkX.
+#
+# NetworkX is distributed under a BSD license; see LICENSE.txt for more
+# information.
+#
+
 import os
 
 import pytest
 
 from graphscope import nx
-
-"""
-    Unit tests for voterank.
-"""
 
 
 @pytest.mark.skipif(
