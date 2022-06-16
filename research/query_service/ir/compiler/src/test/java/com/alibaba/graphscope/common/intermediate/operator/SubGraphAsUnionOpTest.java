5061621f19adea9baee2b88dae8f20e4c28f9a8f
shirly121 <zxlmillie@163.com>
2022-06-16 17:37:55 +0800

    [IR Compiler] format java codes

--- research/query_service/ir/compiler/src/test/java/com/alibaba/graphscope/common/intermediate/operator/SubGraphAsUnionOpTest.java
+++ research/query_service/ir/compiler/src/test/java/com/alibaba/graphscope/common/intermediate/operator/SubGraphAsUnionOpTest.java
@@ -17,18 +17,11 @@
 package com.alibaba.graphscope.common.intermediate.operator;
 
 import com.alibaba.graphscope.common.IrPlan;
-import com.alibaba.graphscope.common.intermediate.InterOpCollection;
-import com.alibaba.graphscope.common.store.IrMeta;
-import com.alibaba.graphscope.common.utils.FileUtils;
-import com.alibaba.graphscope.gremlin.InterOpCollectionBuilder;
-import com.alibaba.graphscope.gremlin.plugin.processor.IrStandardOpProcessor;
 
-import org.apache.tinkerpop.gremlin.process.traversal.Traversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerFactory;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class SubGraphAsUnionOpTest {
@@ -39,13 +32,13 @@ public class SubGraphAsUnionOpTest {
     // g.E().subgraph()
     @Test
     public void subgraphOpTest() throws Exception {
-//        Traversal traversal = g.E().subgraph("graph_1");
-//        IrStandardOpProcessor.applyStrategies(traversal);
-//        InterOpCollection ops = (new InterOpCollectionBuilder(traversal)).build();
-//        InterOpCollection.process(ops);
-//        irPlan = new IrPlan(new IrMeta(""), ops);
-//        String actual = irPlan.getPlanAsJson();
-//        Assert.assertEquals(FileUtils.readJsonFromResource("subgraph.json"), actual);
+        //        Traversal traversal = g.E().subgraph("graph_1");
+        //        IrStandardOpProcessor.applyStrategies(traversal);
+        //        InterOpCollection ops = (new InterOpCollectionBuilder(traversal)).build();
+        //        InterOpCollection.process(ops);
+        //        irPlan = new IrPlan(new IrMeta(""), ops);
+        //        String actual = irPlan.getPlanAsJson();
+        //        Assert.assertEquals(FileUtils.readJsonFromResource("subgraph.json"), actual);
     }
 
     @After
