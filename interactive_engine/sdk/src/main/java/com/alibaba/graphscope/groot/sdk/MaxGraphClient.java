39f4b1c0665a36179c3bd0a0a3cb0f2060932777
siyuan0322 <siyuan0322@gmail.com>
2022-06-17 14:04:07 +0800

    fix bugs

--- interactive_engine/sdk/src/main/java/com/alibaba/graphscope/groot/sdk/MaxGraphClient.java
+++ interactive_engine/sdk/src/main/java/com/alibaba/graphscope/groot/sdk/MaxGraphClient.java
@@ -78,6 +78,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class MaxGraphClient implements Closeable {
