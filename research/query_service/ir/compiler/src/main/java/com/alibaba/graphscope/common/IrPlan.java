34425e81b83060b796539a05239908ff04a5ef41
shirly121 <zxlmillie@163.com>
2022-07-01 16:28:34 +0800

    [IR Compiler] format java codes

--- research/query_service/ir/compiler/src/main/java/com/alibaba/graphscope/common/IrPlan.java
+++ research/query_service/ir/compiler/src/main/java/com/alibaba/graphscope/common/IrPlan.java
@@ -620,7 +620,8 @@ public class IrPlan implements Closeable {
         if (ptrPlan != null) {
             FfiError e = irCoreLib.printPlanAsJson(ptrPlan);
             if (e.code != ResultCode.Success) {
-                throw new InterOpIllegalArgException(InterOpBase.class, "printPlanAsJson", "code is " + e.code);
+                throw new InterOpIllegalArgException(
+                        InterOpBase.class, "printPlanAsJson", "code is " + e.code);
             }
             json = e.msg;
         }
