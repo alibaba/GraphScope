9c656fa5220155dde667b7f81e2ebc0cbfcb85b1
BingqingLyu <lv_bingqing@163.com>
2022-08-11 14:01:03 +0800

    [GAIA Runtime] Use `hashbrown` for optimization (#1952)
    * try hashbrown
    
    * fix ci tests
    
    * replace hashbrown with ahash

--- interactive_engine/executor/ir/graph_proxy/src/adapters/exp_store/read_graph.rs
+++ interactive_engine/executor/ir/graph_proxy/src/adapters/exp_store/read_graph.rs
@@ -13,7 +13,7 @@
 //! See the License for the specific language governing permissions and
 //! limitations under the License.
 
-use std::collections::HashMap;
+use ahash::{HashMap, HashMapExt};
 use std::fmt;
 use std::path::Path;
 use std::sync::atomic::{AtomicPtr, Ordering};
