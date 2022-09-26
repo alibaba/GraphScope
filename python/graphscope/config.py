f019af09b69095cb8579b05e07d9bb048ce3d6bf
lidongze0629 <9546726@qq.com>
2022-09-26 14:46:51 +0800

    Change nightly image tag to 'major.minor.alpha_patch'

--- python/graphscope/config.py
+++ python/graphscope/config.py
@@ -38,9 +38,7 @@ class GSConfig(object):
     # image
     k8s_etcd_image = "quay.io/coreos/etcd:v3.4.13"
     k8s_gs_image = (
-        "registry.cn-hongkong.aliyuncs.com/graphscope/graphscope:nightly"
-        if __is_prerelease__
-        else f"registry.cn-hongkong.aliyuncs.com/graphscope/graphscope:{__version__}"
+        f"registry.cn-hongkong.aliyuncs.com/graphscope/graphscope:{__version__}"
     )
 
     # image pull configuration
@@ -107,9 +105,7 @@ class GSConfig(object):
     # Demo dataset related
     mount_dataset = None
     k8s_dataset_image = (
-        "registry.cn-hongkong.aliyuncs.com/graphscope/dataset:nightly"
-        if __is_prerelease__
-        else f"registry.cn-hongkong.aliyuncs.com/graphscope/dataset:{__version__}"
+        f"registry.cn-hongkong.aliyuncs.com/graphscope/dataset:{__version__}"
     )
 
     # download_retries
