27d3c80d0683184cc3e0db55be4a120cc2977afb
Tao He <sighingnow@gmail.com>
2022-12-01 22:31:17 +0800

    Fixes the required version of patchelf
    Signed-off-by: Tao He <sighingnow@gmail.com>

--- k8s/build_scripts/build_patchelf.sh
+++ k8s/build_scripts/build_patchelf.sh
@@ -3,8 +3,11 @@ set -euxo pipefail
 
 echo "Working directory is ${WORKDIR:="/tmp"}"
 
+# use specified version of patchelf, as we found issues on AliLinux 2
+# with newer version.
+#
 cd ${WORKDIR} && \
-    git clone --depth=1 https://github.com/NixOS/patchelf.git && \
+    git clone --depth=1 -b ec72eeb4ddba65abea31719ed84e41760bcb993a https://github.com/NixOS/patchelf.git && \
     cd patchelf && \
     ./bootstrap.sh && \
     ./configure && \
