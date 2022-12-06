3dcc3ba94fb2662690768e11c45e7fe6ae827b4c
Tao He <sighingnow@gmail.com>
2022-12-06 12:02:52 +0800

    Fixes
    Signed-off-by: Tao He <sighingnow@gmail.com>

--- analytical_engine/core/utils/transform_utils.h
+++ analytical_engine/core/utils/transform_utils.h
@@ -1149,7 +1149,7 @@ class TransformUtils<
           std::make_shared<vineyard::TensorBuilder<std::string>>(client, shape,
                                                                  part_idx);
       for (size_t i = 0; i < vertices.size(); i++) {
-        tensor_builder->Append(frag_.GetId(vertices[i]));
+        tensor_builder->Append(frag_.GetId(vertices[i]).GetString());
       }
       return std::dynamic_pointer_cast<vineyard::ITensorBuilder>(
           tensor_builder);
