ead8682ce79fcad41cd483c93e736ef334f118ee
nengli.ln <nengli.ln@alibaba-inc.com>
2022-12-26 09:57:26 +0800

    fix operator blocking

--- interactive_engine/executor/engine/pegasus/pegasus/src/communication/decorator/exchange.rs
+++ interactive_engine/executor/engine/pegasus/pegasus/src/communication/decorator/exchange.rs
@@ -176,7 +176,7 @@ impl<D: Data> ExchangeByDataPush<D> {
 
     fn update_end(
         &mut self, target: Option<usize>, end: &EndOfScope,
-    ) -> impl Iterator<Item = (u64, u64, DynPeers)> {
+    ) -> impl Iterator<Item=(u64, u64, DynPeers)> {
         let mut push_stat = Vec::with_capacity(self.pushes.len());
         for (index, p) in self.pushes.iter().enumerate() {
             let mut pushes = p.get_push_count(&end.tag).unwrap_or(0) as u64;
@@ -245,17 +245,15 @@ impl<D: Data> ExchangeByDataPush<D> {
         }
 
         if has_block {
-            if !batch.is_empty() {
-                trace_worker!(
+            trace_worker!(
                     "output[{:?}] blocking on push batch(len={}) of {:?} ;",
                     self.port,
                     batch.len(),
                     batch.tag
                 );
-                self.blocks
-                    .get_mut_or_insert(&batch.tag)
-                    .push_back(BlockEntry::Batch(batch));
-            }
+            self.blocks
+                .get_mut_or_insert(&batch.tag)
+                .push_back(BlockEntry::Batch(batch));
             would_block!("no buffer available in exchange;")
         } else {
             if let Some(end) = batch.take_end() {
@@ -590,7 +588,7 @@ impl<D: Data> ExchangeByBatchPush<D> {
 
     fn update_end(
         &mut self, target: Option<usize>, end: &EndOfScope,
-    ) -> impl Iterator<Item = (u64, u64, DynPeers)> {
+    ) -> impl Iterator<Item=(u64, u64, DynPeers)> {
         let mut push_stat = Vec::with_capacity(self.pushes.len());
         for (index, p) in self.pushes.iter().enumerate() {
             let mut pushes = p.get_push_count(&end.tag).unwrap_or(0) as u64;
