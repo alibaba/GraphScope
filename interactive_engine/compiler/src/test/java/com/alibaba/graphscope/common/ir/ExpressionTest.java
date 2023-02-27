ecfe3777f17ad336f995d4274949c7d0bf8ae8eb
shirly121 <yihe.zxl@alibaba-inc.com>
2023-02-27 12:20:27 +0800

    [GAIA Compiler] update according to code refactor

--- interactive_engine/compiler/src/test/java/com/alibaba/graphscope/common/ir/ExpressionTest.java
+++ interactive_engine/compiler/src/test/java/com/alibaba/graphscope/common/ir/ExpressionTest.java
@@ -67,7 +67,7 @@ public class ExpressionTest {
         } catch (IllegalArgumentException e) {
             return;
         }
-        Assert.fail("tag 'a' should not exist");
+        Assert.fail("tag 'a' does not exist");
     }
 
     @Test
@@ -113,7 +113,7 @@ public class ExpressionTest {
         Assert.assertEquals("=(a.age, 'X')", equal.toString());
     }
 
-    // a.age + 10 > 30
+    // a.age + 10 == 30
     @Test
     public void equal_plus_test() {
         RexNode var = builder.source(mockSourceConfig("a")).variable("a", "age");
