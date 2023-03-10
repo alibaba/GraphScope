6bc9cc847726c36e7dd0b7205b3fac9a62f870eb
nengli.ln <nengli.ln@alibaba-inc.com>
2023-03-10 14:46:30 +0800

    fix warning

--- interactive_engine/executor/ir/graph_proxy/src/adapters/csr_store/read_graph.rs
+++ interactive_engine/executor/ir/graph_proxy/src/adapters/csr_store/read_graph.rs
@@ -13,7 +13,6 @@
 //! See the License for the specific language governing permissions and
 //! limitations under the License.
 
-use std::cmp::max_by_key;
 use std::convert::TryFrom;
 use std::fmt;
 use std::sync::atomic::{AtomicPtr, Ordering};
