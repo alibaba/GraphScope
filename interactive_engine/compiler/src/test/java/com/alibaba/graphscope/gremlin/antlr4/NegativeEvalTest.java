ff287e4747e20222c2d368862c2515167becad8a
Xiaoli Zhou <yihe.zxl@alibaba-inc.com>
2023-03-19 16:26:58 +0800

    [GAIA Compiler] Refine error handling in antlr grammar (#2530)
    <!--
    Thanks for your contribution! please review
    https://github.com/alibaba/GraphScope/blob/main/CONTRIBUTING.md before
    opening an issue.
    -->
    
    ## What do these changes do?
    1. fix bugs when handling exceptions from antlr, to print more
    meaningful messages (including position with the error text), i.e.
    `g.Vd` -> syntax error occurs at [line: 1, column: 3]; msg is: [token
    recognition error at: 'd']
    `g.V().order().by('a', 'b')` -> syntax error occurs at [line: 1, column:
    22]; msg is: [mismatched input '"b"' expecting {'asc', 'Order.asc',
    'desc', 'Order.desc', 'shuffle', 'Order.shuffle'}]
    3. throw exceptions when composition of operators is invalid, i.e. 
    `g.V().subgraph('X')` -> edge induced subgraph should follow an edge
    expand operator [inE, outE, bothE]
    `g.V().endV()` -> endV should follow a path expand operator
    [out('$1..$2'), in('$1..$2'), both('$1..$2')]
    6. refine parameters in `hasId` which should be a non-empty integer list
    
    <!-- Please give a short brief about these changes. -->
    
    ## Related issue number
    
    <!-- Are there any issues opened that will be resolved by merging this
    change? -->
    
    Related Issue: #2531
    
    ---------
    
    Co-authored-by: Longbin Lai <longbin.lailb@alibaba-inc.com>

--- interactive_engine/compiler/src/test/java/com/alibaba/graphscope/gremlin/antlr4/NegativeEvalTest.java
+++ interactive_engine/compiler/src/test/java/com/alibaba/graphscope/gremlin/antlr4/NegativeEvalTest.java
@@ -20,6 +20,7 @@ import com.alibaba.graphscope.gremlin.exception.InvalidGremlinScriptException;
 import com.alibaba.graphscope.gremlin.plugin.script.AntlrToJavaScriptEngine;
 import com.alibaba.graphscope.gremlin.plugin.traversal.IrCustomizedTraversalSource;
 
+import org.antlr.v4.runtime.misc.ParseCancellationException;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerFactory;
@@ -51,8 +52,12 @@ public class NegativeEvalTest {
     public void g111_test() {
         try {
             scriptEngine.eval("g111()", context);
-        } catch (InvalidGremlinScriptException e) {
+        } catch (ParseCancellationException e) {
             // expected error
+            Assert.assertEquals(
+                    "syntax error occurs at [line: 1, column: 1]; msg is: [no viable alternative at"
+                            + " input 'g111']",
+                    e.getMessage());
             return;
         }
         Assert.fail();
@@ -62,8 +67,12 @@ public class NegativeEvalTest {
     public void g_V111_test() {
         try {
             scriptEngine.eval("g.V111()", context);
-        } catch (InvalidGremlinScriptException e) {
+        } catch (ParseCancellationException e) {
             // expected error
+            Assert.assertEquals(
+                    "syntax error occurs at [line: 1, column: 3]; msg is: [no viable alternative at"
+                            + " input 'g.V111']",
+                    e.getMessage());
             return;
         }
         Assert.fail();
@@ -73,8 +82,12 @@ public class NegativeEvalTest {
     public void g_V_outX_test() {
         try {
             scriptEngine.eval("g.V().outX()", context);
-        } catch (InvalidGremlinScriptException e) {
+        } catch (ParseCancellationException e) {
             // expected error
+            Assert.assertEquals(
+                    "syntax error occurs at [line: 1, column: 9]; msg is: [token recognition error"
+                            + " at: 'X']",
+                    e.getMessage());
             return;
         }
         Assert.fail();
@@ -84,8 +97,12 @@ public class NegativeEvalTest {
     public void g_V_out_id_test() {
         try {
             scriptEngine.eval("g.V().out(1)", context);
-        } catch (InvalidGremlinScriptException e) {
+        } catch (ParseCancellationException e) {
             // expected error
+            Assert.assertEquals(
+                    "syntax error occurs at [line: 1, column: 10]; msg is: [no viable alternative"
+                            + " at input '1']",
+                    e.getMessage());
             return;
         }
         Assert.fail();
@@ -95,8 +112,12 @@ public class NegativeEvalTest {
     public void g_V_out_limit_str_test() {
         try {
             scriptEngine.eval("g.V().out().limit('xxx')", context);
-        } catch (InvalidGremlinScriptException e) {
+        } catch (ParseCancellationException e) {
             // expected error
+            Assert.assertEquals(
+                    "syntax error occurs at [line: 1, column: 18]; msg is: [mismatched input"
+                            + " ''xxx'' expecting IntegerLiteral]",
+                    e.getMessage());
             return;
         }
         Assert.fail();
@@ -106,8 +127,12 @@ public class NegativeEvalTest {
     public void g_V_has_key_id_test() {
         try {
             scriptEngine.eval("g.V().has(1, 1)", context);
-        } catch (InvalidGremlinScriptException e) {
+        } catch (ParseCancellationException e) {
             // expected error
+            Assert.assertEquals(
+                    "syntax error occurs at [line: 1, column: 10]; msg is: [no viable alternative"
+                            + " at input 'has(1']",
+                    e.getMessage());
             return;
         }
         Assert.fail();
@@ -117,8 +142,12 @@ public class NegativeEvalTest {
     public void g_V_valueMap_int_test() {
         try {
             scriptEngine.eval("g.V().valueMap(1)", context);
-        } catch (InvalidGremlinScriptException e) {
+        } catch (ParseCancellationException e) {
             // expected error
+            Assert.assertEquals(
+                    "syntax error occurs at [line: 1, column: 15]; msg is: [no viable alternative"
+                            + " at input '1']",
+                    e.getMessage());
             return;
         }
         Assert.fail();
@@ -128,8 +157,12 @@ public class NegativeEvalTest {
     public void g_V_select_none_test() {
         try {
             scriptEngine.eval("g.V().select()", context);
-        } catch (InvalidGremlinScriptException e) {
+        } catch (ParseCancellationException e) {
             // expected error
+            Assert.assertEquals(
+                    "syntax error occurs at [line: 1, column: 13]; msg is: [no viable alternative"
+                            + " at input 'select()']",
+                    e.getMessage());
             return;
         }
         Assert.fail();
@@ -139,8 +172,12 @@ public class NegativeEvalTest {
     public void g_V_select_by_keys_test() {
         try {
             scriptEngine.eval("g.V().select().by('name', 'id')", context);
-        } catch (InvalidGremlinScriptException e) {
+        } catch (ParseCancellationException e) {
             // expected error
+            Assert.assertEquals(
+                    "syntax error occurs at [line: 1, column: 13]; msg is: [no viable alternative"
+                            + " at input 'select()']",
+                    e.getMessage());
             return;
         }
         Assert.fail();
@@ -150,8 +187,12 @@ public class NegativeEvalTest {
     public void g_V_order_by_orders_test() {
         try {
             scriptEngine.eval("g.V().order().by(asc, desc)", context);
-        } catch (InvalidGremlinScriptException e) {
+        } catch (ParseCancellationException e) {
             // expected error
+            Assert.assertEquals(
+                    "syntax error occurs at [line: 1, column: 20]; msg is: [mismatched input ','"
+                            + " expecting ')']",
+                    e.getMessage());
             return;
         }
         Assert.fail();
@@ -161,8 +202,13 @@ public class NegativeEvalTest {
     public void g_V_order_by_keys_test() {
         try {
             scriptEngine.eval("g.V().order().by('name', 'id')", context);
-        } catch (InvalidGremlinScriptException e) {
+        } catch (ParseCancellationException e) {
             // expected error
+            Assert.assertEquals(
+                    "syntax error occurs at [line: 1, column: 25]; msg is: [mismatched input ''id''"
+                            + " expecting {'asc', 'Order.asc', 'desc', 'Order.desc', 'shuffle',"
+                            + " 'Order.shuffle'}]",
+                    e.getMessage());
             return;
         }
         Assert.fail();
@@ -172,8 +218,12 @@ public class NegativeEvalTest {
     public void g_V_as_invalid_test() {
         try {
             scriptEngine.eval("g.V().as('a', 'b')", context);
-        } catch (InvalidGremlinScriptException e) {
+        } catch (ParseCancellationException e) {
             // expected error
+            Assert.assertEquals(
+                    "syntax error occurs at [line: 1, column: 12]; msg is: [mismatched input ','"
+                            + " expecting ')']",
+                    e.getMessage());
             return;
         }
         Assert.fail();
@@ -183,8 +233,12 @@ public class NegativeEvalTest {
     public void g_V_str_id_test() {
         try {
             scriptEngine.eval("g.V(\"1\")", context);
-        } catch (InvalidGremlinScriptException e) {
+        } catch (ParseCancellationException e) {
             // expected error
+            Assert.assertEquals(
+                    "syntax error occurs at [line: 1, column: 4]; msg is: [no viable alternative at"
+                            + " input 'g.V(\"1\"']",
+                    e.getMessage());
             return;
         }
         Assert.fail();
@@ -194,8 +248,12 @@ public class NegativeEvalTest {
     public void g_E_str_id_test() {
         try {
             scriptEngine.eval("g.E(\"1\")", context);
-        } catch (InvalidGremlinScriptException e) {
+        } catch (ParseCancellationException e) {
             // expected error
+            Assert.assertEquals(
+                    "syntax error occurs at [line: 1, column: 4]; msg is: [no viable alternative at"
+                            + " input 'g.E(\"1\"']",
+                    e.getMessage());
             return;
         }
         Assert.fail();
@@ -205,8 +263,12 @@ public class NegativeEvalTest {
     public void g_V_hasId_str_id_test() {
         try {
             scriptEngine.eval("g.V().hasId(\"1\")", context);
-        } catch (InvalidGremlinScriptException e) {
+        } catch (ParseCancellationException e) {
             // expected error
+            Assert.assertEquals(
+                    "syntax error occurs at [line: 1, column: 12]; msg is: [mismatched input"
+                            + " '\"1\"' expecting {IntegerLiteral, '['}]",
+                    e.getMessage());
             return;
         }
         Assert.fail();
@@ -217,8 +279,44 @@ public class NegativeEvalTest {
     public void g_V_group_by_name_age_test() {
         try {
             scriptEngine.eval("g.V().group().by('name', 'age')", context);
+        } catch (ParseCancellationException e) {
+            // expected error
+            Assert.assertEquals(
+                    "syntax error occurs at [line: 1, column: 23]; msg is: [no viable alternative"
+                            + " at input 'group().by('name',']",
+                    e.getMessage());
+            return;
+        }
+        Assert.fail();
+    }
+
+    // invalid composition of operators
+    @Test
+    public void g_V_subgraph() {
+        try {
+            scriptEngine.eval("g.V().subgraph('X')", context);
         } catch (InvalidGremlinScriptException e) {
             // expected error
+            Assert.assertEquals(
+                    "edge induced subgraph should follow an edge output operator [E, inE, outE,"
+                            + " bothE]",
+                    e.getMessage());
+            return;
+        }
+        Assert.fail();
+    }
+
+    // invalid composition of operators
+    @Test
+    public void g_V_out_endV() {
+        try {
+            scriptEngine.eval("g.V().out().endV()", context);
+        } catch (InvalidGremlinScriptException e) {
+            // expected error
+            Assert.assertEquals(
+                    "endV should follow a path expand operator [out('$1..$2'), in('$1..$2'),"
+                            + " both('$1..$2')]",
+                    e.getMessage());
             return;
         }
         Assert.fail();
