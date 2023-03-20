d83d7ea4bd174d8fd8452f87d0b30c330e8db053
nengli.ln <nengli.ln@alibaba-inc.com>
2023-03-20 12:12:07 +0800

    add test graph for mcsr

--- interactive_engine/compiler/src/main/java/com/alibaba/graphscope/gremlin/service/GraphServiceMain.java
+++ interactive_engine/compiler/src/main/java/com/alibaba/graphscope/gremlin/service/GraphServiceMain.java
@@ -1,11 +1,6 @@
 /*
  * Copyright 2020 Alibaba Group Holding Limited.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
+
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,17 +21,30 @@ import com.alibaba.graphscope.common.store.IrMetaFetcher;
 import com.alibaba.graphscope.gremlin.integration.result.TestGraphFactory;
 
 public class GraphServiceMain {
+    public final static String GRAPH_STORE_KEY = "graph.store";
+    public final static String EXPERIMENTAL = "exp";
+    public final static String CSR = "csr";
+
     public static void main(String[] args) throws Exception {
         Configs configs = new Configs("conf/ir.compiler.properties", FileLoadType.RELATIVE_PATH);
         IrMetaFetcher irMetaFetcher = new ExperimentalMetaFetcher(configs);
         RpcChannelFetcher fetcher = new HostsChannelFetcher(configs);
 
         IrGremlinServer server = new IrGremlinServer();
-        server.start(
-                configs,
-                irMetaFetcher,
-                fetcher,
-                new IrMetaQueryCallback(irMetaFetcher),
-                TestGraphFactory.EXPERIMENTAL);
+        if (configs.get(GRAPH_STORE_KEY).equals(CSR)) {
+            server.start(
+                    configs,
+                    irMetaFetcher,
+                    fetcher,
+                    new IrMetaQueryCallback(irMetaFetcher),
+                    TestGraphFactory.MCSR);
+        } else {
+            server.start(
+                    configs,
+                    irMetaFetcher,
+                    fetcher,
+                    new IrMetaQueryCallback(irMetaFetcher),
+                    TestGraphFactory.EXPERIMENTAL);
+       }
     }
 }
