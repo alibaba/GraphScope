513e3b532159588ca8984b23fe3d7663eab7319a
luoxiaojian <359651204@qq.com>
2023-05-10 16:57:38 +0000

    update doc

--- flex/docs/html/search/functions_10.js
+++ flex/docs/html/search/functions_10.js
@@ -8,7 +8,7 @@ var searchData=
   ['release_5fread_5ftimestamp_1022',['release_read_timestamp',['../classgs_1_1VersionManager.html#a628a5d2ee3065a7afb7e1ac6a2f0f8f6',1,'gs::VersionManager']]],
   ['release_5fupdate_5ftimestamp_1023',['release_update_timestamp',['../classgs_1_1VersionManager.html#aa58321bdb12fa87a947a8a521dd6fd15',1,'gs::VersionManager']]],
   ['reserve_1024',['reserve',['../classgs_1_1ArenaAllocator.html#a0ad54d6f9f14c028d47dc2ac1acc30a0',1,'gs::ArenaAllocator']]],
-  ['reset_1025',['reset',['../structgs_1_1Date.html#a0308448c7ca411709e7e5dfe4e0f1979',1,'gs::Date']]],
+  ['reset_1025',['reset',['../classgs_1_1Decoder.html#a51aca9a3ffbc685c80f885190d4e7e24',1,'gs::Decoder::reset()'],['../structgs_1_1Date.html#a0308448c7ca411709e7e5dfe4e0f1979',1,'gs::Date::reset()']]],
   ['reset_5fheader_1026',['reset_header',['../classgs_1_1Table.html#a436714df0b244ec79cdcc8a72d03752e',1,'gs::Table']]],
   ['reset_5fto_5fempty_5fstate_1027',['reset_to_empty_state',['../classgs_1_1IdIndexer.html#a9741ce73de85e27fb318e5c8d285f251',1,'gs::IdIndexer']]],
   ['resize_1028',['resize',['../classgs_1_1mmap__array.html#acd2e1c69c5bc661c79c956631dcc4bd8',1,'gs::mmap_array::resize()'],['../classgs_1_1mmap__array_3_01std_1_1string__view_01_4.html#afe72fb3a771af1fb6bc6382837c863b7',1,'gs::mmap_array&lt; std::string_view &gt;::resize()']]],
