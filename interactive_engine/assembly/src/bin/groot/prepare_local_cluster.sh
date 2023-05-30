6c4f762dac0c9f3b0f848b1968eba0361aecf125
siyuan0322 <siyuanzhang.zsy@alibaba-inc.com>
2023-05-30 12:43:54 +0800

    update

--- interactive_engine/assembly/src/bin/groot/prepare_local_cluster.sh
+++ interactive_engine/assembly/src/bin/groot/prepare_local_cluster.sh
@@ -7,6 +7,9 @@ sed "s@LOG4RS_CONFIG@${GROOT_DIR}/conf/log4rs.yml@" ${GROOT_DIR}/conf/config.tem
 
 GROOT_CONF_FILE=${CONFIG_FILE} ${GROOT_HOME}/bin/store_ctl.sh start
 
+# Start container with port mapping
+# docker run -p 12312:12312 -p 55556:55556 graphscope/graphscope-store:latest
+
 # pip3 install graphscope_client --user
 # export NODE_IP="127.0.0.1"
 # export GREMLIN_PORT="12312"
