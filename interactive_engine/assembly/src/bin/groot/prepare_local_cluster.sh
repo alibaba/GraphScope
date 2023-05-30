d195bafd662ef3e9747859e5c3f2bcfb24f107e1
siyuan0322 <siyuan0322@gmail.com>
2023-05-30 12:41:51 +0800

    put all jars into assembly

--- interactive_engine/assembly/src/bin/groot/prepare_local_cluster.sh
+++ interactive_engine/assembly/src/bin/groot/prepare_local_cluster.sh
@@ -3,13 +3,9 @@
 declare -r GROOT_DIR=${GROOT_HOME:-/usr/local/groot}
 declare -r CONFIG_FILE="/tmp/groot.config"
 
-# cp ~/.m2/repository/org/apache/curator/curator-test/5.4.0/curator-test-5.4.0.jar ${SOURCE_DIR}/lib/
-# cp ~/.m2/repository/org/scala-lang/scala-library/2.13.9/scala-library-2.13.9.jar ${SOURCE_DIR}/lib
-
-sed "s@LOG4RS_CONFIG@${GROOT_DIR}/conf/log4rs.yml@" ${GROOT_DIR}/conf/config.template ${CONFIG_FILE}
+sed "s@LOG4RS_CONFIG@${GROOT_DIR}/conf/log4rs.yml@" ${GROOT_DIR}/conf/config.template >${CONFIG_FILE}
 
 GROOT_CONF_FILE=${CONFIG_FILE} ${GROOT_HOME}/bin/store_ctl.sh start
-
 
 # pip3 install graphscope_client --user
 # export NODE_IP="127.0.0.1"
--- interactive_engine/assembly/src/bin/groot/store_ctl.sh
+++ interactive_engine/assembly/src/bin/groot/store_ctl.sh
@@ -5,7 +5,7 @@
 set -xeo pipefail
 
 usage() {
-cat <<END
+	cat <<END
   A script to launch groot service.
 
   Usage: store_ctl.sh [options] [parameters]
@@ -22,61 +22,64 @@ END
 
 # a function to setup common variable and env
 _setup_env() {
-  declare script="$0"
-  if [ -z "${GROOT_HOME}" ]; then
-    # set GROOT_HOME base location of the script
-    while [ -h "${script}" ] ; do
-      ls=$(ls -ld "${script}")
-      # Drop everything prior to ->
-      link=$(expr "${ls}" : '.*-> \(.*\)$')
-      if expr "${link}" : '/.*' > /dev/null; then
-        script="${link}"
-      else
-        script="$(dirname ${script})/${link}"
-      fi
-      done
-      GROOT_HOME=$(dirname "${script}")
-      GROOT_HOME=$(cd "${GROOT_HOME}"; pwd)
-      readonly GROOT_HOME=$(dirname ${GROOT_HOME})
-  fi
+	declare script="$0"
+	if [ -z "${GROOT_HOME}" ]; then
+		# set GROOT_HOME base location of the script
+		while [ -h "${script}" ]; do
+			ls=$(ls -ld "${script}")
+			# Drop everything prior to ->
+			link=$(expr "${ls}" : '.*-> \(.*\)$')
+			if expr "${link}" : '/.*' >/dev/null; then
+				script="${link}"
+			else
+				script="$(dirname ${script})/${link}"
+			fi
+		done
+		GROOT_HOME=$(dirname "${script}")
+		GROOT_HOME=$(
+			cd "${GROOT_HOME}"
+			pwd
+		)
+		readonly GROOT_HOME=$(dirname ${GROOT_HOME})
+	fi
 
-  if [ -z "${GROOT_CONF_DIR}" ]; then
-    readonly GROOT_CONF_DIR="${GROOT_HOME}/conf"
-  fi
+	if [ -z "${GROOT_CONF_DIR}" ]; then
+		readonly GROOT_CONF_DIR="${GROOT_HOME}/conf"
+	fi
 
-  if [ -z "${GROOT_LOGBACK_FILE}" ]; then
-    readonly GROOT_LOGBACK_FILE="${GROOT_CONF_DIR}/logback.xml"
-  fi
+	if [ -z "${GROOT_LOGBACK_FILE}" ]; then
+		readonly GROOT_LOGBACK_FILE="${GROOT_CONF_DIR}/logback.xml"
+	fi
 
-  if [ -z "${GROOT_CONF_FILE}" ]; then
-    readonly GROOT_CONF_FILE="${GROOT_CONF_DIR}/groot.config"
-  fi
+	if [ -z "${GROOT_CONF_FILE}" ]; then
+		readonly GROOT_CONF_FILE="${GROOT_CONF_DIR}/groot.config"
+	fi
 
-  if [ -z "${LOG_NAME}" ]; then
-    readonly LOG_NAME="groot"
-  fi
+	if [ -z "${LOG_NAME}" ]; then
+		readonly LOG_NAME="groot"
+	fi
 
-  export LD_LIBRARY_PATH=${GROOT_HOME}/native:${GROOT_HOME}/native/lib:${LD_LIBRARY_PATH}:/usr/local/lib
+	export LD_LIBRARY_PATH=${GROOT_HOME}/native:${GROOT_HOME}/native/lib:${LD_LIBRARY_PATH}:/usr/local/lib
 
-  if [ -z "${LOG_DIR}" ]; then
-    GS_LOG="/var/log/graphscope"
-    if [[ ! -d "${GS_LOG}" || ! -w "${GS_LOG}" ]]; then
-      # /var/log/graphscope is not existed/writable, switch to ${HOME}/.local/log/graphscope
-      GS_LOG=${HOME}/.local/log/graphscope
-    fi
-    readonly GS_LOG
-    export LOG_DIR=${GS_LOG}
-  fi
+	if [ -z "${LOG_DIR}" ]; then
+		GS_LOG="/var/log/graphscope"
+		if [[ ! -d "${GS_LOG}" || ! -w "${GS_LOG}" ]]; then
+			# /var/log/graphscope is not existed/writable, switch to ${HOME}/.local/log/graphscope
+			GS_LOG=${HOME}/.local/log/graphscope
+		fi
+		readonly GS_LOG
+		export LOG_DIR=${GS_LOG}
+	fi
 
-  mkdir -p ${LOG_DIR}
+	mkdir -p ${LOG_DIR}
 
-  libpath="$(echo "${GROOT_HOME}"/lib/*.jar | tr ' ' ':')"
+	libpath="$(echo "${GROOT_HOME}"/lib/*.jar | tr ' ' ':')"
 }
 
 # start groot server
 start_server() {
-  _setup_env
-  java_opt="-server
+	_setup_env
+	java_opt="-server
             -Djava.awt.headless=true
             -Dfile.encoding=UTF-8
             -Dsun.jnu.encoding=UTF-8
@@ -98,25 +101,34 @@ start_server() {
             -XX:NumberOfGCLogFiles=32
             -XX:GCLogFileSize=64m"
 
-  java "${java_opt}" \
-      -Dlogback.configurationFile="${GROOT_LOGBACK_FILE}" \
-      -Dconfig.file="${GROOT_CONF_FILE}" \
-      -Dlog.dir="${LOG_DIR}" \
-      -Dlog.name="${LOG_NAME}" \
-      -cp "${libpath}" com.alibaba.graphscope.groot.servers.GrootGraph \
-      "$@" > >(tee -a "${LOG_DIR}/${LOG_NAME}.out") 2> >(tee -a "${LOG_DIR}/${LOG_NAME}.err" >&2)
+	java ${java_opt} \
+		-Dlogback.configurationFile="${GROOT_LOGBACK_FILE}" \
+		-Dconfig.file="${GROOT_CONF_FILE}" \
+		-Dlog.dir="${LOG_DIR}" \
+		-Dlog.name="${LOG_NAME}" \
+		-cp "${libpath}" com.alibaba.graphscope.groot.servers.GrootGraph \
+		"$@" > >(tee -a "${LOG_DIR}/${LOG_NAME}.out") 2> >(tee -a "${LOG_DIR}/${LOG_NAME}.err" >&2)
 }
 
 # parse argv
 while test $# -ne 0; do
-  arg=$1; shift
-  case $arg in
-    -h|--help) usage; exit ;;
-    start) start_server "$@"; exit;;
-    *)
-      echo "unrecognized option or command '${arg}'"
-      usage; exit;;
-  esac
+	arg=$1
+	shift
+	case $arg in
+	-h | --help)
+		usage
+		exit
+		;;
+	start)
+		start_server "$@"
+		exit
+		;;
+	*)
+		echo "unrecognized option or command '${arg}'"
+		usage
+		exit
+		;;
+	esac
 done
 
 set +xeo pipefail
