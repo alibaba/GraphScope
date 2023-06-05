alibaba/GraphScope

commit 278a24c795d99bdca61728c889a67f7381c27040

Adapt to latest vineyard for varint encoding in CSR (#2791)
## Related issue number

Part of #2628

Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

.github/workflows/gae.yml
@@ -29,12 +29,12 @@ jobs:
     runs-on: ubuntu-20.04
     if: ${{ github.repository == 'alibaba/GraphScope' }}
     container:
-      image: registry.cn-hongkong.aliyuncs.com/graphscope/graphscope-dev:v0.14.5
+      image: registry.cn-hongkong.aliyuncs.com/graphscope/graphscope-dev:v0.15.0
     steps:
     - uses: actions/checkout@v3
 
     - name: Install latest vineyard
-      if: false
+      if: true
       run: |
         . /home/graphscope/.graphscope_env
         sudo pip3 install libclang

gs
@@ -1035,7 +1035,7 @@ install_vineyard() {
   make -j"${jobs}"
   make install
   strip "${V6D_PREFIX}"/bin/vineyard* "${V6D_PREFIX}"/lib/libvineyard*
-  pip3 install --no-cache -i https://pypi.org/simple "vineyard==${v6d_version}"
+  pip3 install --no-cache -i https://pypi.org/simple -U "vineyard" "vineyard-io"
   cp -rs "${V6D_PREFIX}"/* "${install_prefix}"/
   popd || exit
   popd || exit

k8s/dockerfiles/coordinator.Dockerfile
@@ -54,5 +54,7 @@ RUN sudo chmod a+wrx /tmp
 USER graphscope
 WORKDIR /home/graphscope
 
+ENV PATH ${PATH}:/home/graphscope/.local/bin:/opt/graphscope/bin
+
 COPY --from=builder /home/graphscope/install /opt/graphscope/
 RUN python3 -m pip install --user --no-cache-dir /opt/graphscope/*.whl && sudo rm -rf /opt/graphscope/*.whl

k8s/internal/Makefile
@@ -42,7 +42,7 @@ GRAPHSCOPE_HOME ?= /usr/local
 INSTALL_PREFIX ?= /opt/graphscope
 
 VERSION ?= latest
-VINEYARD_VERSION ?= v0.14.5
+VINEYARD_VERSION ?= v0.15.0
 PROFILE ?= release
 CI ?= false
 
@@ -101,6 +101,21 @@ graphscope-darwin-py3:
 		done
 
 graphscope-manylinux2014-py3-nodocker:
+	cd /tmp && \
+	git clone --single-branch --depth=1 https://github.com/v6d-io/v6d.git /tmp/v6d && \
+	cd /tmp/v6d && \
+	git submodule update --init && \
+	mkdir -p build && cd build && \
+	cmake .. \
+		-DCMAKE_INSTALL_PREFIX=/opt/vineyard \
+		-DCMAKE_PREFIX_PATH=/opt/graphscope \
+		-DOPENSSL_ROOT_DIR=/opt/graphscope \
+		-DUSE_EXTERNAL_ETCD_LIBS=OFF \
+		-DBUILD_SHARED_LIBS=ON \
+		-DBUILD_VINEYARD_TESTS=OFF && \
+	make -j$(nproc) && \
+	sudo make install && \
+	sudo rsync -av /opt/vineyard/ ${GRAPHSCOPE_HOME}/ && \
 	cd $(WORKING_DIR)/../.. && \
 	if [[ "${ARCH}" == "aarch64" ]]; then python3 -m pip install grpcio==1.49.1 --no-binary grpcio; \
 		export AUDITWHEEL_PLAT=manylinux2014_${ARCH}; \

k8s/Makefile
@@ -7,7 +7,7 @@ ifeq ($(REGISTRY),)
 endif
 
 VERSION ?= latest
-VINEYARD_VERSION ?= v0.14.5
+VINEYARD_VERSION ?= v0.15.0
 # This is the version of builder base image in most cases, except for graphscope-dev
 BUILDER_VERSION ?= $(VINEYARD_VERSION)
 # This is the version of runtime base image

k8s/actions-runner-controller/manylinux/Makefile
@@ -12,7 +12,7 @@ TARGETPLATFORM ?= $(shell arch)
 RUNNER_VERSION ?= 2.287.1
 DOCKER_VERSION ?= 20.10.12
 
-VINEYARD_VERSION ?= v0.14.5
+VINEYARD_VERSION ?= v0.15.0
 BUILDER_VERSION ?= $(VINEYARD_VERSION)
 
 # default list of platforms for which multiarch image is built

.dockerignore
@@ -32,7 +32,9 @@ python/*.zip
 # Needed by build analytical image in CI, the COPY should copy the generated proto files
 # **/*.pb.*
 **/*_pb2.py
+**/*_pb2.pyi
 **/*_pb2_grpc.py
+**/*_pb2_grpc.pyi
 
 # Visual Studio Code
 .vscode/

.github/workflows/build-graphscope-wheels-macos.yml
@@ -92,11 +92,12 @@ jobs:
           echo "${version}a${time}" > ${GITHUB_WORKSPACE}/VERSION;
         fi
 
-        python3 -m pip install numpy pandas "grpcio>=1.49" "grpcio-tools>=1.49" wheel
+        python3 -m pip install numpy pandas "grpcio>=1.49" "grpcio-tools>=1.49" "mypy-protobuf>=3.4.0" wheel
 
+        export PATH=$PATH:/Users/runner/Library/Python/3.11/bin
         # build graphscope server wheel
         cd ${GITHUB_WORKSPACE}/k8s/internal
-        sudo -E make graphscope-py3-package GRAPHSCOPE_HOME=/usr/local
+        sudo -E env PATH=$PATH make graphscope-py3-package GRAPHSCOPE_HOME=/usr/local
 
         # package
         cd ${GITHUB_WORKSPACE}
@@ -156,7 +157,7 @@ jobs:
       run: |
         . ~/.graphscope_env
         python3 -m pip install libclang
-        git clone --single-branch -b v0.14.5 --depth=1 https://github.com/v6d-io/v6d.git /tmp/v6d
+        git clone --single-branch -b v0.15.0 --depth=1 https://github.com/v6d-io/v6d.git /tmp/v6d
         cd /tmp/v6d
         git submodule update --init
         cmake .  -DCMAKE_INSTALL_PREFIX=/usr/local \

commit 9efd4f672286de5eb9f3594a5cbc29bec0044e9a

Add local python script path to PATH (#2804)
Fixes the CI failures introduced in
https://github.com/alibaba/GraphScope/pull/2791

---------

Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

.github/workflows/gae.yml
@@ -34,7 +34,7 @@ jobs:
     - uses: actions/checkout@v3
 
     - name: Install latest vineyard
-      if: true
+      if: false
       run: |
         . /home/graphscope/.graphscope_env
         sudo pip3 install libclang

gs
@@ -1030,7 +1030,7 @@ install_vineyard() {
         -DCMAKE_INSTALL_PREFIX="${V6D_PREFIX}" \
         -DBUILD_VINEYARD_TESTS=OFF \
         -DBUILD_SHARED_LIBS=ON \
-        -DBUILD_VINEYARD_PYTHON_BINDINGS=ON  \
+        -DBUILD_VINEYARD_PYTHON_BINDINGS=ON \
         -DBUILD_VINEYARD_GRAPH_WITH_GAR=ON
   make -j"${jobs}"
   make install

k8s/dockerfiles/analytical.Dockerfile
@@ -81,3 +81,5 @@ RUN chmod +x /opt/graphscope/bin/*
 
 USER graphscope
 WORKDIR /home/graphscope
+
+ENV PATH=${PATH}:/home/graphscope/.local/bin

k8s/dockerfiles/coordinator.Dockerfile
@@ -54,7 +54,7 @@ RUN sudo chmod a+wrx /tmp
 USER graphscope
 WORKDIR /home/graphscope
 
-ENV PATH ${PATH}:/home/graphscope/.local/bin:/opt/graphscope/bin
+ENV PATH=${PATH}:/home/graphscope/.local/bin
 
 COPY --from=builder /home/graphscope/install /opt/graphscope/
 RUN python3 -m pip install --user --no-cache-dir /opt/graphscope/*.whl && sudo rm -rf /opt/graphscope/*.whl

k8s/dockerfiles/graphscope-store.Dockerfile
@@ -38,6 +38,8 @@ RUN sudo chmod a+wrx /tmp
 USER graphscope
 WORKDIR /home/graphscope
 
+ENV PATH=${PATH}:/home/graphscope/.local/bin
+
 # init log directory
 RUN sudo mkdir /var/log/graphscope \
   && sudo chown -R $(id -u):$(id -g) /var/log/graphscope

k8s/dockerfiles/interactive-experimental.Dockerfile
@@ -37,3 +37,4 @@ RUN sudo chown -R graphscope:graphscope /opt/graphscope
 USER graphscope
 WORKDIR /home/graphscope
 
+ENV PATH=${PATH}:/home/graphscope/.local/bin

k8s/dockerfiles/interactive.Dockerfile
@@ -87,4 +87,4 @@ RUN python3 -m pip install --no-cache-dir vineyard vineyard-io --user
 USER graphscope
 WORKDIR /home/graphscope
 
-
+ENV PATH=${PATH}:/home/graphscope/.local/bin

k8s/dockerfiles/learning.Dockerfile
@@ -40,3 +40,5 @@ RUN sudo chmod a+wrx /tmp
 
 COPY --from=builder /home/graphscope/install /opt/graphscope/
 RUN python3 -m pip install --no-cache-dir /opt/graphscope/*.whl && sudo rm -rf /opt/graphscope/*.whl
+
+ENV PATH=${PATH}:/home/graphscope/.local/bin

k8s/internal/Makefile
@@ -101,21 +101,6 @@ graphscope-darwin-py3:
 		done
 
 graphscope-manylinux2014-py3-nodocker:
-	cd /tmp && \
-	git clone --single-branch --depth=1 https://github.com/v6d-io/v6d.git /tmp/v6d && \
-	cd /tmp/v6d && \
-	git submodule update --init && \
-	mkdir -p build && cd build && \
-	cmake .. \
-		-DCMAKE_INSTALL_PREFIX=/opt/vineyard \
-		-DCMAKE_PREFIX_PATH=/opt/graphscope \
-		-DOPENSSL_ROOT_DIR=/opt/graphscope \
-		-DUSE_EXTERNAL_ETCD_LIBS=OFF \
-		-DBUILD_SHARED_LIBS=ON \
-		-DBUILD_VINEYARD_TESTS=OFF && \
-	make -j$(nproc) && \
-	sudo make install && \
-	sudo rsync -av /opt/vineyard/ ${GRAPHSCOPE_HOME}/ && \
 	cd $(WORKING_DIR)/../.. && \
 	if [[ "${ARCH}" == "aarch64" ]]; then python3 -m pip install grpcio==1.49.1 --no-binary grpcio; \
 		export AUDITWHEEL_PLAT=manylinux2014_${ARCH}; \