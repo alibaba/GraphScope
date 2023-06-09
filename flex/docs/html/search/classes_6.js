2b4e3f0e3908f345dd21de5283008019f54a2661
Shen Sijie <ds_ssj@sjtu.edu.cn>
2023-06-09 17:13:08 +0800

    auto-gen docs of flex
    Signed-off-by: Shen Sijie <ds_ssj@sjtu.edu.cn>

--- flex/docs/html/search/classes_6.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['keybuffer_663',['KeyBuffer',['../structgs_1_1id__indexer__impl_1_1KeyBuffer.html',1,'gs::id_indexer_impl']]],
-  ['keybuffer_3c_20key_5ft_20_3e_664',['KeyBuffer&lt; KEY_T &gt;',['../structgs_1_1id__indexer__impl_1_1KeyBuffer.html',1,'gs::id_indexer_impl']]],
-  ['keybuffer_3c_20std_3a_3astring_20_3e_665',['KeyBuffer&lt; std::string &gt;',['../structgs_1_1id__indexer__impl_1_1KeyBuffer_3_01std_1_1string_01_4.html',1,'gs::id_indexer_impl']]],
-  ['keybuffer_3c_20std_3a_3astring_5fview_20_3e_666',['KeyBuffer&lt; std::string_view &gt;',['../structgs_1_1id__indexer__impl_1_1KeyBuffer_3_01std_1_1string__view_01_4.html',1,'gs::id_indexer_impl']]]
-];
--- flex/docs/html/search/classes_a.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['readtransaction_689',['ReadTransaction',['../classgs_1_1ReadTransaction.html',1,'gs']]]
-];
