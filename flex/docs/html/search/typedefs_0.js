9bf071cf3217f795bd2c844293a0459f517ab99e
ds-ssj <shensijie.ssj@alibaba-inc.com>
2023-06-09 17:52:56 +0800

    auto-gen docs of flex (#2851)
    <!--
    Thanks for your contribution! please review
    https://github.com/alibaba/GraphScope/blob/main/CONTRIBUTING.md before
    opening an issue.
    -->
    
    ## What do these changes do?
    
    <!-- Please give a short brief about these changes. -->
    
    Auto-gen the docs of `flex` and remove the pre-built html files. Due to
    the reduction of dependencies at build time (e.g. using cmake for flex),
    a new Doxygen file was created, namely `Doxygen-flex`.
    
    ## Related issue number
    #2726
    
    Signed-off-by: Sijie Shen <shensijie.ssj@alibaba-inc.com>

--- flex/docs/html/search/typedefs_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['adjlist_5ft_1276',['adjlist_t',['../classgs_1_1MutableCsr.html#a66823198f75043d97b327907c2112c16',1,'gs::MutableCsr::adjlist_t()'],['../classgs_1_1MutableCsr_3_01std_1_1string_01_4.html#a636743600760f1db2887504582982101',1,'gs::MutableCsr&lt; std::string &gt;::adjlist_t()']]]
-];
--- flex/docs/html/search/typedefs_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['ind_5fbuffer_5ft_1280',['ind_buffer_t',['../classgs_1_1IdIndexer.html#a0fac3ced93c1e64be3dca6d2efba31b0',1,'gs::IdIndexer']]],
-  ['intcolumn_1281',['IntColumn',['../namespacegs.html#ad79b0153c166cb0b6f6157829b7bdc27',1,'gs']]],
-  ['intemptycolumn_1282',['IntEmptyColumn',['../namespacegs.html#ae200011097d9b858e4400ec046f7a3c4',1,'gs']]]
-];
