da7a7b0ec3e93d4da772da45b248d9fd366b0c0b
siyuan0322 <siyuanzhang.zsy@alibaba-inc.com>
2023-06-09 10:27:51 +0800

    update

--- interactive_engine/data-load-tool/src/main/java/com/alibaba/graphscope/groot/dataload/databuild/DataBuildMapperOdps.java
+++ interactive_engine/data-load-tool/src/main/java/com/alibaba/graphscope/groot/dataload/databuild/DataBuildMapperOdps.java
@@ -81,7 +81,7 @@ public class DataBuildMapperOdps extends MapperBase {
         String[] items = new String[columnCount];
         for (int i = 0; i < columnCount; i++) {
             if (record.get(i) == null) {
-//                items[i] = "";
+                //                items[i] = "";
                 items[i] = null;
             } else {
                 items[i] = record.get(i).toString();
