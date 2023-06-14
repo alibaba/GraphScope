alibaba/GraphScope

commit 5fd6cc173b9936162e1d1ae0f7047e15ddac9e52

Fixes the inconsistent usage of msgpack/json in graph reporter and avoid protobuf message creation in __del__ (#2843)
Fixes #2823.

Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

python/graphscope/client/session.py
@@ -787,6 +787,10 @@ class Session(object):
     def closed(self):
         return self._closed
 
+    @property
+    def disconnected(self):
+        return self._grpc_client is None or self._disconnected
+
     def eager(self):
         return self._config_params["mode"] == "eager"
 

python/graphscope/framework/app.py
@@ -352,6 +352,10 @@ class AppDAGNode(DAGNode):
         # add op to dag
         self._session.dag.add_op(self._op)
 
+        # statically create the unload op to prevent a possible segmentation fault
+        # inside the protobuf library.
+        self._unload_op = unload_app(self)
+
     def __repr__(self):
         s = f"graphscope.App <type: {self._app_assets.type}, algorithm: {self._app_assets.algo} "
         s += f"bounded_graph: {str(self._graph)}>"
@@ -420,8 +424,7 @@ class AppDAGNode(DAGNode):
         Returns:
             :class:`graphscope.framework.app.UnloadedApp`: Evaluated in eager mode.
         """
-        op = unload_app(self)
-        return UnloadedApp(self._session, op)
+        return UnloadedApp(self._session, self._unload_op)
 
 
 class App(object):

python/graphscope/framework/context.py
@@ -121,6 +121,9 @@ class BaseContextDAGNode(DAGNode):
         self._op = run_app(self._bound_app, *args, **kwargs)
         self._session.dag.add_op(self._op)
 
+        # statically create the unload op
+        self._unload_op = dag_utils.unload_context(self)
+
     def _check_selector(self, selector):
         raise NotImplementedError()
 
@@ -279,8 +282,7 @@ class BaseContextDAGNode(DAGNode):
             pass
 
     def _unload(self):
-        op = dag_utils.unload_context(self)
-        return UnloadedContext(self._session, op)
+        return UnloadedContext(self._session, self._unload_op)
 
 
 class TensorContextDAGNode(BaseContextDAGNode):

python/graphscope/framework/graph.py
@@ -292,6 +292,9 @@ class GraphDAGNode(DAGNode, GraphInterface):
         self._resolve_op(incoming_data)
         self._session.dag.add_op(self._op)
 
+        # statically create the unload op
+        self._unload_op = dag_utils.unload_graph(self)
+
     @property
     def v_labels(self):
         return self._v_labels
@@ -714,8 +717,7 @@ class GraphDAGNode(DAGNode, GraphInterface):
         Returns:
             :class:`graphscope.framework.graph.UnloadedGraph`: Evaluated in eager mode.
         """
-        op = dag_utils.unload_graph(self)
-        return UnloadedGraph(self._session, op)
+        return UnloadedGraph(self._session, self._unload_op)
 
     def project(
         self,

python/graphscope/nx/classes/graph.py
@@ -20,7 +20,6 @@
 #
 
 import copy
-import threading
 
 import orjson as json
 from networkx import freeze
@@ -369,6 +368,12 @@ class Graph(_GraphBase):
         self._saved_signature = self.signature
         self._is_client_view = False
 
+        # statically create the unload op
+        if self.op is None:
+            self._unload_op = None
+        else:
+            self._unload_op = dag_utils.unload_graph(self)
+
     def _is_gs_graph(self, incoming_graph_data):
         return (
             hasattr(incoming_graph_data, "graph_type")
@@ -376,22 +381,16 @@ class Graph(_GraphBase):
         )
 
     def __del__(self):
-        if self._session.info["status"] != "active" or self._key is None:
+        if self._key is None or self._session.disconnected:
             return
 
-        # use thread to avoid dead-lock
-        def _del(graph):
-            # cancel cache fetch future
-            if graph.cache.enable_iter_cache:
-                graph.cache.shutdown()
-            op = dag_utils.unload_graph(graph)
-            op.eval()
-            graph._key = None
+        if self.cache.enable_iter_cache:
+            self.cache.shutdown()
+        self.cache.shutdown_executor()
 
-        if not self._is_client_view:
-            t = threading.Thread(target=_del, args=(self,))
-            t.daemon = True
-            t.start()
+        if not self._is_client_view and self._unload_op is not None:
+            self._unload_op.eval()
+        self._key = None
 
     @property
     def op(self):

analytical_engine/core/fragment/dynamic_fragment.h
@@ -1529,7 +1529,7 @@ class DynamicFragmentMutator {
       v_fid = partitioner.GetPartitionId(oid);
       if (modify_type == rpc::NX_ADD_NODES) {
         vm_ptr_->AddVertex(std::move(oid), gid);
-        if (!v_data.Empty()) {
+        if (v_data.IsObject() && !v_data.GetObject().ObjectEmpty()) {
           for (const auto& prop : v_data.GetObject()) {
             if (!fragment_->schema_["vertex"].HasMember(prop.name)) {
               dynamic::Value key(prop.name);
@@ -1606,7 +1606,7 @@ class DynamicFragmentMutator {
                                                   std::move(empty_data));
           }
         }
-        if (!e_data.Empty()) {
+        if (e_data.IsObject() && !e_data.GetObject().ObjectEmpty()) {
           for (const auto& prop : e_data.GetObject()) {
             if (!fragment_->schema_["edge"].HasMember(prop.name)) {
               dynamic::Value key(prop.name);

analytical_engine/core/fragment/fragment_reporter.h
@@ -105,7 +105,6 @@ class DynamicFragmentReporter : public grape::Communicator {
       }
       break;
     }
-
     case rpc::HAS_EDGE: {
       BOOST_LEAF_AUTO(edge_in_json, params.Get<std::string>(rpc::EDGE));
       dynamic::Value edge;
@@ -761,7 +760,9 @@ class ArrowFragmentReporter<
         }
       }
       // archive the start gid and nodes attribute array.
-      arc << gid << nodes_attr;
+      msgpack::sbuffer sbuf;
+      msgpack::pack(&sbuf, nodes_attr);
+      arc << gid << sbuf;
     }
   }
 
@@ -859,7 +860,9 @@ class ArrowFragmentReporter<
         }
       }
       // archive the start gid and edges attributes array.
-      arc << gid << adj_list;
+      msgpack::sbuffer sbuf;
+      msgpack::pack(&sbuf, adj_list);
+      arc << gid << sbuf;
     }
   }
 

python/graphscope/nx/classes/cache.py
@@ -113,9 +113,8 @@ class Cache:
     def align_node_attr_cache(self):
         """Check and align the node attr cache with node id cache"""
         if self.enable_iter_cache and self.node_attr_align is False:
-            f = self.futures["node_attr"]
-            if f is not None:
-                start_gid, self.node_attr_cache = f.result()
+            if self.futures["node_attr"] is not None:
+                start_gid, self.node_attr_cache = self.futures["node_attr"].result()
                 if start_gid == self.iter_pre_gid:
                     # align to current node_id_cache
                     if self.iter_gid != self.iter_pre_gid:
@@ -129,8 +128,7 @@ class Cache:
     def align_succ_cache(self):
         """Check and align the succ neighbor cache with node id cache"""
         if self.enable_iter_cache and self.succ_align is False:
-            f = self.futures["succ"]
-            start_gid, self.succ_cache = f.result()
+            start_gid, self.succ_cache = self.futures["succ"].result()
             if start_gid == self.iter_pre_gid:
                 if self.iter_gid != self.iter_pre_gid:
                     self._async_fetch_succ_cache(self.iter_gid)
@@ -143,9 +141,8 @@ class Cache:
     def align_succ_attr_cache(self):
         """Check and align the succ neighbor attr cache with node id cache"""
         if self.enable_iter_cache and self.succ_attr_align is False:
-            f = self.futures["succ_attr"]
-            if f is not None:
-                start_gid, self.succ_attr_cache = f.result()
+            if self.futures["succ_attr"] is not None:
+                start_gid, self.succ_attr_cache = self.futures["succ_attr"].result()
                 if start_gid == self.iter_pre_gid:
                     if self.iter_gid != self.iter_pre_gid:
                         self._async_fetch_succ_attr_cache(self.iter_gid)
@@ -160,8 +157,7 @@ class Cache:
         if self.enable_iter_cache and self.pred_align is False:
             if self.futures["pred"] is None:
                 self._async_fetch_pred_cache(self.iter_pre_gid)
-            f = self.futures["pred"]
-            start_gid, self.pred_cache = f.result()
+            start_gid, self.pred_cache = self.futures["pred"].result()
             if start_gid == self.iter_pre_gid:
                 if self.iter_gid != self.iter_pre_gid:
                     self._async_fetch_pred_cache(self.iter_gid)
@@ -177,8 +173,7 @@ class Cache:
         if self.enable_iter_cache and self.pred_attr_align is False:
             if self.futures["pred_attr"] is None:
                 self._async_fetch_pred_attr_cache(self.iter_pre_gid)
-            f = self.futures["pred_attr"]
-            start_gid, self.pred_attr_cache = f.result()
+            start_gid, self.pred_attr_cache = self.futures["pred_attr"].result()
             if start_gid == self.iter_pre_gid:
                 if self.iter_gid != self.iter_pre_gid:
                     self._async_fetch_pred_attr_cache(self.iter_gid)
@@ -258,6 +253,9 @@ class Cache:
                 except concurrent.futures.CancelledError:
                     pass
                 future = None
+
+    def shutdown_executor(self):
+        self.executor.shutdown(wait=True)
 
     def clear(self):
         """Clear batch cache and lru cache, reset the status and warmup again"""

commit fbf40f6ddcaa5394aa8f557b63196aa37c41785f

Update the pre-genereated unload_op when the op itself been deepcopied (#2888)
The issue was first introduced in #2843.

Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

python/graphscope/client/session.py
@@ -672,7 +672,7 @@ class Session(object):
         #       "vineyard_socket": "...",
         #       "vineyard_rpc_endpoint": "..."
         #   }
-        self._engine_config: {} = None
+        self._engine_config: dict = None
 
         # interactive instance related graph map
         self._interactive_instance_dict = {}
@@ -713,7 +713,7 @@ class Session(object):
         return self._session_id
 
     @property
-    def dag(self):
+    def dag(self) -> Dag:
         return self._dag
 
     def _log_session_info(self):

python/graphscope/framework/app.py
@@ -442,6 +442,7 @@ class App(object):
         # copy and set op evaluated
         self._app_node.op = deepcopy(self._app_node.op)
         self._app_node.evaluated = True
+        self._app_node._unload_op = unload_app(self._app_node)
         self._session.dag.add_op(self._app_node.op)
         self._saved_signature = self.signature
 

python/graphscope/framework/context.py
@@ -610,6 +610,7 @@ class Context(object):
         # copy and set op evaluated
         self._context_node.op = deepcopy(self._context_node.op)
         self._context_node.evaluated = True
+        self._context_node._unload_op = dag_utils.unload_context(self._context_node)
         self._saved_signature = self.signature
 
     @property

python/graphscope/framework/graph.py
@@ -292,8 +292,12 @@ class GraphDAGNode(DAGNode, GraphInterface):
         self._resolve_op(incoming_data)
         self._session.dag.add_op(self._op)
 
-        # statically create the unload op
-        self._unload_op = dag_utils.unload_graph(self)
+        # statically create the unload op, as the op may change, the
+        # unload op should be refreshed as well.
+        if self._op is None:
+            self._unload_op = None
+        else:
+            self._unload_op = dag_utils.unload_graph(self)
 
     @property
     def v_labels(self):
@@ -377,6 +381,8 @@ class GraphDAGNode(DAGNode, GraphInterface):
                 self._op = self._from_nx_graph(incoming_data)
             else:
                 raise RuntimeError("Not supported incoming data.")
+        # update the unload op
+        self._unload_op = dag_utils.unload_graph(self)
 
     def to_numpy(self, selector, vertex_range=None):
         """Select some elements of the graph and output to numpy.
@@ -806,6 +812,7 @@ class Graph(GraphInterface):
         # copy and set op evaluated
         self._graph_node.op = deepcopy(self._graph_node.op)
         self._graph_node.evaluated = True
+        self._graph_node._unload_op = dag_utils.unload_graph(self._graph_node)
         self._session.dag.add_op(self._graph_node.op)
 
         self._key = None

python/graphscope/nx/classes/graph.py
@@ -369,6 +369,9 @@ class Graph(_GraphBase):
         self._is_client_view = False
 
         # statically create the unload op
+        #
+        # networkx operations update the op, but keep the key as same, thus
+        # the unload op don't need to be refreshed.
         if self.op is None:
             self._unload_op = None
         else: