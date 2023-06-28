3085f9087ccd676a8d86f18000f530f3efd76cc4
晓简 <lxj193371@alibaba-inc.com>
2023-06-28 17:51:35 +0800

    output dir

--- flex/src/root_command.sh
+++ flex/src/root_command.sh
@@ -2,6 +2,11 @@
 # echo "# you can edit it freely and regenerate (it will not be overwritten)"
 # inspect_args
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+output_dir=`realpath ${args[--output_dir]}`
+mkdir -p ${output_dir}
+
 if [ ${args[--app]} == "db" ]; then
   package_name="graphscope_flex_${args[--app]}"
   comps=''
@@ -9,15 +14,14 @@ if [ ${args[--app]} == "db" ]; then
   for i in "${comps[@]}"; do
     package_name="${package_name}_${i}"
   done
-  output_dir=`pwd`
   build_dir=`mktemp -d`
   pushd $build_dir > /dev/null
-  cmd="cmake -DCPACK_PACKAGE_NAME=${package_name} ${output_dir} && make -j && make package && mv ${package_name}*.deb ${output_dir}/"
+  cmd="cmake -DCPACK_PACKAGE_NAME=${package_name} ${SCRIPT_DIR} && make -j && make package && mv ${package_name}*.deb ${output_dir}/"
   echo $cmd
   eval $cmd
   popd > /dev/null
 else
-  echo "not reach db"
+  echo "unexpected app type"
 fi
 
 # TODO: parse args and make.
