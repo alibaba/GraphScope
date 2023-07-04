090b1cd851b21cfa5bc0aecb2c1f497316f4a5c1
Tao He <linzhu.ht@alibaba-inc.com>
2023-07-04 11:07:00 +0800

    Fixes two compliation warnings
    Signed-off-by: Tao He <linzhu.ht@alibaba-inc.com>

--- interactive_engine/executor/engine/pegasus/pegasus/src/communication/buffer.rs
+++ interactive_engine/executor/engine/pegasus/pegasus/src/communication/buffer.rs
@@ -118,7 +118,7 @@ impl<D: Data> BufSlotPtr<D> {
     fn destroy(&mut self) {
         unsafe {
             let ptr = self.ptr;
-            Box::from_raw(ptr.as_ptr());
+            drop(Box::from_raw(ptr.as_ptr()));
         }
     }
 }
