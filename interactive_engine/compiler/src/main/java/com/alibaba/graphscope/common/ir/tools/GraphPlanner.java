18b2dc1860108f4231c3d4f1fc44e3a667906ca6
zhanglei1949 <zhanglei1949@sjtu.edu.cn>
2023-07-06 10:56:37 +0000

    format
    Committed-by: zhanglei1949 from Dev container

--- interactive_engine/compiler/src/main/java/com/alibaba/graphscope/common/ir/tools/GraphPlanner.java
+++ interactive_engine/compiler/src/main/java/com/alibaba/graphscope/common/ir/tools/GraphPlanner.java
@@ -187,8 +187,9 @@ public class GraphPlanner {
         ExperimentalMetaFetcher metaFetcher = new ExperimentalMetaFetcher(configs);
         if (args.length < 2 || args[0].isEmpty() || args[1].isEmpty()) {
             throw new IllegalArgumentException(
-                    "usage: make physical_plan query='<query in string>/<file://path_to_query_file>' physical='<path to the"
-                            + " physical output file>'");
+                    "usage: make physical_plan query='<query in"
+                        + " string>/<file://path_to_query_file>' physical='<path to the physical"
+                        + " output file>'");
         }
         String query = args[0];
         if (query.startsWith("file://")) {
