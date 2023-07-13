b1e1891461d9cb4ccb7832d46bb8534c9dd41d89
Xiaoli Zhou <yihe.zxl@alibaba-inc.com>
2023-07-13 09:55:48 +0800

    [GIE Compiler] Make Config path Configurable from User Given Parameters (#2982)
    <!--
    Thanks for your contribution! please review
    https://github.com/alibaba/GraphScope/blob/main/CONTRIBUTING.md before
    opening an issue.
    -->
    
    ## What do these changes do?
    as titled.
    
    <!-- Please give a short brief about these changes. -->
    
    ## Related issue number
    
    <!-- Are there any issues opened that will be resolved by merging this
    change? -->
    
    Fixes
    
    ---------
    
    Co-authored-by: Longbin Lai <longbin.lailb@alibaba-inc.com>

--- interactive_engine/compiler/src/main/java/com/alibaba/graphscope/GraphServer.java
+++ interactive_engine/compiler/src/main/java/com/alibaba/graphscope/GraphServer.java
@@ -133,7 +133,10 @@ public class GraphServer {
     }
 
     public static void main(String[] args) throws Exception {
-        Configs configs = new Configs("conf/ir.compiler.properties", FileLoadType.RELATIVE_PATH);
+        if (args.length == 0 || args[0].isEmpty()) {
+            throw new IllegalArgumentException("usage: GraphServer '<path_to_config_file>'");
+        }
+        Configs configs = new Configs(args[0], FileLoadType.RELATIVE_PATH);
         IrMetaQueryCallback queryCallback =
                 new IrMetaQueryCallback(new ExperimentalMetaFetcher(configs));
         GraphServer server =
