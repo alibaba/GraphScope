0d3e5466893fb76faee7c98efe4392ba6ac80b63
xiaolei.zl <xiaolei.zl@alibaba-inc.com>
2023-08-25 15:36:08 +0800

    fix test

--- flex/tests/hqps/hqps_cypher_test.sh
+++ flex/tests/hqps/hqps_cypher_test.sh
@@ -82,9 +82,9 @@ create_ir_conf(){
     echo "hiactor.hosts: localhost:10000" >> ${HQPS_IR_CONF}
     echo "graph.store: exp" >> ${HQPS_IR_CONF}
     echo "graph.schema: ${GS_TEST_DIR}/flex/ldbc-sf01-long-date/ldbc_schema_csr_ic.json" >> ${HQPS_IR_CONF}
-    echo "graph.planner.is.on: true" >> /tmp/ir.compiler.properties
-    echo "graph.planner.opt: RBO" >> /tmp/ir.compiler.properties
-    echo "graph.planner.rules: FilterMatchRule" >> /tmp/ir.compiler.properties
+    echo "graph.planner.is.on: true" >> ${HQPS_IR_CONF}
+    echo "graph.planner.opt: RBO" >> ${HQPS_IR_CONF}
+    echo "graph.planner.rules: FilterMatchRule" >> ${HQPS_IR_CONF}
     echo "gremlin.server.disabled: true" >> ${HQPS_IR_CONF}
     echo "neo4j.bolt.server.port: 7687" >> ${HQPS_IR_CONF}
 
