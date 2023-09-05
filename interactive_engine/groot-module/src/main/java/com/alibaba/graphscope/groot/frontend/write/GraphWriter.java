e13b12d5fe36784273f0235777c537aae8500112
siyuanzhang.zsy <siyuanzhang.zsy@alibaba-inc.com>
2023-09-05 05:21:51 +0000

    fix
    Committed-by: siyuanzhang.zsy from Dev container

--- interactive_engine/groot-module/src/main/java/com/alibaba/graphscope/groot/frontend/write/GraphWriter.java
+++ interactive_engine/groot-module/src/main/java/com/alibaba/graphscope/groot/frontend/write/GraphWriter.java
@@ -351,7 +351,8 @@ public class GraphWriter implements MetricsAgent {
                     parseRawProperties(dstVertexDef, dstVertexRecordKey.getProperties());
             long dstVertexHashId =
                     getPrimaryKeysHashId(dstVertexDef.getLabelId(), dstVertexPkVals, dstVertexDef);
-            long edgeInnerId = overwrite ? edgeIdGenerator.getNextId() : edgeRecordKey.getEdgeInnerId();
+            long edgeInnerId = 
+                    overwrite ? edgeIdGenerator.getNextId() : edgeRecordKey.getEdgeInnerId();
 
             return new EdgeId(
                     new VertexId(srcVertexHashId), new VertexId(dstVertexHashId), edgeInnerId);
