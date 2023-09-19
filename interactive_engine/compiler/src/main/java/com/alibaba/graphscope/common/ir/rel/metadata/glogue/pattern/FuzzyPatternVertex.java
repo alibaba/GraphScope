a07b97742cd5225afd0d873969fa97005ee0392a
bingqing.lbq <bingqing.lbq@alibaba-inc.com>
2023-09-19 03:15:16 +0000

    [GLogue] add FuzzyPatternVertex and FuzzyPatternEdge
    Committed-by: bingqing.lbq from Dev container

---
+++ interactive_engine/compiler/src/main/java/com/alibaba/graphscope/common/ir/rel/metadata/glogue/pattern/FuzzyPatternVertex.java
@@ -0,0 +1,34 @@
+package com.alibaba.graphscope.common.ir.rel.metadata.glogue.pattern;
+
+import java.util.List;
+
+public class FuzzyPatternVertex extends PatternVertex {
+
+    private Integer id;
+    private List<Integer> vertexTypeIds;
+
+    public FuzzyPatternVertex(List<Integer> vertexTypeIds) {
+        this.vertexTypeIds = vertexTypeIds;
+        this.id = 0;
+    }
+
+    public FuzzyPatternVertex(List<Integer> vertexTypeIds, int id) {
+        this.vertexTypeIds = vertexTypeIds;
+        this.id = id;
+    }
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public List<Integer> getVertexTypeIds() {
+        return vertexTypeIds;
+    }
+
+    @Override
+    public boolean isDistinct() {
+        return false;
+    }
+}
