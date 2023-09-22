5f7d432d24ef69c7281fe0679f9d4281b109228b
longbin.lailb <longbin.lailb@alibaba-inc.com>
2023-09-22 14:39:47 +0800

    fix unit test

--- interactive_engine/executor/ir/integrated/tests/pathxd_test.rs
+++ interactive_engine/executor/ir/integrated/tests/pathxd_test.rs
@@ -967,6 +967,7 @@ mod test {
             alias: None,
             params: Some(query_params(vec![PERSON_LABEL.into()], vec![], None)),
             idx_predicate: None,
+            is_count_only: false,
             meta_data: None,
         };
 
