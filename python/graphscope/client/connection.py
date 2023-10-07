47307298e36c3af589118ce825af4be60cb06527
siyuan0322 <siyuanzhang.zsy@alibaba-inc.com>
2023-10-07 15:30:34 +0800

    add python interface
    Committed-by: siyuanzhang.zsy from Dev container

--- python/graphscope/client/connection.py
+++ python/graphscope/client/connection.py
@@ -34,6 +34,8 @@ from graphscope.proto import ddl_service_pb2
 from graphscope.proto import ddl_service_pb2_grpc
 from graphscope.proto import write_service_pb2
 from graphscope.proto import write_service_pb2_grpc
+from graphscope.proto.groot.sdk import client_pb2_grpc
+from graphscope.proto.groot.sdk import model_pb2
 
 
 class Graph:
@@ -99,6 +101,7 @@ class Connection:
         channel = grpc.insecure_channel(addr, options=options)
         self._ddl_service_stub = ddl_service_pb2_grpc.GrootDdlServiceStub(channel)
         self._write_service_stub = write_service_pb2_grpc.ClientWriteStub(channel)
+        self._client_service_stub = client_pb2_grpc.ClientStub(channel)
         self._client_id = None
         self._metadata = self._encode_metadata(username, password)
         gremlin_url = f"ws://{self._gremlin_endpoint}/gremlin"
@@ -173,6 +176,13 @@ class Connection:
             request, metadata=self._metadata
         )
         return response.success
+
+    def get_store_state(self):
+        request = model_pb2.GetStoreStateRequest()
+        response = self._client_service_stub.getStoreState(
+            request, metadata=self._metadata
+        )
+        return response.partitionStates
 
     def _encode_metadata(self, username, password):
         if not (username and password):
