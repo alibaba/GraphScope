5728325285d2d2aa0b5efc44a492d1ef2c52cf35
nengli.ln <nengli.ln@alibaba-inc.com>
2023-11-21 10:48:49 +0800

    update worker

--- interactive_engine/executor/engine/pegasus/pegasus/src/worker.rs
+++ interactive_engine/executor/engine/pegasus/pegasus/src/worker.rs
@@ -131,9 +131,7 @@ impl<D: Data, T: Debug + Send + 'static> Worker<D, T> {
     }
 
     fn release(&mut self) {
-        if self.peer_guard.fetch_sub(1, Ordering::SeqCst) == 1 {
-            pegasus_memory::alloc::remove_task(self.conf.job_id as usize);
-        }
+        self.peer_guard.fetch_sub(1, Ordering::SeqCst);
         if !crate::remove_cancel_hook(self.conf.job_id).is_ok() {
             error!("JOB_CANCEL_MAP is poisoned!");
         }
