ccb028f9d0279dfeb02ccc4e91058ee84aef7670
nengli.ln <nengli.ln@alibaba-inc.com>
2023-11-21 10:27:39 +0800

    specify type for from_raw

--- interactive_engine/executor/engine/pegasus/common/src/rc.rs
+++ interactive_engine/executor/engine/pegasus/common/src/rc.rs
@@ -97,9 +97,8 @@ impl<T: std::fmt::Debug> std::fmt::Debug for RcPointer<T> {
 impl<T: ?Sized> Drop for RcPointer<T> {
     fn drop(&mut self) {
         if self.count.fetch_sub(1, Ordering::SeqCst) == 1 {
-            //unsafe { std::ptr::drop_in_place(self.ptr.as_ptr()) }
             unsafe {
-                let _ = Box::from_raw(self.ptr.as_ptr());
+                let _ = Box::<T>::from_raw(self.ptr.as_ptr());
             }
         }
     }
