93056871fa3fa61fe158cc1025f099908d7e76d4
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-02-15 05:04:44 -0500

    spelling: lang
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- interactive_engine/executor/ir/integrated/tests/match_test.rs
+++ interactive_engine/executor/ir/integrated/tests/match_test.rs
@@ -103,7 +103,7 @@ mod test {
             params: Some(query_params(
                 vec![CREATED_LABEL.into()],
                 vec![],
-                str_to_expr_pb("@.lange=\"Java\"".to_string()).ok(),
+                str_to_expr_pb("@.lang=\"Java\"".to_string()).ok(),
             )),
             expand_opt: 0,
             alias: None,
