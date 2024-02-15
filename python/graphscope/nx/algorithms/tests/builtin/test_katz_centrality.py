0c56725141ce0d6a21b1985326aefbcfcd407115
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-02-15 06:46:50 -0500

    spelling: number
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- python/graphscope/nx/algorithms/tests/builtin/test_katz_centrality.py
+++ python/graphscope/nx/algorithms/tests/builtin/test_katz_centrality.py
@@ -130,7 +130,7 @@ class TestKatzCentrality(object):
             beta = {0: 77}
             e = nx.builtin.katz_centrality(G, 0.1, beta=beta)
 
-    def test_bad_beta_numbe(self):
+    def test_bad_beta_number(self):
         with pytest.raises(nx.NetworkXException):
             G = nx.Graph([(0, 1)])
             e = nx.builtin.katz_centrality(G, 0.1, beta="foo")
@@ -240,7 +240,7 @@ class TestKatzCentralityNumpy(object):
             beta = {0: 77}
             e = nx.katz_centrality_numpy(G, 0.1, beta=beta)
 
-    def test_bad_beta_numbe(self):
+    def test_bad_beta_number(self):
         with pytest.raises(nx.NetworkXException):
             G = nx.Graph([(0, 1)])
             e = nx.katz_centrality_numpy(G, 0.1, beta="foo")
