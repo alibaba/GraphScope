5c4a3680e5cd226f23bd8dbce9978a2bdba7cda5
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-02-15 00:21:28 -0500

    spelling: following
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- .github/free-disk-space.sh
+++ .github/free-disk-space.sh
@@ -22,7 +22,7 @@
 # Free: 17 GB
 # This script frees up 28 GB of disk space by deleting unneeded packages and 
 # large directories.
-# The Flink end to end tests download and generate more than 17 GB of files,
+# The following end to end tests download and generate more than 17 GB of files,
 # causing unpredictable behavior and build failures.
 #
 echo "=============================================================================="
