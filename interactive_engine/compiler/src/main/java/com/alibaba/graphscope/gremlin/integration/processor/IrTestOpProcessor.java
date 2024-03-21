alibaba/GraphScope

commit 292b636905a326d45546db5197219ddae5e2e9d8

fix(interactive): Fix Timeout Bug & Process Results From Pegasus Synchronously (#3630)
<!--
Thanks for your contribution! please review
https://github.com/alibaba/GraphScope/blob/main/CONTRIBUTING.md before
opening an issue.
-->

## What do these changes do?
as titled.

<!-- Please give a short brief about these changes. -->

## Related issue number

<!-- Are there any issues opened that will be resolved by merging this
change? -->

Fixes

interactive_engine/compiler/src/main/java/com/alibaba/graphscope/gremlin/integration/processor/IrTestOpProcessor.java
@@ -131,11 +131,11 @@ public class IrTestOpProcessor extends IrStandardOpProcessor {
                                             traversal,
                                             new com.alibaba.graphscope.gremlin.integration.result
                                                     .GremlinTestResultProcessor(
+                                                    configs,
                                                     ctx,
                                                     traversal,
                                                     statusCallback,
                                                     testGraph,
-                                                    this.configs,
                                                     timeoutConfig),
                                             irMeta,
                                             new QueryTimeoutConfig(ctx.getRequestTimeout()),
@@ -149,12 +149,14 @@ public class IrTestOpProcessor extends IrStandardOpProcessor {
                                             new ResultSchema(summary.getLogicalPlan());
                                     ExecutionResponseListener listener =
                                             new GremlinTestResultProcessor(
+                                                    configs,
                                                     ctx,
-                                                    statusCallback,
                                                     new GremlinTestRecordParser(
                                                             resultSchema,
                                                             testGraph.getProperties(configs)),
-                                                    resultSchema);
+                                                    resultSchema,
+                                                    statusCallback,
+                                                    timeoutConfig);
                                     if (value.result != null && value.result.isCompleted) {
                                         List<IrResult.Results> records = value.result.records;
                                         records.forEach(k -> listener.onNext(k.getRecord()));

interactive_engine/compiler/conf/ir.compiler.properties
@@ -55,3 +55,6 @@ calcite.default.charset: UTF-8
 
 # the output plan format, can be ffi(default) or proto
 # physical.opt.config: ffi
+
+# set the max capacity of the result streaming buffer for each query
+# per.query.stream.buffer.max.capacity: 256

interactive_engine/compiler/src/main/java/com/alibaba/graphscope/common/config/FrontendConfig.java
@@ -57,4 +57,7 @@ public class FrontendConfig {
 
     public static final Config<String> PHYSICAL_OPT_CONFIG =
             Config.stringConfig("physical.opt.config", "ffi");
+
+    public static final Config<Integer> PER_QUERY_STREAM_BUFFER_MAX_CAPACITY =
+            Config.intConfig("per.query.stream.buffer.max.capacity", 256);
 }

interactive_engine/compiler/src/main/java/com/alibaba/graphscope/common/config/QueryTimeoutConfig.java
@@ -25,10 +25,10 @@ public class QueryTimeoutConfig {
     private final long executionTimeoutMS;
     private static final double GRADUAL_FACTOR = 0.1d;
 
-    public QueryTimeoutConfig(long engineTimeoutMS) {
-        this.engineTimeoutMS = engineTimeoutMS;
-        this.channelTimeoutMS = (long) (engineTimeoutMS * (1 + GRADUAL_FACTOR));
-        this.executionTimeoutMS = (long) (engineTimeoutMS * (1 + 2 * GRADUAL_FACTOR));
+    public QueryTimeoutConfig(long executionTimeoutMS) {
+        this.executionTimeoutMS = executionTimeoutMS;
+        this.channelTimeoutMS = (long) (executionTimeoutMS * (1 - GRADUAL_FACTOR));
+        this.engineTimeoutMS = (long) (executionTimeoutMS * (1 - 2 * GRADUAL_FACTOR));
     }
 
     public long getExecutionTimeoutMS() {

interactive_engine/compiler/src/main/java/com/alibaba/graphscope/gremlin/integration/result/GremlinTestResultProcessor.java
@@ -45,13 +45,13 @@ public class GremlinTestResultProcessor extends GremlinResultProcessor {
     private static String EDGE_PROPERTIES = "edge_properties";
 
     public GremlinTestResultProcessor(
+            Configs configs,
             Context writeResult,
             Traversal traversal,
             QueryStatusCallback statusCallback,
             GraphProperties testGraph,
-            Configs configs,
             QueryTimeoutConfig timeoutConfig) {
-        super(writeResult, traversal, statusCallback, timeoutConfig);
+        super(configs, writeResult, traversal, statusCallback, timeoutConfig);
         this.cachedProperties = testGraph.getProperties(configs);
     }
 

interactive_engine/compiler/src/main/java/com/alibaba/graphscope/gremlin/integration/resultx/GremlinTestResultProcessor.java
@@ -16,6 +16,8 @@
 
 package com.alibaba.graphscope.gremlin.integration.resultx;
 
+import com.alibaba.graphscope.common.config.Configs;
+import com.alibaba.graphscope.common.config.QueryTimeoutConfig;
 import com.alibaba.graphscope.common.result.RecordParser;
 import com.alibaba.graphscope.gaia.proto.IrResult;
 import com.alibaba.graphscope.gremlin.plugin.QueryStatusCallback;
@@ -34,15 +36,17 @@ import java.util.stream.Collectors;
 
 public class GremlinTestResultProcessor extends GremlinResultProcessor {
     public GremlinTestResultProcessor(
+            Configs configs,
             Context ctx,
-            QueryStatusCallback statusCallback,
             RecordParser recordParser,
-            ResultSchema resultSchema) {
-        super(ctx, statusCallback, recordParser, resultSchema);
+            ResultSchema resultSchema,
+            QueryStatusCallback statusCallback,
+            QueryTimeoutConfig timeoutConfig) {
+        super(configs, ctx, recordParser, resultSchema, statusCallback, timeoutConfig);
     }
 
     @Override
-    public void onNext(IrResult.Record record) {
+    protected void processRecord(IrResult.Record record) {
         List<Object> results = recordParser.parseFrom(record);
         if (resultSchema.isGroupBy && !results.isEmpty()) {
             if (results.stream().anyMatch(k -> !(k instanceof Map))) {
@@ -65,7 +69,7 @@ public class GremlinTestResultProcessor extends GremlinResultProcessor {
     }
 
     @Override
-    public void onCompleted() {
+    protected void finishRecord() {
         List<Object> results = Lists.newArrayList();
         if (resultSchema.isGroupBy) {
             results.add(new DefaultRemoteTraverser(reducer, 1L));

interactive_engine/compiler/src/main/java/com/alibaba/graphscope/gremlin/plugin/processor/IrStandardOpProcessor.java
@@ -52,7 +52,6 @@ import com.alibaba.graphscope.gremlin.plugin.traversal.IrCustomizedTraversalSour
 import com.alibaba.graphscope.gremlin.result.processor.AbstractResultProcessor;
 import com.alibaba.graphscope.gremlin.result.processor.GremlinResultProcessor;
 import com.alibaba.pegasus.RpcClient;
-import com.alibaba.pegasus.intf.ResultProcessor;
 import com.alibaba.pegasus.service.protocol.PegasusClient;
 import com.google.common.collect.Maps;
 import com.google.protobuf.ByteString;
@@ -152,6 +151,7 @@ public class IrStandardOpProcessor extends StandardOpProcessor {
         String jobName = idGenerator.generateName(jobId);
         IrMeta irMeta = metaQueryCallback.beforeExec();
         QueryStatusCallback statusCallback = createQueryStatusCallback(script, jobId);
+        QueryTimeoutConfig timeoutConfig = new QueryTimeoutConfig(ctx.getRequestTimeout());
         String language = FrontendConfig.GREMLIN_SCRIPT_LANGUAGE_NAME.get(configs);
         GremlinExecutor.LifeCycle lifeCycle;
         switch (language) {
@@ -162,18 +162,21 @@ public class IrStandardOpProcessor extends StandardOpProcessor {
                                 gremlinExecutorSupplier,
                                 bindingsSupplier,
                                 irMeta,
-                                statusCallback);
+                                statusCallback,
+                                timeoutConfig);
                 break;
             case GremlinCalciteScriptEngineFactory.LANGUAGE_NAME:
                 lifeCycle =
                         new LifeCycleSupplier(
+                                        configs,
                                         ctx,
                                         queryCache,
                                         executionClient,
                                         jobId,
                                         jobName,
                                         irMeta,
-                                        statusCallback)
+                                        statusCallback,
+                                        timeoutConfig)
                                 .get();
                 break;
             default:
@@ -185,11 +188,9 @@ public class IrStandardOpProcessor extends StandardOpProcessor {
             evalFuture.handle(
                     (v, t) -> {
                         metaQueryCallback.afterExec(irMeta);
-                        if (t != null) {
-                            statusCallback.onEnd(false, null);
-                            if (v instanceof AbstractResultProcessor) {
-                                ((AbstractResultProcessor) v).cancel();
-                            }
+                        // TimeoutException has been handled in ResultProcessor, skip it here
+                        if (t != null && !(t instanceof TimeoutException)) {
+                            statusCallback.onEnd(false, t.getMessage());
                             Optional<Throwable> possibleTemporaryException =
                                     determineIfTemporaryException(t);
                             if (possibleTemporaryException.isPresent()) {
@@ -225,19 +226,6 @@ public class IrStandardOpProcessor extends StandardOpProcessor {
                                                             "Timeout during script evaluation"
                                                                 + " triggered by"
                                                                 + " TimedInterruptCustomizerProvider")
-                                                    .statusAttributeException(t)
-                                                    .create());
-                                } else if (t instanceof TimeoutException) {
-                                    errorMessage =
-                                            String.format(
-                                                    "Script evaluation exceeded the configured"
-                                                            + " threshold for request [%s]",
-                                                    msg);
-                                    statusCallback.getQueryLogger().warn(errorMessage, t);
-                                    ctx.writeAndFlush(
-                                            ResponseMessage.build(msg)
-                                                    .code(ResponseStatusCode.SERVER_ERROR_TIMEOUT)
-                                                    .statusMessage(t.getMessage())
                                                     .statusAttributeException(t)
                                                     .create());
                                 } else if (t instanceof MultipleCompilationErrorsException
@@ -306,8 +294,8 @@ public class IrStandardOpProcessor extends StandardOpProcessor {
             Supplier<GremlinExecutor> gremlinExecutorSupplier,
             BindingSupplier bindingsSupplier,
             IrMeta irMeta,
-            QueryStatusCallback statusCallback) {
-        QueryTimeoutConfig timeoutConfig = new QueryTimeoutConfig(ctx.getRequestTimeout());
+            QueryStatusCallback statusCallback,
+            QueryTimeoutConfig timeoutConfig) {
         return GremlinExecutor.LifeCycle.build()
                 .evaluationTimeoutOverride(timeoutConfig.getExecutionTimeoutMS())
                 .beforeEval(
@@ -335,7 +323,11 @@ public class IrStandardOpProcessor extends StandardOpProcessor {
                                     processTraversal(
                                             traversal,
                                             new GremlinResultProcessor(
-                                                    ctx, traversal, statusCallback, timeoutConfig),
+                                                    configs,
+                                                    ctx,
+                                                    traversal,
+                                                    statusCallback,
+                                                    timeoutConfig),
                                             irMeta,
                                             timeoutConfig,
                                             statusCallback.getQueryLogger());
@@ -350,7 +342,7 @@ public class IrStandardOpProcessor extends StandardOpProcessor {
     // add script argument to print with ir plan
     protected void processTraversal(
             Traversal traversal,
-            ResultProcessor resultProcessor,
+            AbstractResultProcessor resultProcessor,
             IrMeta irMeta,
             QueryTimeoutConfig timeoutConfig,
             QueryLogger queryLogger)
@@ -390,6 +382,8 @@ public class IrStandardOpProcessor extends StandardOpProcessor {
         request = request.toBuilder().setConf(jobConfig).build();
 
         this.rpcClient.submit(request, resultProcessor, timeoutConfig.getChannelTimeoutMS());
+        // request results from remote engine service in blocking way
+        resultProcessor.request();
     }
 
     private Configs getQueryConfigs(Traversal traversal) {

interactive_engine/compiler/src/main/java/com/alibaba/graphscope/gremlin/plugin/processor/LifeCycleSupplier.java
@@ -18,7 +18,7 @@ package com.alibaba.graphscope.gremlin.plugin.processor;
 
 import com.alibaba.graphscope.common.client.ExecutionClient;
 import com.alibaba.graphscope.common.client.type.ExecutionRequest;
-import com.alibaba.graphscope.common.client.type.ExecutionResponseListener;
+import com.alibaba.graphscope.common.config.Configs;
 import com.alibaba.graphscope.common.config.QueryTimeoutConfig;
 import com.alibaba.graphscope.common.ir.tools.GraphPlanner;
 import com.alibaba.graphscope.common.ir.tools.QueryCache;
@@ -37,6 +37,7 @@ import java.util.List;
 import java.util.function.Supplier;
 
 public class LifeCycleSupplier implements Supplier<GremlinExecutor.LifeCycle> {
+    private final Configs configs;
     private final QueryCache queryCache;
     private final ExecutionClient client;
     private final Context ctx;
@@ -44,15 +45,19 @@ public class LifeCycleSupplier implements Supplier<GremlinExecutor.LifeCycle> {
     private final String queryName;
     private final IrMeta meta;
     private final QueryStatusCallback statusCallback;
+    private final QueryTimeoutConfig timeoutConfig;
 
     public LifeCycleSupplier(
+            Configs configs,
             Context ctx,
             QueryCache queryCache,
             ExecutionClient client,
             long queryId,
             String queryName,
             IrMeta meta,
-            QueryStatusCallback statusCallback) {
+            QueryStatusCallback statusCallback,
+            QueryTimeoutConfig timeoutConfig) {
+        this.configs = configs;
         this.ctx = ctx;
         this.queryCache = queryCache;
         this.client = client;
@@ -60,11 +65,11 @@ public class LifeCycleSupplier implements Supplier<GremlinExecutor.LifeCycle> {
         this.queryName = queryName;
         this.meta = meta;
         this.statusCallback = statusCallback;
+        this.timeoutConfig = timeoutConfig;
     }
 
     @Override
     public GremlinExecutor.LifeCycle get() {
-        QueryTimeoutConfig timeoutConfig = new QueryTimeoutConfig(ctx.getRequestTimeout());
         return GremlinExecutor.LifeCycle.build()
                 .evaluationTimeoutOverride(timeoutConfig.getExecutionTimeoutMS())
                 .beforeEval(
@@ -88,12 +93,14 @@ public class LifeCycleSupplier implements Supplier<GremlinExecutor.LifeCycle> {
                                         .info("ir plan {}", summary.getPhysicalPlan().explain());
                                 ResultSchema resultSchema =
                                         new ResultSchema(summary.getLogicalPlan());
-                                ExecutionResponseListener listener =
+                                GremlinResultProcessor listener =
                                         new GremlinResultProcessor(
+                                                configs,
                                                 ctx,
-                                                statusCallback,
                                                 new GremlinRecordParser(resultSchema),
-                                                resultSchema);
+                                                resultSchema,
+                                                statusCallback,
+                                                timeoutConfig);
                                 if (value.result != null && value.result.isCompleted) {
                                     List<IrResult.Results> records = value.result.records;
                                     records.forEach(k -> listener.onNext(k.getRecord()));
@@ -108,6 +115,8 @@ public class LifeCycleSupplier implements Supplier<GremlinExecutor.LifeCycle> {
                                             listener,
                                             timeoutConfig);
                                 }
+                                // request results from remote engine in a blocking way
+                                listener.request();
                             } catch (Exception e) {
                                 throw new RuntimeException(e);
                             }

commit 2ebe3515f4b431a34ec269a90d9c435d9a73f191

fix(interactive): Fix CI Bug Caused By PR #3630 (#3647)
<!--
Thanks for your contribution! please review
https://github.com/alibaba/GraphScope/blob/main/CONTRIBUTING.md before
opening an issue.
-->

## What do these changes do?
The issue is caused by PR #3630, which changed the result collection
from asynchronous to synchronous. After submitting a request to the
client, it is necessary to actively call a request to retrieve the
result from the buffer. However, the test chain forgot to add this call,
leading to the problem. This PR mainly fix the issue.

<!-- Please give a short brief about these changes. -->

## Related issue number

<!-- Are there any issues opened that will be resolved by merging this
change? -->

Fixes

interactive_engine/compiler/src/main/java/com/alibaba/graphscope/gremlin/integration/processor/IrTestOpProcessor.java
@@ -19,7 +19,6 @@ package com.alibaba.graphscope.gremlin.integration.processor;
 import com.alibaba.graphscope.common.client.ExecutionClient;
 import com.alibaba.graphscope.common.client.channel.ChannelFetcher;
 import com.alibaba.graphscope.common.client.type.ExecutionRequest;
-import com.alibaba.graphscope.common.client.type.ExecutionResponseListener;
 import com.alibaba.graphscope.common.config.Configs;
 import com.alibaba.graphscope.common.config.FrontendConfig;
 import com.alibaba.graphscope.common.config.QueryTimeoutConfig;
@@ -147,7 +146,7 @@ public class IrTestOpProcessor extends IrStandardOpProcessor {
                                     GraphPlanner.Summary summary = value.summary;
                                     ResultSchema resultSchema =
                                             new ResultSchema(summary.getLogicalPlan());
-                                    ExecutionResponseListener listener =
+                                    GremlinTestResultProcessor listener =
                                             new GremlinTestResultProcessor(
                                                     configs,
                                                     ctx,
@@ -171,6 +170,8 @@ public class IrTestOpProcessor extends IrStandardOpProcessor {
                                                 listener,
                                                 timeoutConfig);
                                     }
+                                    // request results from remote engine in a blocking way
+                                    listener.request();
                                     break;
                                 default:
                                     throw new IllegalArgumentException(