46e3559fb5477a374bf91f67555ca9d8a78b8597
Siyuan Zhang <siyuanzhang.zsy@alibaba-inc.com>
2024-05-20 09:52:13 +0800

    fix(interactive): refine rust otel export (#3810)

--- interactive_engine/executor/engine/pegasus/pegasus/src/config.rs
+++ interactive_engine/executor/engine/pegasus/pegasus/src/config.rs
@@ -37,7 +37,6 @@ macro_rules! configure_with_default {
 pub struct Configuration {
     pub network: Option<NetworkConfig>,
     pub max_pool_size: Option<u32>,
-    pub enable_tracing: Option<bool>,
 }
 
 impl Configuration {
@@ -46,11 +45,11 @@ impl Configuration {
     }
 
     pub fn singleton() -> Self {
-        Configuration { network: None, max_pool_size: None, enable_tracing: None }
+        Configuration { network: None, max_pool_size: None }
     }
 
     pub fn with(network: NetworkConfig) -> Self {
-        Configuration { network: Some(network), max_pool_size: None, enable_tracing: None }
+        Configuration { network: Some(network), max_pool_size: None }
     }
 
     pub fn server_id(&self) -> u64 {
