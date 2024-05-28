dad5421b4d7f93174495483669042765b279062a
杨关 <yangguan.yg@alibaba-inc.com>
2024-05-28 17:25:43 +0800

    fix bug

--- interactive_engine/groot-module/src/main/java/com/alibaba/graphscope/groot/schema/ddl/AddVertexTypePropertiesExecutor.java
+++ interactive_engine/groot-module/src/main/java/com/alibaba/graphscope/groot/schema/ddl/AddVertexTypePropertiesExecutor.java
@@ -1,6 +1,9 @@
 package com.alibaba.graphscope.groot.schema.ddl;
 
+import java.util.Map;
+
 import com.alibaba.graphscope.groot.common.schema.wrapper.GraphDef;
+import com.alibaba.graphscope.groot.common.schema.wrapper.LabelId;
 import com.alibaba.graphscope.groot.common.schema.wrapper.TypeDef;
 import com.alibaba.graphscope.groot.operation.Operation;
 import com.alibaba.graphscope.groot.operation.ddl.AddVertexTypePropertiesOperation;
@@ -9,8 +12,10 @@ public class AddVertexTypePropertiesExecutor extends AbstractAddTypePropertiesEx
 
     @Override
     protected Operation makeOperation(int partition, long version, TypeDef typeDef, GraphDef newGraphDef) {
+        Map<LabelId, Long> vertexTableIdMap = newGraphDef.getVertexTableIds();
+        Long tableId = vertexTableIdMap.get(typeDef.getTypeLabelId());
         return new AddVertexTypePropertiesOperation(
-                partition, version, typeDef, newGraphDef.getTableIdx()
+                partition, version, typeDef, tableId
         );
     }
 }
