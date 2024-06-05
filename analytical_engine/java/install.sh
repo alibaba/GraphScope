ec92cf3b2e32693411fa85c010915bd8d544df43
xiaolei.zl <xiaolei.zl@alibaba-inc.com>
2024-06-05 14:47:56 +0800

    remove test script
    Committed-by: xiaolei.zl from Dev container

--- analytical_engine/java/install.sh
+++
@@ -1,3 +0,0 @@
-cp grape-runtime/target/native/libgrape-jni.so /opt/graphscope/lib 
-cp grape-runtime/target/grape-runtime-0.28.0-shaded.jar /opt/graphscope/lib
-cp grape-giraph/target/grape-giraph-0.28.0-shaded.jar /opt/graphscope/lib
