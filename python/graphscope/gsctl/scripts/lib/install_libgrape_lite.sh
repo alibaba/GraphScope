36cea8db231ea1636dec3a4f1c89f46f4d48a4e3
xiaolei.zl <xiaolei.zl@alibaba-inc.com>
2024-06-06 15:56:25 +0800

    minor
    Committed-by: xiaolei.zl from Dev container
    
    Committed-by: xiaolei.zl from Dev container

--- python/graphscope/gsctl/scripts/lib/install_libgrape_lite.sh
+++ python/graphscope/gsctl/scripts/lib/install_libgrape_lite.sh
@@ -1,8 +1,7 @@
 install_libgrape_lite() {
     pushd /tmp
-    git clone https://github.com/alibaba/libgrape-lite.git
+    git clone https://github.com/alibaba/libgrape-lite.git -b v0.3.2 --single-branch
     cd libgrape-lite
-    git checkout v0.3.2
     mkdir build && cd build && cmake .. -DBUILD_LIBGRAPELITE_TESTS=OFF
     make -j ${parallelism} && sudo make install
     sudo cp /usr/local/lib/libgrape-lite.so /usr/lib/libgrape-lite.so
