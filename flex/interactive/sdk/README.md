217fc962b982f0d22c377bc2ffdd740822315d87
zhanglei1949 <xiaolei.zl@alibaba-inc.com>
2024-06-11 02:19:18 +0000

    revision interactive from 0.0.3 to 0.3

--- flex/interactive/sdk/README.md
+++ flex/interactive/sdk/README.md
@@ -6,7 +6,7 @@ GraphScope Interactive use [OpenAPI](https://openapis.org) to define the http in
 
 To use Interactive Python SDK
 ```bash
-pip3 install interactive-sdk==0.0.3
+pip3 install interactive-sdk==0.3
 ```
 
 To use Interactive Java SDK
@@ -14,7 +14,7 @@ To use Interactive Java SDK
 <dependency>
     <groupId>com.alibaba.graphscope</groupId>
     <artifactId>interactive-sdk</artifactId>
-    <version>0.0.3</version>
+    <version>0.3</version>
 </dependency>
 ```
 
--- flex/interactive/sdk/python/setup.py
+++ flex/interactive/sdk/python/setup.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 """
-    GraphScope Interactive API v0.0.3
+    GraphScope Interactive API v0.3
 
     This is the definition of GraphScope Interactive API, including   - AdminService API   - Vertex/Edge API   - QueryService   AdminService API (with tag AdminService) defines the API for GraphManagement, ProcedureManagement and Service Management.  Vertex/Edge API (with tag GraphService) defines the API for Vertex/Edge management, including creation/updating/delete/retrive.  QueryService API (with tag QueryService) defines the API for procedure_call, Ahodc query. 
 
@@ -22,7 +22,7 @@ from setuptools import find_packages, setup  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "interactive_sdk"
-VERSION = "0.0.3"
+VERSION = "0.3"
 PYTHON_REQUIRES = ">=3.7"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 2.1.0",
@@ -74,11 +74,11 @@ class BuildProto(Command):
 setup(
     name=NAME,
     version=VERSION,
-    description="GraphScope Interactive API v0.0.3",
+    description="GraphScope Interactive API v0.3",
     author="OpenAPI Generator community",
     author_email="graphscope@alibaba-inc.com",
     url="",
-    keywords=["OpenAPI", "OpenAPI-Generator", "GraphScope Interactive API v0.0.3"],
+    keywords=["OpenAPI", "OpenAPI-Generator", "GraphScope Interactive API v0.3"],
     install_requires=REQUIRES,
     packages=find_packages(exclude=["test", "tests"]),
     include_package_data=True,
