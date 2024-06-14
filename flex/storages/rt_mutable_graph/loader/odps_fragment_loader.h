465f63ab41f95ac1665d926c57588f204d759fa6
liulx20 <liulexiao.llx@alibaba-inc.com>
2024-06-14 20:51:27 +0800

    fix odps

--- flex/storages/rt_mutable_graph/loader/odps_fragment_loader.h
+++ flex/storages/rt_mutable_graph/loader/odps_fragment_loader.h
@@ -120,7 +120,7 @@ class ODPSStreamRecordBatchSupplier : public IRecordBatchSupplier {
   ODPSStreamRecordBatchSupplier(label_t label_id, const std::string& file_path,
                                 const ODPSReadClient& odps_table_reader,
                                 const std::string& session_id, int split_count,
-                                TableIdentifier table_identifier,
+                                TableIdentifier table_identifier, int worker_id,
                                 int worker_num);
 
   std::shared_ptr<arrow::RecordBatch> GetNextBatch() override;
@@ -130,10 +130,11 @@ class ODPSStreamRecordBatchSupplier : public IRecordBatchSupplier {
   const ODPSReadClient& odps_read_client_;
   std::string session_id_;
   int split_count_;
-  int worker_num_;
+
   TableIdentifier table_identifier_;
 
   int32_t cur_split_index_;
+  int worker_num_;
   ReadRowsReq read_rows_req_;
   std::shared_ptr<Reader> cur_batch_reader_;
 };
