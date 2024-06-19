d3950c962b86cff248313a45dbcbc99f74f5a179
shirly121 <yihe.zxl@alibaba-inc.com>
2024-06-19 16:41:26 +0800

    minor fix

--- interactive_engine/compiler/src/test/java/com/alibaba/graphscope/gremlin/antlr4x/GraphBuilderTest.java
+++ interactive_engine/compiler/src/test/java/com/alibaba/graphscope/gremlin/antlr4x/GraphBuilderTest.java
@@ -1731,7 +1731,7 @@ public class GraphBuilderTest {
         RelNode rel =
                 eval(
                         "g.V().out('2..3').with('RESULT_OPT',"
-                            + " 'ALL_V_E').as('a').select('a').valueMap('~len')");
+                                + " 'ALL_V_E').as('a').select('a').valueMap('~len')");
         Assert.assertEquals(
                 "GraphLogicalProject($f0=[$f0], isAppend=[false])\n"
                     + "  GraphLogicalProject($f0=[MAP(_UTF-8'~len', a.~len)], isAppend=[true])\n"
@@ -1748,7 +1748,7 @@ public class GraphBuilderTest {
         rel =
                 eval(
                         "g.V().out('2..3').with('RESULT_OPT',"
-                            + " 'ALL_V_E').as('a').select('a').valueMap('name', 'weight')");
+                                + " 'ALL_V_E').as('a').select('a').valueMap('name', 'weight')");
         Assert.assertEquals(
                 "GraphLogicalProject($f0=[$f0], isAppend=[false])\n"
                     + "  GraphLogicalProject($f0=[PATH_FUNCTION(a, FLAG(VERTEX_EDGE),"
