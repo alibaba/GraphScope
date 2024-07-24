3c7c99dc674463c5e4e7dcf90de9ed28aa40141a
xiaolei.zl <xiaolei.zl@alibaba-inc.com>
2024-07-24 20:36:59 +0800

    fixing ci
    Committed-by: xiaolei.zl from Dev container

--- flex/interactive/sdk/python/test/test_driver.py
+++ flex/interactive/sdk/python/test/test_driver.py
@@ -32,7 +32,11 @@ class TestDriver(unittest.TestCase):
 
     def setUp(self):
         # get endpoint from environment variable INTERACTIVE_ADMIN_ENDPOINT
-        self._driver = Driver()
+        self._endpoint = os.getenv("INTERACTIVE_ADMIN_ENDPOINT")
+        if self._endpoint is None:
+            self._endpoint = "http://localhost:7777"
+        print("endpoint: ", self._endpoint)
+        self._driver = Driver(self._endpoint)
         self._sess = self._driver.getDefaultSession()
         self._gremlin_client = self._driver.getGremlinClient()
         self._graph_id = None
