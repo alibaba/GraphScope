1f21c797e1171af983c23a1b916d007b2aab7863
霄磊 <xiaolei.zl@alibaba-inc.com>
2024-08-08 14:40:49 +0800

    add reporting message

--- charts/graphscope-interactive/script/switch_graph.py
+++ charts/graphscope-interactive/script/switch_graph.py
@@ -16,6 +16,11 @@
 # limitations under the License.
 #
 
+import time
+import hmac
+import hashlib
+import base64
+import urllib.parse
 
 import sys
 
@@ -48,18 +53,60 @@ import os
 script_directory = os.path.dirname(os.path.abspath(__file__))
 print("script directory", script_directory)
 
+uri = "https://oapi.dingtalk.com/robot/send?access_token="
+# read token from ${HOME}/.dingtalk_token
+token = ""
+with open(os.path.expanduser("~/.dingtalk_token"), "r") as f:
+    token = f.read().strip()
+if token == "":
+    raise Exception("token is empty")
+
+secret = ""
+with open(os.path.expanduser("~/.dingtalk_secret"), "r") as f:
+    secret = f.read().strip()
+if secret == "":
+    raise Exception("secret is empty")
+
+
+def get_full_uri():
+    timestamp = str(round(time.time() * 1000))
+    secret_enc = secret.encode("utf-8")
+    string_to_sign = "{}\n{}".format(timestamp, secret)
+    string_to_sign_enc = string_to_sign.encode("utf-8")
+    hmac_code = hmac.new(
+        secret_enc, string_to_sign_enc, digestmod=hashlib.sha256
+    ).digest()
+    sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
+    print(timestamp)
+    print(sign)
+    return uri + token + "&timestamp=" + timestamp + "&sign=" + sign
+
+
+def report_message(message: str):
+    uri = get_full_uri()
+    print(uri)
+    real_msg = {"msgtype": "text", "text": {"content": message}}
+    print(real_msg)
+    import requests
+
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(uri, json=real_msg, headers=headers)
+    print(response.text)
+
 
 def restart_service(sess: Session, graph_id: str):
     resp = sess.start_service(
         start_service_request=StartServiceRequest(graph_id=graph_id)
     )
-    assert resp.is_ok()
+    if not resp.is_ok():
+        report_message(f"Failed to restart service, graph_id: {graph_id}")
     print("restart service successfully")
 
 
 def get_service_status(sess: Session):
     resp = sess.get_service_status()
-    assert resp.is_ok()
+    if not resp.is_ok():
+        report_message("Failed to get service status")
     print("service status: ", resp.get_value())
     status = resp.get_value()
     print("service running is now running on graph", status.graph.id)
@@ -67,14 +114,16 @@ def get_service_status(sess: Session):
 
 def get_current_running_graph(sess: Session):
     resp = sess.get_service_status()
-    assert resp.is_ok()
+    if not resp.is_ok():
+        report_message("Failed to get service
     status = resp.get_value()
     return status.graph.id
 
 
 def list_graph(sess: Session):
     resp = sess.list_graphs()
-    assert resp.is_ok()
+    if not resp.is_ok():
+        report_message("Failed to list graph")
     res = resp.get_value()
     graph_id_arr = [graph.id for graph in res]
     print("list graph: ", graph_id_arr)
@@ -83,7 +132,8 @@ def list_graph(sess: Session):
 def check_graph_exits(sess: Session, graph_id: str):
     resp = sess.get_graph_schema(graph_id=graph_id)
     print("check graph exits: ", resp.is_ok())
-    assert resp.is_ok()
+    if not resp.is_ok():
+        report_message(f"Failed to get graph schema, graph_id: {graph_id}")
     print("graph exits: ", resp.get_value())
 
 
@@ -94,10 +144,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--endpoint", type=str, default="http://localhost:7777")
     parser.add_argument("--graph_id", type=str, default=None, required=False)
+    parser.add_argument("--validate-reporting", type=bool, default=False)
 
     # finish
     args = parser.parse_args()
     print(args)
+    if args.validate_reporting:
+        report_message("Testing message")
+        sys.exit(0)
     print("connecting to ", args.endpoint)
     driver = Driver(args.endpoint)
     sess = driver.session()
@@ -132,4 +186,9 @@ if __name__ == "__main__":
     # after switch to new graph, delete the old graph
     delete_graph = sess.delete_graph(old_graph)
     print("delete graph res: ", delete_graph)
-    assert delete_graph.is_ok()
+    if not delete_graph.is_ok():
+        report_message(f"Failed to delete graph {old_graph}")
+
+    report_message(
+        f"Switched to graph {graph_id} successfully, restart service cost {execution_time:.6f}seconds"
+    )
