9169cd810b66a007c690c83b74a1291a6693b5e3
xiaolei.zl <xiaolei.zl@alibaba-inc.com>
2024-08-09 16:39:37 +0800

    fix
    Committed-by: xiaolei.zl from Dev container

--- flex/storages/rt_mutable_graph/loading_config.cc
+++ flex/storages/rt_mutable_graph/loading_config.cc
@@ -902,11 +902,10 @@ LoadingConfig::GetVertexColumnMappings(label_t label_id) const {
 const std::vector<std::tuple<size_t, std::string, std::string>>&
 LoadingConfig::GetEdgeColumnMappings(label_t src_label_id, label_t dst_label_id,
                                      label_t edge_label_id) const {
-  THROW_EXCEPTION_IF(edge_column_mappings_.find(std::make_tuple(
-                         src_label_id, dst_label_id, edge_label_id)) ==
-                         edge_column_mappings_.end(),
-                     "Edge label id not found in edge column mappings");
   auto key = std::make_tuple(src_label_id, dst_label_id, edge_label_id);
+  THROW_EXCEPTION_IF(
+      edge_column_mappings_.find(key) == edge_column_mappings_.end(),
+      "Edge label id not found in edge column mappings");
   return edge_column_mappings_.at(key);
 }
 
@@ -914,11 +913,9 @@ const std::pair<std::vector<std::pair<std::string, size_t>>,
                 std::vector<std::pair<std::string, size_t>>>&
 LoadingConfig::GetEdgeSrcDstCol(label_t src_label_id, label_t dst_label_id,
                                 label_t edge_label_id) const {
-  THROW_EXCEPTION_IF(edge_src_dst_col_.find(std::make_tuple(
-                         src_label_id, dst_label_id, edge_label_id)) ==
-                         edge_src_dst_col_.end(),
-                     "Edge label id not found in edge column mappings");
   auto key = std::make_tuple(src_label_id, dst_label_id, edge_label_id);
+  THROW_EXCEPTION_IF(edge_src_dst_col_.find(key) == edge_src_dst_col_.end(),
+                     "Edge label id not found in edge column mappings");
   return edge_src_dst_col_.at(key);
 }
 
