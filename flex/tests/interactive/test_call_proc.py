9f4b897a4eb2dc84190305aea4d87cbdcefa873f
xiaolei.zl <xiaolei.zl@alibaba-inc.com>
2024-08-29 10:31:09 +0800

    add encoder and decoder for python sdk
    Committed-by: xiaolei.zl from Dev container

--- flex/tests/interactive/test_call_proc.py
+++ flex/tests/interactive/test_call_proc.py
@@ -23,6 +23,7 @@ import sys
 sys.path.append("../../interactive/sdk/python")
 
 from gs_interactive.client.driver import Driver
+from gs_interactive.client.utils import Encoder, Decoder
 from gs_interactive.models.base_edge_type_vertex_type_pair_relations_inner import (
     BaseEdgeTypeVertexTypePairRelationsInner,
 )
@@ -98,19 +99,18 @@ class ProcedureCaller():
     def callProcedureWithEncoder(self, graph_id : str):
         # count_vertex_num, should be with id 1
         # construct a byte array with bytes: 0x01
-        params = chr(1)
-        resp = self._sess.call_procedure_raw(graph_id, params)
+        encoder = Encoder()
+        encoder.put_byte(1)
+        resp = self._sess.call_procedure_raw(graph_id, encoder.get_bytes())
         if not resp.is_ok():
             print("call count_vertex_num failed: ", resp.get_status_message())
             exit(1)
 
         # plus_one, should be with id 2
-        # construct a byte array with bytes: the 4 bytes of integer 1, and a byte 2
-        value = 1
-        byte_string = value.to_bytes(4, byteorder=sys.byteorder) + bytes([2])
-        # byte_string = bytes([1,0,0,0,2]) # 4 bytes of integer 1, and a byte 3
-        params = byte_string.decode('utf-8')
-        resp = self._sess.call_procedure_raw(graph_id, params)
+        encoder2 = Encoder()
+        encoder2.put_int(1)
+        encoder2.put_byte(2)
+        resp = self._sess.call_procedure_raw(graph_id, encoder2.get_bytes())
         if not resp.is_ok():
             print("call plus_one failed: ", resp.get_status_message())
             exit(1)
