dd6e8d28be01f23aa0e5f8e379279490f0182421
xiaolei.zl <xiaolei.zl@alibaba-inc.com>
2024-09-05 11:39:02 +0800

    code refactor
    Committed-by: xiaolei.zl from Dev container

--- flex/interactive/sdk/generate_sdk_doc.sh
+++ flex/interactive/sdk/generate_sdk_doc.sh
@@ -29,7 +29,7 @@ function usage() {
 
 function echo_and_run() {
   echo "$@"
-  eval "$@"
+  eval "$@" || exit 1
 }
 
 function do_gen() {
@@ -49,23 +49,23 @@ function do_gen() {
   # replace all occurrence of $GENERATED_ENDPOINT with $ENDPOINT_PLACE_HOLDER
   # in the generated docs, for all files under ./java/docs and ./python/docs
   cmd="sed -i 's|${GENERATED_ENDPOINT}|${ENDPOINT_PLACE_HOLDER}|g' ./java/docs/*"
-  echo_and_run $cmd
+  echo_and_run ${cmd}
   cmd="sed -i 's|${GENERATED_ENDPOINT}|${ENDPOINT_PLACE_HOLDER}|g' ./python/docs/*"
-  echo_and_run $cmd
+  echo_and_run ${cmd}
   cmd="sed -i 's|\.\.\/README|python_sdk|g' ./python/docs/*"
-  echo_and_run $cmd
+  echo_and_run ${cmd}
   cmd="sed -i 's|README|python_sdk|g' ./python/docs/*"
-  echo_and_run $cmd
+  echo_and_run ${cmd}
   cmd="sed -i 's|documentation-for-models|documentation-for-data-structures|g' ./python/docs/*"
-  echo_and_run $cmd
+  echo_and_run ${cmd}
   cmd="sed -i 's|documentation-for-api-endpoints|documentation-for-service-apis|g' ./python/docs/*"
-  echo_and_run $cmd
+  echo_and_run ${cmd}
 
   # # Copy the generated docs to the output path
   cmd="find ./java/docs/* -type f ! -name "*Api*" -exec cp {} ${OUTPUT_PATH}/java/ \;"
-  echo_and_run $cmd
+  echo_and_run ${cmd}
   cmd="find ./python/docs/* -type f ! -name "*Api*" -exec cp {} ${OUTPUT_PATH}/python/ \;"
-  echo_and_run $cmd
+  echo_and_run ${cmd}
 
   echo "SDK documentation generated successfully."
 }
