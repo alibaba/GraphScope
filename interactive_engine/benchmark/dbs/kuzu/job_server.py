18d0019a79ef6597937ed6427095132c1452b8ce
BingqingLyu <bingqing.lbq@alibaba-inc.com>
2024-09-18 21:03:28 +0800

    refine

--- interactive_engine/benchmark/dbs/kuzu/job_server.py
+++ interactive_engine/benchmark/dbs/kuzu/job_server.py
@@ -1,26 +1,24 @@
 import kuzu
+import sys
 
 def execute_cypher_file(conn, file_path):
     with open(file_path, 'r') as file:
         for line in file:
             statement = line.strip()
             if statement:
-                print(statement)
                 conn.execute(statement)
 
-def main():
-    # Initialize database
-    print("starting...")
-    db = kuzu.Database("./job_db")
+def main(db_path, schema_path, dataloading_path):
+    db = kuzu.Database(db_path)
     conn = kuzu.Connection(db)
 
     print("start inserting")
 
     # Create schema
-    execute_cypher_file(conn, "./utils/job_schema.cypher")
+    execute_cypher_file(conn, schema_path)
 
     # Insert data
-    execute_cypher_file(conn, "./utils/job_dataloading.cypher")
+    execute_cypher_file(conn, dataloading_path)
 
     print("start querying")
 
@@ -37,6 +35,11 @@ def main():
 
     print("query end")
 
-
 if __name__ == "__main__":
-        main()
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <db_path> <schema_path> <dataloading_path>")
+    else:
+        db_path = sys.argv[1]
+        schema_path = sys.argv[2]
+        dataloading_path = sys.argv[3]
+        main(db_path, schema_path, dataloading_path)
--- interactive_engine/benchmark/example/job_bench.sh
+++ interactive_engine/benchmark/example/job_bench.sh
@@ -1,36 +1,60 @@
 #!/bin/bash
 
 set -x
-BASE_DIR=$(cd "$(dirname "$0")"; pwd)
-CURR_DIR=$(pwd)
+CURR_DIR=$(cd "$(dirname "$0")"; pwd)
+BENCHMARK_DIR= ${CURR_DIR}/..
 declare -r CONFIG_FILE="${CURR_DIR}/job_benchmark.properties"
 declare -r DATA_DIR="/tmp/bench_data"
 declare -r GIE_COMPILER_DIR=${CURR_DIR}/../../compiler
 declare -r GIE_ENGINE_DIR=${CURR_DIR}/../../executor/ir
+declare -r GIE_EXP_STORE_DIR=${CURR_DIR}/../../executor/store/exp_store
 declare -r KUZU_DB_DIR=${CURR_DIR}/../dbs/kuzu
-
+declare -r KUZU_JOB_DIR="/tmp/bench_data/kuzu_job"
 
 # download binary data for GIE
 mkdir -p ${DATA_DIR}
-curl -o ${DATA_DIR}/gie_imdb_bin.tar.gz -L "https://graphscope.oss-cn-beijing.aliyuncs.com/gopt_data/imdb_bin.tar.gz"
-cd ${DATA_DIR} && tar xvzf gie_imdb_bin.tar.gz
+if [ ! -d ${DATA_DIR}/imdb_bin ]; then
+    echo "Downloading binary data for GIE..."
+    curl -o ${DATA_DIR}/imdb_bin.tar.gz -L "https://graphscope.oss-cn-beijing.aliyuncs.com/gopt_data/imdb_bin.tar.gz"
+    cd ${DATA_DIR} && tar xvzf imdb_bin.tar.gz
+else
+    echo "Binary data for GIE already exists."
+fi
+
+# download raw data for kuzu
+if [ ! -d ${DATA_DIR}/imdb ]; then
+    echo "Downloading raw csv data for kuzu..."
+    curl -o ${DATA_DIR}/imdb.tar.gz -L "https://graphscope.oss-accelerate-overseas.aliyuncs.com/dataset/imdb.tar.gz"
+    cd ${DATA_DIR} && tar xvzf imdb.tar.gz
+else
+    echo "Raw csv data for kuzu already exists."
+fi
+
+# clean service
+ps -ef | grep "com.alibaba.graphscope.GraphServer" | grep -v grep | awk '{print $2}' | xargs kill -9 || true
+ps -ef | grep "start_rpc_server" | grep -v grep | awk '{print $2}' | xargs kill -9
+
 # start engine for GIE
-cd ${GIE_COMPILER_DIR} && make build && make run gremlin.script.language.name=antlr_gremlin_calcite graph.physical.opt=proto graph.planner.opt=CBO graph.statistics=./src/test/resources/statistics/imdb_statistics.json &
-cd ${GIE_ENGINE_DIR}/target/release && RUST_LOG=info DATA_PATH=/tmp/bench_data/gie_imdb_bin ./start_rpc_server --config ${GIE_ENGINE_DIR}/integrated/config &
+sed -i "s/SingleValueTable/PropertyTable/g" ${GIE_EXP_STORE_DIR}/src/graph_db/graph_db_impl.rs # This is a temporary fix for exp store, as imdb data is encoded as PropertyTable
+echo "Starting GIE engine..."
+cd ${GIE_COMPILER_DIR} && make build 
+cd ${GIE_COMPILER_DIR} && make run graph.schema:=../executor/ir/core/resource/imdb_schema.yaml gremlin.script.language.name=antlr_gremlin_calcite graph.physical.opt=proto graph.planner.opt=CBO graph.statistics=./src/test/resources/statistics/imdb_statistics.json &
+cd ${GIE_ENGINE_DIR}/target/release && RUST_LOG=info DATA_PATH=/tmp/bench_data/imdb_bin ./start_rpc_server --config ${GIE_ENGINE_DIR}/integrated/config &
+# waiting for loading data
+sleep 60
 
-
-# download raw csv data for kuzu
-curl -o ${DATA_DIR}/imdb_csv.tar.gz -L "https://graphscope.oss-accelerate-overseas.aliyuncs.com/dataset/imdb.tar.gz"
-cd ${DATA_DIR} && tar xvzf imdb_csv.tar.gz
-
-# start engine for kuzu
-cd ${KUZU_DB_DIR} && python job_server.py
+# start kuzu
+echo "Starting kuzu..."
+if ! python3 -c "import kuzu" &> /dev/null; then
+    echo "Installing kuzu..."
+    python3 -m pip install kuzu
+else
+    echo "kuzu is already installed."
+fi
+if [ ! d ${KUZU_JOB_DIR} ]; then
+    cd ${KUZU_DB_DIR} && python3 job_server.py ${KUZU_JOB_DIR} ${KUZU_DB_DIR}/resource/job_schema.cypher ${KUZU_DB_DIR}/resource/job_dataloading.cypher 
+fi
 
 # run the benchmark
-cd ${BASE_DIR} && make build && make config.path=${CONFIG_FILE} run && make config.path=${CONFIG_FILE} collect
-
-
-
-
-
-
+echo "Running the benchmark..."
+cd ${BENCHMARK_DIR} && make build && make config.path=${CONFIG_FILE} run && make config.path=${CONFIG_FILE} collect
