b55b290e7a6388d56cc1f299fd63ad92e603a600
Dongze Li <dongze.ldz@alibaba-inc.com>
2024-09-18 17:20:49 +0800

    update

--- python/setup_gsctl.py
+++ python/setup_gsctl.py
@@ -51,7 +51,7 @@ REQUIRES = [
         if platform.system() == "Linux" and platform.machine() == "aarch64"
         else "click >= 8.1.6"
     ),
-    "graphscope-flex >= 0.28.0",
+    "graphscope-flex >= 0.28.1",
     "treelib",
     "packaging",
     "pyyaml",
