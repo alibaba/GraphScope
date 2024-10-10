d163d5b6d1ea946e90c637b4b8c00e26cc84e95a
zhichao-shi <shizhichao@hnu.edu.cn>
2024-10-10 15:20:45 +0800

    feat(interactive): Add three builtin procedures (#4249)
    This pull request introduces three built-in stored procedures as per the
    OSPP requirements. The following stored procedures have been
    implemented:
    
    **Shortest Path Between Three Points**: A procedure to compute the
    shortest path between any given three points in the graph.
    
    **K-hop Neighbor Traversal from a Specified Node**: A procedure that
    traverses the graph and retrieves all nodes within k hops from a
    specified starting node.
    
    **PageRank Calculation**: A procedure to compute the PageRank values of
    nodes within the graph.
    
    At present, both **k_neighbors** and **shortest_path_among_three** only
    support types where the id input parameter is of type int64. For
    example, CALL k_neighbors(1L, "person", 2);
    
    This PR addresses the following issue:
    https://github.com/alibaba/GraphScope/issues/3737
    Closes #3737

--- flex/interactive/sdk/python/gs_interactive/tests/test_robustness.py
+++ flex/interactive/sdk/python/gs_interactive/tests/test_robustness.py
@@ -176,6 +176,7 @@ def test_procedure_creation(interactive_session, neo4j_session, create_modern_gr
 
 def test_builtin_procedure(interactive_session, neo4j_session, create_modern_graph):
     print("[Test builtin procedure]")
+    import_data_to_full_modern_graph(interactive_session, create_modern_graph)
     # Delete the builtin procedure should fail
     with pytest.raises(Exception):
         delete_procedure(interactive_session, create_modern_graph, "count_vertices")
@@ -197,12 +198,41 @@ def test_builtin_procedure(interactive_session, neo4j_session, create_modern_gra
         )
     # Call the builtin procedure
     start_service_on_graph(interactive_session, create_modern_graph)
-    call_procedure(neo4j_session, create_modern_graph, "count_vertices", '"person"')
+    call_procedure(
+        neo4j_session,
+        create_modern_graph,
+        "count_vertices",
+        '"person"',
+    )
 
+    call_procedure(
+        neo4j_session,
+        create_modern_graph,
+        "pagerank",
+        '"person"',
+        '"knows"',
+        "0.85",
+        "100",
+        "0.000001",
+    )
 
-def test_stop_service(interactive_session, create_modern_graph):
-    print("[Test stop service]")
-    start_service_on_graph(interactive_session, create_modern_graph)
-    # stop the service
-    stop_res = interactive_session.stop_service(graph_id="A Invalid graph id")
-    assert not stop_res.is_ok()
+    call_procedure(
+        neo4j_session,
+        create_modern_graph,
+        "k_neighbors",
+        "1L",
+        '"person"',
+        "2",
+    )
+
+    call_procedure(
+        neo4j_session,
+        create_modern_graph,
+        "shortest_path_among_three",
+        '"person"',
+        "1L",
+        '"person"',
+        "2L",
+        '"person"',
+        "4L",
+    )
