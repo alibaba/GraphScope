ec09f80252770dbde9d2e9da133f5bc1ff721b96
xiaolei.zl <xiaolei.zl@alibaba-inc.com>
2024-10-18 14:33:31 +0800

    minor format
    Committed-by: xiaolei.zl from Dev container

--- interactive_engine/compiler/build_ir_core.sh
+++ interactive_engine/compiler/build_ir_core.sh
@@ -5,8 +5,8 @@ set -x
 
 if $(! command -v cargo &> /dev/null)
 then
-   echo "cargo not exit, skip compile"
+    echo "cargo not exit, skip compile"
 else
-   cd ../executor/ir/core
-   cargo build --release
+    cd ../executor/ir/core
+    cargo build --release
 fi
