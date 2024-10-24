99c769bd77a00bd95ed51893184ac85fa922b4c0
xiaolei.zl <xiaolei.zl@alibaba-inc.com>
2024-10-24 12:52:47 +0800

    fix
    Committed-by: xiaolei.zl from Dev container

--- flex/storages/rt_mutable_graph/mutable_property_fragment.cc
+++ flex/storages/rt_mutable_graph/mutable_property_fragment.cc
@@ -192,11 +192,7 @@ void MutablePropertyFragment::Open(const std::string& work_dir,
     }
 
     // We will reserve the at least 4096 slots for each vertex label
-    auto max_vnum = schema_.get_max_vnum(v_label_name);
-    size_t vertex_capacity =
-        std::max(max_vnum == Schema::DEFAULT_MAX_VNUM ? lf_indexers_[i].size()
-                                                      : max_vnum,
-                 (size_t) 4096);
+    size_t vertex_capacity = std::max(lf_indexers_[i].size(), (size_t) 4096);
     if (vertex_capacity >= lf_indexers_[i].size()) {
       lf_indexers_[i].reserve(vertex_capacity);
     }
