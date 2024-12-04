df1eb99836c24bacff6379dd26381cbe8d21b6b1
xiaolei.zl <xiaolei.zl@alibaba-inc.com>
2024-12-04 14:15:05 +0800

    format
    Committed-by: xiaolei.zl from Dev container

--- flex/engines/graph_db/runtime/adhoc/expr_impl.h
+++ flex/engines/graph_db/runtime/adhoc/expr_impl.h
@@ -85,7 +85,8 @@ class WithInExpr : public ExprBase {
   WithInExpr(const ReadTransaction& txn, const Context& ctx,
              std::unique_ptr<ExprBase>&& key, const common::Value& array)
       : key_(std::move(key)) {
-    if constexpr( (std::is_same_v<T, int64_t>) || (std::is_same_v<T, int32_t>)) {
+    if constexpr ((std::is_same_v<T, int64_t>) ||
+                  (std::is_same_v<T, int32_t>) ) {
       // Implicitly convert to T
       if (array.item_case() == common::Value::kI64Array) {
         size_t len = array.i64_array().item_size();
@@ -102,8 +103,7 @@ class WithInExpr : public ExprBase {
                    << typeid(T).name() << " with array of type "
                    << array.item_case();
       }
-    }
-     else if constexpr (std::is_same_v<T, std::string>) {
+    } else if constexpr (std::is_same_v<T, std::string>) {
       CHECK(array.item_case() == common::Value::kStrArray);
       size_t len = array.str_array().item_size();
       for (size_t idx = 0; idx < len; ++idx) {
