9dcc7010fdeb442c88fb44a1576203429d6af881
xiaolei.zl <xiaolei.zl@alibaba-inc.com>
2024-12-18 17:24:31 +0800

    fix
    Committed-by: xiaolei.zl from Dev container

--- flex/interactive/sdk/python/gs_interactive/client/status.py
+++ flex/interactive/sdk/python/gs_interactive/client/status.py
@@ -21,9 +21,9 @@ from gs_interactive.exceptions import ApiException
 from gs_interactive.exceptions import BadRequestException
 from gs_interactive.exceptions import ForbiddenException
 from gs_interactive.exceptions import NotFoundException
-from gs_interactive.exceptions import ProtocolError
 from gs_interactive.exceptions import ServiceException
 from urllib3.exceptions import MaxRetryError
+from urllib3.exceptions import ProtocolError
 
 from gs_interactive.client.generated.interactive_pb2 import Code as StatusCode
 from gs_interactive.models.api_response_with_code import APIResponseWithCode
