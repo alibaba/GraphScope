fb71533a1d7cfe5d3ab070588c413fbe269dbbdb
bingqing.lbq <bingqing.lbq@alibaba-inc.com>
2025-01-08 11:17:16 +0800

    minor refine
    Committed-by: bingqing.lbq from Dev container

---
+++ interactive_engine/groot-http/src/test/java/com/alibaba/graphscope/groot/OpenApiGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibaba.graphscope.groot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
