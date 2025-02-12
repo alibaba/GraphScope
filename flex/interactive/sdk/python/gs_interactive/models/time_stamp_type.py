bed6940389385cca965f6ae5a2b164b16fa9de11
xiaolei.zl <xiaolei.zl@alibaba-inc.com>
2025-02-12 10:34:45 +0800

    avoid multiple match
    Committed-by: xiaolei.zl from Dev container

--- flex/interactive/sdk/python/gs_interactive/models/time_stamp_type.py
+++ flex/interactive/sdk/python/gs_interactive/models/time_stamp_type.py
@@ -86,10 +86,6 @@ class TimeStampType(BaseModel):
         if not isinstance(obj, dict):
             return cls.model_validate(obj)
 
-        for key in obj:
-            if key not in cls.__properties:
-                raise ValueError(f"Unexpected field {key} for TimeStampType")
-
         _obj = cls.model_validate({
             "timestamp": obj.get("timestamp")
         })
