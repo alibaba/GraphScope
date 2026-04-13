9988499af336114819c3d0437eedd1cb866b5536
Longbin Lai <longbin.lailb@alibaba-inc.com>
2026-04-13 13:32:17 +0800

    docs: Upgrade neug doc to version 0.1.1 (#4651)
    As titled
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ neug/_next/static/chunks/app/[lang]/not-found-328ef61f34194bc1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[97],{2550:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=2550)}),_N_E=n.O()}]);
