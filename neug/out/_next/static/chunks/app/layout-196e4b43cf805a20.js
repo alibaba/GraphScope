2baa7a965ba366799130150157b80970eadcceda
Longbin Lai <longbin.lailb@alibaba-inc.com>
2026-07-02 22:06:34 +0800

    Fix some doc glitches of NeuG v0.1.3 docs (#4668)
    Fixes
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- neug/out/_next/static/chunks/app/layout-196e4b43cf805a20.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{6708:function(n,e,u){Promise.resolve().then(u.t.bind(u,7527,23))},7527:function(){}},function(n){n.O(0,[957,971,117,744],function(){return n(n.s=6708)}),_N_E=n.O()}]);
